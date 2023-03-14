c31444d24f5dc26b0b80453aa39f5574511792cb
Marcos Coelho <mviniciusca@gmail.com>
2023-03-13 21:20:50 -0300

    responsive layout to hero section

--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -22,7 +22,7 @@
     <div class="grid">
 
         <x-header-section />
-        {{--  <x-hero-section />  --}}
+        <x-hero-section />
         <x-about-section />
         <x-portfolio-section />
         <x-customers-section />
