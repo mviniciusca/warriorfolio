6d6ee6eb7dc605907b2ef459611e4bca31c6691d
Marcos Coelho <mviniciusca@gmail.com>
2023-03-18 18:16:36 -0300

    edit hero backrgound

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Header;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>codhous</title>
+    <title>Warrioroflio</title>
     {{-- Google fonts --}}
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     {{-- Vite --}}
