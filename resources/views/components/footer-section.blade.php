10b51ed4eb9b2f63748a69f313594c9472f87d23
Marcos Coelho <mviniciusca@gmail.com>
2023-03-29 02:37:07 -0300

    edit footer bg position

--- resources/views/components/footer-section.blade.php
+++ resources/views/components/footer-section.blade.php
@@ -45,8 +45,8 @@
             </a>
         @endisset
     </div>
-{{-- Background Footer Lights --}}
-    <div class="absolute w-full h-[600px] bottom-100 mb-96 -z-30 bg-cover bg-center bg-no-repeat footer-bg" style="background-image: url('{{ asset('/img/blur-end.png') }}')"></div>
 </div>
 </x-content-section>
+{{-- Background Footer Lights --}}
+<div class="absolute w-full h-[600px] mt-[-600px] -z-30 bg-cover bg-center bg-no-repeat" style="background-image: url('{{ asset('/img/blur-end.png') }}')"></div>
 
