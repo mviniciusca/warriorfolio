30dc9cf86a79013690a246c38c670ba8b7b3015f
Marcos Coelho <mviniciusca@gmail.com>
2023-04-15 20:19:57 -0300

    edit toolbar

--- resources/views/components/mail/toolbar.blade.php
+++ resources/views/components/mail/toolbar.blade.php
@@ -1,51 +1,28 @@
 @props(['message', 'inboxCount', 'sentCount', 'starredCount', 'trashedCount'])
 
 <div>
-    <div>
-        <div class="w-full rounded-md bg-white p-2">
-            <nav class="text-xs font-bold" aria-label="Tabs">
+    <div class="rounded-md bg-white p-2">
+        <nav class="text-xs font-bold" aria-label="Tabs">
 
-                {{-- Menu Options --}}
+            {{-- Inbox --}}
+            <x-mail.toolbar.button :counter='$inboxCount' :filter="'inbox'">
+                <x-svg.inbox />
+            </x-mail.toolbar.button>
 
-                <x-mail.toolbar.button :counter='$inboxCount' :title="''"
-                    :filter="'inbox'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
-                    </svg>
-                </x-mail.toolbar.button>
+            {{-- With Star --}}
+            <x-mail.toolbar.button :counter='$starredCount' :filter="'starred'">
+                <x-svg.star />
+            </x-mail.toolbar.button>
 
-                <x-mail.toolbar.button :counter='$starredCount' :title="''"
-                    :filter="'starred'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                    </svg>
-                </x-mail.toolbar.button>
+            {{-- Sent --}}
+            <x-mail.toolbar.button :counter='$sentCount' :filter="'sent'">
+                <x-svg.airplane />
+            </x-mail.toolbar.button>
 
-                <x-mail.toolbar.button :counter='$sentCount' :title="''"
-                    :filter="'sent'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                    </svg>
-                </x-mail.toolbar.button>
+            {{-- Trashed --}}
+            <x-mail.toolbar.button :counter='$trashedCount' :filter="'trashed'">
+                <x-svg.trash />
+            </x-mail.toolbar.button>
 
-                <x-mail.toolbar.button :counter='$trashedCount' :title="''"
-                    :filter="'trashed'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                </x-mail.toolbar.button>
-        </div>
     </div>
 </div>
--- resources/views/components/mail/toolbar/button.blade.php
+++ resources/views/components/mail/toolbar/button.blade.php
@@ -1,8 +1,8 @@
-@props(['filter', 'title', 'counter' => 0])
+@props(['filter', 'title' => '', 'counter' => 0])
 
 <div>
     <button
-        class="flex w-full items-center border-b-2 border-transparent px-1 py-3 transition-all duration-100 hover:rounded-lg hover:border-b-2 hover:text-primary-400"
+        class="flex w-full items-center border-b-2 border-transparent px-1 py-3 transition-all duration-200 hover:rounded-lg hover:border-b-2 hover:text-primary-400 active:text-primary-100 active:opacity-25"
         wire:click="setFilter('{{ $filter }}')">
         <div class="flex w-full items-center gap-2">
             {{ $slot }}
