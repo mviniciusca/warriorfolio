f71eb3476a75c3f2b9cf3fad5f3cac33bacf796e
Marcos Coelho <mviniciusca@gmail.com>
2023-04-20 19:05:53 -0300

    edit colors on messages

--- resources/views/components/mail/message.blade.php
+++ resources/views/components/mail/message.blade.php
@@ -1,7 +1,7 @@
 @props(['message'])
 <div class="flex overflow-hidden transition-all duration-100">
     <div
-        class="{{ $message->is_starred && !$message->is_trashed ? 'bg-orange-50' : '' }} {{ $message->is_read ? 'opacity-40' : '' }} {{ $message->is_sent && !$message->is_trashed ? 'bg-indigo-100' : '' }} {{ $message->is_trashed ? 'bg-red-100 important' : '' }} mb-2 flex w-full gap-4 rounded-md bg-zinc-50 p-4 text-sm text-zinc-500 shadow-sm transition-all duration-200 hover:opacity-70 active:opacity-100">
+        class="{{ $message->is_starred && !$message->is_trashed && !$message->is_sent ? 'bg-orange-50' : '' }} {{ $message->is_read ? 'opacity-40' : '' }} {{ $message->is_sent && !$message->is_trashed ? 'bg-indigo-100' : '' }} {{ $message->is_trashed ? 'bg-red-100 important' : '' }} {{ !$message->is_read && !$message->is_trashed && !$message->is_sent ? 'bg-zinc-50' : '' }} mb-2 flex w-full gap-4 rounded-md p-4 text-sm text-zinc-500 shadow-sm transition-all duration-200 hover:opacity-70 active:opacity-100">
         {{-- Add to favorite --}}
         <x-mail.add-favorite :message="$message" />
         {{-- Message --}}
@@ -12,7 +12,7 @@
             <span>
                 {{ $message->subject }}<br>
                 <p class="text-xs italic">
-                    {{ $message->created_at->format('d/m/Y H:i') }} •
+                    {{ $message->created_at->format('d/m/Y H:i') }}.
                     {{ $message->created_at->diffForHumans() }}
                 </p>
             </span>
