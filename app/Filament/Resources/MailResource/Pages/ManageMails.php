936706b122049e188f6e397f9712985a22c88c9a
Marcos Coelho <mviniciusca@gmail.com>
2023-12-14 22:24:38 -0300

    wip

--- app/Filament/Resources/MailResource/Pages/ManageMails.php
+++ app/Filament/Resources/MailResource/Pages/ManageMails.php
@@ -9,10 +9,7 @@ use Filament\Resources\Pages\ManageRecords;
 class ManageMails extends ManageRecords
 {
     protected static string $resource = MailResource::class;
-
-
     protected static ?string $title = '';
-
     protected function getHeaderActions(): array
     {
         return [
