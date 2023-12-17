7a3d9af7ff6fe4479bbf89049697c1536d564f23
Marcos Coelho <mviniciusca@gmail.com>
2023-12-17 12:36:33 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/courses.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/courses.blade.php
@@ -1,17 +1,43 @@
 @aware(['page'])
-<section class="w-full mr-auto bg-secondary-200 py-24 my-24">
-    <div class="px-4 py-4 md:py-8">
-        <div class="max-w-7xl mx-auto">
-
-            <div class="mr-auto flex-col items-center text-center justify-center">
-                <p class="bg-secondary-800 mr-auto w-24 h-24">
-                </p>
-                <div class="text-center mr-auto bg-red-200">software developer based in rio de janeiro</div>
+<section class="w-full bg-secondary-950 py-12 my-24">
+    <div class="px-4 py-4  md:py-8">
+        <div class="max-w-7xl mx-auto text-center">
+            <div class="header-title">tell about <span class="text-primary-500">you</span>.</div>
+            <div class="bg-secondary-700 h-36 w-36 my-8 rounded-full mx-auto mt-24"></div>
+            <p class="text-3xl tracking-tight font-semibold">warriorfusion software</p>
+            <p class="text-md tracking-tight my-4">fullstack develper • rio de janeiro </p>
+            <div class="flex mx-auto justify-center ">
+                <ion-icon name="logo-twitter" class="text-2xl cursor-pointer mr-4 hover:opacity-90 opacity-60">
+                </ion-icon>
+                <ion-icon name="logo-linkedin" class="text-2xl cursor-pointer mr-4 hover:opacity-90 opacity-60">
+                </ion-icon>
+                <ion-icon name="logo-github" class="text-2xl cursor-pointer mr-4 hover:opacity-90 opacity-60">
+                </ion-icon>
+                <ion-icon name="logo-instagram" class="text-2xl cursor-pointer mr-4 hover:opacity-90 opacity-60">
+                </ion-icon>
+                <ion-icon name="logo-medium" class="text-2xl mr-4 cursor-pointer hover:opacity-90 opacity-60">
+                </ion-icon>
             </div>
-
-
-
-
+            <div class="flex mt-12">
+                <div class="p-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa officiis
+                    exercitationem
+                    quos
+                    ad saepe magni provident! Nesciunt perspiciatis, in excepturi ratione animi laudantium consequatur
+                    quos
+                    exercitationem iure eos ipsa optio.</div>
+                <div class="p-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa officiis
+                    exercitationem
+                    quos
+                    ad saepe magni provident! Nesciunt perspiciatis, in excepturi ratione animi laudantium consequatur
+                    quos
+                    exercitationem iure eos ipsa optio.</div>
+                <div class="p-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa officiis
+                    exercitationem
+                    quos
+                    ad saepe magni provident! Nesciunt perspiciatis, in excepturi ratione animi laudantium consequatur
+                    quos
+                    exercitationem iure eos ipsa optio.</div>
+            </div>
         </div>
     </div>
 </section>
