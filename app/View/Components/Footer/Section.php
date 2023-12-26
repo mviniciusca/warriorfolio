dc9de081e6cdbabcf4f815398711cadd0a391685
Marcos Coelho <mviniciusca@gmail.com>
2023-12-26 12:57:29 -0300

    wip

--- app/View/Components/Footer/Section.php
+++ app/View/Components/Footer/Section.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components\Footer;
 
+use App\Models\Layout;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -21,6 +22,10 @@ class Section extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.footer.section');
+        return view('components.footer.section', [
+            'info' => Layout::query()
+                ->select(['contact_section_title', 'contact_section_subtitle_text'])
+                ->first(),
+        ]);
     }
 }
--- resources/views/livewire/mail/create-mail.blade.php
+++ resources/views/livewire/mail/create-mail.blade.php
@@ -1,9 +1,9 @@
 <div clas="w-full grid gap-8">
-    <form class="grid gap-8" wire:submit="create">
+    <form class="grid gap-8 p-8" wire:submit="create">
         {{ $this->form }}
 
         <button
-            class="bg-secondary-700 hover:opacity-90 transition-all duration-100 focus:border-2 focus:border-primary-500 py-2 px-4 mx-auto inline-block rounded-md"
+            class="bg-secondary-800 dark:border dark:border-secondary-600 hover:opacity-70 active:opacity-100 transition-all duration-100 focus:border focus:border-primary-500 py-2 px-12 mx-auto inline-block rounded-sm"
             type="submit">
             Submit
         </button>
