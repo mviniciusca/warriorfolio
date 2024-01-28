67fc22b716f8eda141ef20a26db307a9deb2b796
Marcos Coelho <mviniciusca@gmail.com>
2024-01-28 17:42:08 -0300

    wip

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -1,6 +1,6 @@
 {{-- Hero Background --}}
 @if($info->background_image_visibility)
-<div id="home" class="-mt-36 {{ $info->background_image ? '' : 'animate-pulse' }} m-auto left-0 right-0 bg-hero animate-opacity opacity-5 bg-fixed
+<div id="home" class="{{ $info->background_image ? '' : 'animate-pulse' }} bg-hero animate-opacity opacity-5
  w-full h-[1080px] absolute -z-20
  {{ $info->background_image_size . ' ' . $info->background_image_position . ' '
  . $info->background_image_repeat }}
