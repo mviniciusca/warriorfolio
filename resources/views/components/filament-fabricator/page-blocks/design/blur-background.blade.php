f2e770b112668c4e6c0533f30481364fe78a5823
Marcos Coelho <mviniciusca@gmail.com>
2024-01-29 12:43:33 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/design/blur-background.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/design/blur-background.blade.php
@@ -1,3 +1,3 @@
 @aware(['page'])
-<div id="bg-footer" class="absolute mt-0 h-48 w-full animate-pulse bg-contain bg-top bg-no-repeat"
+<div id="bg-footer" class="absolute -z-10 mt-0 h-48 w-full animate-pulse bg-contain bg-top bg-no-repeat"
     style="background-image: url('img/core/bg/separator-blur-md-bg.png')"></div>
--- resources/views/components/filament-fabricator/page-blocks/design/blur-separator.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/design/blur-separator.blade.php
@@ -1,3 +1,3 @@
 @aware(['page'])
-<div id="bg-footer" class="-mt-10 h-10 w-full animate-pulse bg-contain bg-bottom bg-no-repeat"
+<div id="bg-footer" class="absolute -z-10 -mt-10 h-10 w-full animate-pulse bg-contain bg-bottom bg-no-repeat"
     style="background-image: url('img/core/bg/separator-blur-sm-bg.png')"></div>
