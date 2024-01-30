6286eadef6528bd88afea5ce43ea10fb4b91ea5a
Marcos Coelho <mviniciusca@gmail.com>
2024-01-29 23:22:17 -0300

    wip

---
+++ app/Filament/Fabricator/PageBlocks/Component/Cta.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks\Component;
+
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class Cta extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('component.cta')
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title')
+                    ->required()
+                    ->columnSpanFull()
+                    ->placeholder('Title'),
+                FileUpload::make('image')
+                    ->columnSpanFull()
+                    ->label('Image')
+                    ->required()
+                    ->image(),
+                RichEditor::make('content')
+                    ->columnSpanFull()
+                    ->label('Content')
+                    ->required()
+                    ->placeholder('Content'),
+                TextInput::make('button_text')
+                    ->label('Button Text')
+                    ->required()
+                    ->placeholder('Button Text'),
+                TextInput::make('button_url')
+                    ->label('Button URL')
+                    ->default('#')
+                    ->required()
+                    ->placeholder('Button URL'),
+                TextInput::make('button_icon')
+                    ->label('Button Icon')
+                    ->placeholder('Button Icon'),
+            ])->columns(3);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return $data;
+    }
+}
---
+++ resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
@@ -0,0 +1,33 @@
+@aware(['page'])
+@props([
+'title' => null,
+'content' => null,
+'button_text' => null,
+'button_url' => '#',
+'button_icon' => null,
+'image' => null
+])
+<x-core.layout>
+    <section class="w-full">
+        <div
+            class="mx-auto max-w-screen-xl items-center gap-8 px-4 py-8 sm:py-16 md:grid md:grid-cols-2 lg:px-6 xl:gap-16">
+            <img class="w-full" src="{{ asset('storage/' . $image) }}" alt="image">
+            <div class="mt-4 md:mt-0">
+                <h2 class="mb-4 text-4xl font-extrabold tracking-tight">
+                    {!! $title !!}
+                </h2>
+                <p class="mb-6 font-light md:text-lg">{!! $content !!}</p>
+                <a href="{{ $button_url }}"
+                    class="my-4 inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    {{ $button_text }}
+                    <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </section>
+</x-core.layout>
