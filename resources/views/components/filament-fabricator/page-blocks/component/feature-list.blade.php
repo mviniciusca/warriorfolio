bf71c6bf63548310fbecb2ae9e645c3ead91459c
Marcos Coelho <mviniciusca@gmail.com>
2024-01-30 18:19:26 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -20,9 +20,7 @@
                                 {{ $item['description'] }}
                             </p>
                         </div>
-
                         @endforeach
-
                     </div>
                 </div>
             </section>
