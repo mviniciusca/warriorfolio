3e859be5008ca02ca9cc0ebd228e5fe9df77c42f
Marcos Coelho <mviniciusca@gmail.com>
2024-01-30 22:11:57 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Component/FeatureList.php
+++ app/Filament/Fabricator/PageBlocks/Component/FeatureList.php
@@ -18,20 +18,25 @@ class FeatureList extends PageBlock
                 Toggle::make('is_active')
                     ->default(true)
                     ->required()
-                    ->label('Is Active'),
+                    ->label('Show Module'),
                 Repeater::make('features')
                     ->schema([
                         TextInput::make('title')
                             ->required()
+                            ->columnSpan(2)
                             ->label('Title of the feature'),
                         TextInput::make('icon')
-                            ->label('Icon')
+                            ->label('Icon (optional)')
                             ->prefix('ion-icon')
-                            ->default('flash-outline')
-                            ->placeholder('name of the icon'),
+                            ->placeholder('icon-name')
+                            ->columnSpan(2)
+                            ->helperText('Ionicon Name (without the ion-icon prefix)'),
                         Textarea::make('description')
+                            ->columnSpanFull()
+                            ->rows(3)
+                            ->helperText('Here you can talk about the feature in detail.')
                             ->label('Description'),
-                    ]),
+                    ])->columns(4),
             ]);
     }
 
