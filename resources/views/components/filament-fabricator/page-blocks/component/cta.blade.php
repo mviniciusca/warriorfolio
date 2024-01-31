42d91fa5b13869cc5db5ef7b3c3a252d917f8492
Marcos Coelho <mviniciusca@gmail.com>
2024-01-31 09:50:53 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
@@ -29,15 +29,10 @@
                 @endif
 
                 @if($button_text)
-                <a href="{{ $button_url ?? '#'}}"
-                    class="my-4 inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    {{ $button_text }}
-                    <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                <a href="{{ $button_url ?? '#'}}">
+                    <x-ui.button type='submit' icon='chevron-forward-outline'>
+                        {{ $button_text }}
+                    </x-ui.button>
                 </a>
                 @endif
 
