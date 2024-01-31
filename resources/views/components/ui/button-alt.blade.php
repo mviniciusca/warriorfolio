ade46386acffdea08221a6eeb0bec8ddc34d4441
Marcos Coelho <mviniciusca@gmail.com>
2024-01-31 09:30:13 -0300

    wip

--- resources/views/components/ui/button-alt.blade.php
+++ resources/views/components/ui/button-alt.blade.php
@@ -1,4 +1,9 @@
-<button
-    class="my-4 inline-flex items-center rounded-lg bg-primary-50 px-5 py-2.5 text-center text-sm font-medium text-primary-700 hover:bg-primary-100 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+@props(['type' => 'button', 'icon' => null])
+
+<button type="{{ $type }}"
+    class="my-4 inline-flex items-center gap-1 rounded-lg bg-white px-5 py-2.5 text-center text-sm font-medium text-primary-500 hover:bg-primary-50 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
     {{ $slot }}
+    @if($icon)
+    <ion-icon name="{{ $icon }}"></ion-icon>
+    @endif
 </button>
