07c669239f80fdc84287a0bf8f9b253ed6976b65
Marcos Coelho <mviniciusca@gmail.com>
2024-02-01 20:40:15 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/heading-description.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/heading-description.blade.php
@@ -1,25 +1,27 @@
 @aware(['page'])
 @props(['is_active' => true, 'heading' => null, 'content' => null, 'is_center' => false , 'heading_text_size' =>
-'text-3xl','content_text_size' => 'text-xl'])
+'text-3xl', 'content_text_size' => 'text-xl'])
 
 @if($is_active)
 <div class="px-4 py-4">
     <div class="mx-auto max-w-7xl">
-        <section>
-            <div class="mx-auto max-w-screen-xl {{ $is_center ? 'text-center':'text-left' }}">
-                <div>
+        <div class="mx-auto max-w-screen-xl {{ $is_center ? 'text-center':'text-left' }}">
+            <div id="heading">
 
-                    <h2 class="heading-module-title mb-4 font-bold tracking-tighter {{ $heading_text_size }}">
-                        {!! $heading!!}
-                    </h2>
+                @if($heading)
+                <h2 class="heading-module-title mb-4 font-bold tracking-tighter {{ $heading_text_size }}">
+                    {!! $heading!!}
+                </h2>
+                @endif
 
-                    <div class="heading-module-subtitle mb-4 font-light {{ $content_text_size }}">
-                        {!! $content !!}
-                    </div>
-
+                @if($content)
+                <div class="heading-module-subtitle mb-4 font-light {{ $content_text_size }}">
+                    {!! $content !!}
                 </div>
+                @endif
+
             </div>
-        </section>
+        </div>
     </div>
 </div>
 @endif
