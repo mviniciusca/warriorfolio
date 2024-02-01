627e4241a7268a5775a2ccc9d74905b09d10752c
Marcos Coelho <mviniciusca@gmail.com>
2024-01-31 21:59:31 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Component/Cta.php
+++ app/Filament/Fabricator/PageBlocks/Component/Cta.php
@@ -5,6 +5,7 @@ namespace App\Filament\Fabricator\PageBlocks\Component;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
@@ -13,38 +14,46 @@ class Cta extends PageBlock
     public static function getBlockSchema(): Block
     {
         return Block::make('component.cta')
+            ->label('CTA: Image + Content Module')
+            ->icon('heroicon-o-cube')
             ->schema([
-                TextInput::make('title')
-                    ->label('Title')
-                    ->required()
-                    ->columnSpanFull()
-                    ->placeholder('Title'),
-                FileUpload::make('image')
-                    ->columnSpanFull()
-                    ->label('Image')
-                    ->required()
-                    ->imageEditor()
-                    ->directory('images/content')
-                    ->moveFiles()
-                    ->image(),
-                RichEditor::make('content')
-                    ->columnSpanFull()
-                    ->label('Content')
-                    ->required()
-                    ->placeholder('Content'),
-                TextInput::make('button_text')
-                    ->label('Button Text')
-                    ->required()
-                    ->placeholder('Button Text'),
-                TextInput::make('button_url')
-                    ->label('Button URL')
-                    ->default('#')
-                    ->required()
-                    ->placeholder('Button URL'),
-                TextInput::make('button_icon')
-                    ->label('Button Icon')
-                    ->placeholder('Button Icon'),
-            ])->columns(3);
+                Section::make('Core: CTA Module')
+                    ->description('This module is composed of a title, image, content, button text, button URL, and button icon.')
+                    ->icon('heroicon-o-cube')
+                    ->collapsed()
+                    ->schema([
+                        TextInput::make('title')
+                            ->label('Title')
+                            ->required()
+                            ->columnSpanFull()
+                            ->placeholder('Title'),
+                        FileUpload::make('image')
+                            ->columnSpanFull()
+                            ->label('Image')
+                            ->required()
+                            ->imageEditor()
+                            ->directory('images/content')
+                            ->moveFiles()
+                            ->image(),
+                        RichEditor::make('content')
+                            ->columnSpanFull()
+                            ->label('Content')
+                            ->required()
+                            ->placeholder('Content'),
+                        TextInput::make('button_text')
+                            ->label('Button Text')
+                            ->required()
+                            ->placeholder('Button Text'),
+                        TextInput::make('button_url')
+                            ->label('Button URL')
+                            ->default('#')
+                            ->required()
+                            ->placeholder('Button URL'),
+                        TextInput::make('button_icon')
+                            ->label('Button Icon')
+                            ->placeholder('Button Icon'),
+                    ])->columns(3),
+            ]);
     }
 
     public static function mutateData(array $data): array
