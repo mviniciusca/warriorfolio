741bc9c4fa6be4c9a3d8ed205f43182376eb4ccc
Marcos Coelho <mviniciusca@gmail.com>
2024-02-02 11:45:09 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/design/blur-background.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/design/blur-background.blade.php
@@ -1,3 +1,5 @@
 @aware(['page'])
-<div id="bg-footer" class="absolute -z-10 -mt-1 h-44 w-full animate-pulse bg-contain bg-top bg-no-repeat"
-    style="background-image: url('img/core/bg/separator-blur-md-bg.png')"></div>
+
+<div class="absolute -z-10 h-44 w-full animate-pulse bg-contain bg-top bg-no-repeat"
+    style="background-image: url('img/core/bg/separator-blur-md-bg.png')">
+</div>
