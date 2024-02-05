9d0009cb7ddb207a2a0ee8b0cfb171133bf7827c
Marcos Coelho <mviniciusca@gmail.com>
2024-02-05 01:43:02 -0300

    wip

--- resources/views/components/header/alerts.blade.php
+++ resources/views/components/header/alerts.blade.php
@@ -1,15 +1,10 @@
 @props(['maintenance' => null, 'discovery' => null])
 
 {{-- Messaging Hub --}}
-
 @foreach ($alerts as $alert)
 <livewire:alert wire:key="$alert->id" :style='$alert->style' :id="$alert->title" :message="$alert->message"
     :is_active="$alert->is_active" :is_dismissible="$alert->is_dismissible" />
 @endforeach
-
-
-
-
 
 
 {{-- Core Message : Discovery Mode Alert --}}
