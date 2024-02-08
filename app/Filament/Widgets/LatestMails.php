25ae9a9bcbfea876a35955abff14d63bc7371a0c
Marcos Coelho <mviniciusca@gmail.com>
2024-02-08 12:33:42 -0300

    wip

---
+++ app/Filament/Widgets/LatestMails.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Mail;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestMails extends BaseWidget
+{
+    protected static ?int $sort = 1;
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Mail::query()
+                    ->latest()
+                    ->limit(5)
+            )
+            ->paginated(false)
+            ->searchable(false)
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('Name')),
+                TextColumn::make('subject')
+                    ->label(__('Subject')),
+            ]);
+    }
+}
---
+++ app/Filament/Widgets/SystemStatus.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Maintenance;
+use App\Models\Setting;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class SystemStatus extends BaseWidget
+{
+    protected static ?int $sort = 2;
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Maintenance::query()->select())
+            ->paginated(false)
+            ->columns([
+                IconColumn::make('is_discovery')
+                    ->label(__('Discovery Mode'))
+                    ->alignCenter()
+                    ->trueColor('success')
+                    ->falseColor('gray')
+                    ->icon('heroicon-o-globe-alt')
+                    ->boolean(),
+                IconColumn::make('is_active')
+                    ->label(__('Maintenance Mode'))
+                    ->alignCenter()
+                    ->trueColor('success')
+                    ->falseColor('gray')
+                    ->falseIcon('heroicon-o-server')
+                    ->trueIcon('heroicon-o-wrench')
+                    ->boolean(),
+            ])->contentGrid([
+                    'md' => 3,
+                    'lg' => 4
+                ]);
+    }
+}
