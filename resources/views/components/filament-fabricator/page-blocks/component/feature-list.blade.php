c75cb10b984dba64b118d9bb891866b4351cad7f
Marcos Coelho <mviniciusca@gmail.com>
2024-02-17 13:42:55 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -3,19 +3,18 @@
 
 @if($is_active)
 <section>
-    <div class="mx-auto max-w-7xl">
+    <div class="mx-auto max-w-7xl px-5">
         <div class="mx-auto max-w-screen-xl py-4">
             <div
-                class="space-y-8 {{ $is_center ? 'text-center' : 'text-left' }} md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+                class="space-y-8 {{ $is_center ? 'text-center' : 'text-left' }} md:grid md:grid-cols-3 md:gap-12 md:space-y-0 lg:grid-cols-3">
                 @foreach ($features as $item)
                 <div>
-                    <div class="mx-auto mb-4 flex {{ $is_center ? 'items-center' : 'items-start' }}
+                    <div class="mx-auto mb-2 flex {{ $is_center ? 'items-center' : 'items-start' }}
                             {{ $is_center ? 'justify-center' : 'justify-normal' }}">
-                        <ion-icon class="h-9 w-9" name="{{ $item['icon'] }}">
-                        </ion-icon>
+                        <ion-icon class="h-10 w-10" name="{{ $item['icon'] }}"></ion-icon>
                     </div>
-                    <h3 class="mb-4 text-lg font-bold">{{ $item['title'] }}</h3>
-                    <p class="text-md opacity-70">
+                    <h3 class="mb-2 text-lg font-bold">{{ $item['title'] }}</h3>
+                    <p class="text-sm opacity-80">
                         {{ $item['description'] }}
                     </p>
                 </div>
