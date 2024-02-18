e1c8c8c81c4b1ca207cc5095a08aff339c470173
Marcos Coelho <mviniciusca@gmail.com>
2024-02-17 21:54:13 -0300

    wip

--- resources/views/components/header/navigation.blade.php
+++ resources/views/components/header/navigation.blade.php
@@ -1,6 +1,7 @@
 @props(['navigation' => null])
 
-<nav class="flex flex-wrap items-center">
+<nav
+    class="absolute z-50 min-w-36 max-w-sm items-center rounded-md border border-secondary-500 border-opacity-15 bg-white p-4 dark:border-opacity-20 dark:bg-secondary-800 lg:flex lg:w-auto lg:flex-wrap lg:rounded-none lg:border-none lg:bg-transparent lg:p-0 lg:shadow-none dark:lg:bg-transparent">
     @foreach ($navigation as $index => $key )
     @if($key['is_active'])
     <x-ui.nav-link :text="$key['name']" :href="$key['url']" :target="$key['target']" />
--- resources/views/components/ui/nav-link.blade.php
+++ resources/views/components/ui/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 <a href="{{ $href }}" target="{{ $target }}">
     <button
-        class="mx-2 flex cursor-pointer flex-wrap items-center gap-1 text-sm font-bold transition-all duration-100 hover:border-b-4 hover:border-b-primary-600 hover:py-1 hover:font-bold hover:text-primary-600 hover:opacity-95 active:opacity-50 dark:hover:text-primary-500">
+        class="flex cursor-pointer flex-wrap items-center gap-1 py-2 text-sm font-bold transition-all duration-100 hover:border-b-4 hover:border-b-primary-600 hover:font-bold hover:text-primary-600 hover:opacity-95 active:opacity-50 dark:hover:text-primary-500 lg:mx-2 lg:py-0">
         {{ $text }}
         @if($target == '_blank')
         <ion-icon class="h-5 w-5" name="trending-up-outline"></ion-icon>
