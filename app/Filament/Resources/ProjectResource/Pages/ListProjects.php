5a3181c0fc3ae7f9174e77c2e1e602e426db3380
Marcos Coelho <mviniciusca@gmail.com>
2024-11-12 00:10:12 -0300

    wip

--- app/Filament/Resources/ProjectResource/Pages/ListProjects.php
+++ app/Filament/Resources/ProjectResource/Pages/ListProjects.php
@@ -13,7 +13,9 @@ class ListProjects extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label(__('New Project'))
+                ->icon('heroicon-o-rocket-launch'),
         ];
     }
 }
