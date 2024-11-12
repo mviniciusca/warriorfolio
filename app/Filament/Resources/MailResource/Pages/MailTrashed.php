9b47572804ad8891ffa4a483a641ebcde8358f42
Marcos Coelho <mviniciusca@gmail.com>
2024-11-12 19:21:43 -0300

    wip

--- app/Filament/Resources/MailResource/Pages/MailTrashed.php
+++ app/Filament/Resources/MailResource/Pages/MailTrashed.php
@@ -6,16 +6,70 @@ use App\Filament\Resources\MailResource;
 use App\Models\Mail;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
+use Illuminate\Contracts\Support\Htmlable;
 
 class MailTrashed extends ListRecords
 {
     protected static string $resource = MailResource::class;
 
+    protected static ?string $navigationIcon = 'heroicon-o-trash';
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Trashed Mails');
+    }
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Trashed Mails');
+    }
+
     public function table(Table $table): Table
     {
         return $table
             ->query(Mail::query()->onlyTrashed())
-            ->columns([]);
+            ->columns([
+                IconColumn::make('is_important')
+                    ->label('')
+                    ->boolean()
+                    ->trueIcon('heroicon-s-star')
+                    ->falseIcon('heroicon-o-star')
+                    ->falseColor('gray')
+                    ->trueColor('warning'),
+                ToggleColumn::make('is_read')
+                    ->alignCenter()
+                    ->label(__('Mark as Read')),
+                TextColumn::make('name')
+                    ->label(__('From'))
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label(__('Email'))
+                    ->searchable(),
+                TextColumn::make('subject')
+                    ->label(__('Subject'))
+                    ->words(5)
+                    ->searchable(),
+            ])
+            ->actions([
+                ActionGroup::make([
+                    ForceDeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    ForceDeleteBulkAction::make(),
+                ]),
+            ]);
     }
 }
--- app/Filament/Resources/NewsletterResource/Pages/ListNewsletters.php
+++ app/Filament/Resources/NewsletterResource/Pages/ListNewsletters.php
@@ -5,16 +5,21 @@ namespace App\Filament\Resources\NewsletterResource\Pages;
 use App\Filament\Resources\NewsletterResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListNewsletters extends ListRecords
 {
     protected static string $resource = NewsletterResource::class;
-    protected static ?string $title = 'Newsletter Subscribers';
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Newsletter Subscribers');
+    }
 
     public function getHeaderWidgetsColumns(): int|array
     {
         return [
-            'sm' => 1
+            'sm' => 1,
         ];
     }
 
