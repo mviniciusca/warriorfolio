2d6119e6b7c6470d925f1af9c98971fde25c109d
Marcos Coelho <mviniciusca@gmail.com>
2024-11-12 18:44:52 -0300

    wip

--- app/Filament/Resources/MailResource.php
+++ app/Filament/Resources/MailResource.php
@@ -2,35 +2,34 @@
 
 namespace App\Filament\Resources;
 
-use Filament\Forms;
-use App\Models\Mail;
-use Filament\Forms\Components\Group;
-use Filament\Tables;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Filament\Resources\Resource;
-use Filament\Resources\Pages\Page;
-use Filament\Forms\Components\Section;
-use Filament\Tables\Actions\ActionGroup;
-use Illuminate\Database\Eloquent\Builder;
-use Filament\Tables\Filters\TernaryFilter;
 use App\Filament\Resources\MailResource\Pages;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\MailResource\RelationManagers;
+use App\Models\Mail;
+use Filament\Forms;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Table;
 
 class MailResource extends Resource
 {
     protected static ?string $model = Mail::class;
+
     protected static ?string $navigationIcon = 'heroicon-o-inbox-stack';
+
     public static function getNavigationLabel(): string
     {
         return __('Mail');
     }
+
     public static function getNavigationGroup(): ?string
     {
         return __('Core Features');
     }
+
     public static function getNavigationBadge(): ?string
     {
         if (static::getModel()::where('is_read', false)->count() > 0) {
@@ -40,8 +39,10 @@ class MailResource extends Resource
                 return static::getModel()::where('is_read', false)->count();
             }
         }
+
         return null;
     }
+
     public static function form(Form $form): Form
     {
         return $form
@@ -82,6 +83,7 @@ class MailResource extends Resource
                     ->maxLength(65535),
             ]);
     }
+
     public static function table(Table $table): Table
     {
         return $table
@@ -127,7 +129,7 @@ class MailResource extends Resource
                 ActionGroup::make([
                     Tables\Actions\EditAction::make(),
                     Tables\Actions\DeleteAction::make(),
-                ])
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
