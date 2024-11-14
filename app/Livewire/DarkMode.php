5a39e8c389c4ce7fdb14aa2530df35b62cf41476
Marcos Coelho <mviniciusca@gmail.com>
2024-11-14 17:32:35 -0300

    wip

--- app/Livewire/DarkMode.php
+++ app/Livewire/DarkMode.php
@@ -2,34 +2,37 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class DarkMode extends Component
 {
     public $active;
+
     public $theme;
 
-    public function mount()
+    public function mount(): void
     {
         $this->active = isset($_COOKIE['theme']) && $_COOKIE['theme'] === 'dark';
         $this->theme = $this->active ? 'dark' : 'light';
     }
-    public function render()
+
+    public function render(): View
     {
         return view('livewire.dark-mode', [
             'darkMode' => $this->toggleDarkMode(),
         ]);
     }
 
-    public function toggleDarkMode()
+    public function toggleDarkMode(): void
     {
         if ($this->active) {
-            setcookie('theme', 'dark', time() + (86400 * 30), "/");
+            setcookie('theme', 'dark', time() + (86400 * 30), '/');
             session(['theme' => 'dark']);
             $this->js("document.documentElement.classList.add('dark')");
             $this->theme = 'light';
         } else {
-            setcookie('theme', 'light', time() + (86400 * 30), "/");
+            setcookie('theme', 'light', time() + (86400 * 30), '/');
             session(['theme' => 'light']);
             $this->js("document.documentElement.classList.remove('dark')");
             $this->theme = 'dark';
