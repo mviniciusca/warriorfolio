01763dfa6fecd9c90540c6208a28c7131d5b34bb
Marcos Coelho <mviniciusca@gmail.com>
2024-11-14 16:18:44 -0300

    wip

--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -2,19 +2,10 @@
 
 <div class="inline-flex">
     <a href="https://{{ $href ?? '#' }}" target="_blank">
-        @if($name != 'devto')
+        @if($name)
         <ion-icon name="{{ 'logo-' . $name }}"
-            class="mr-4 cursor-pointer text-2xl opacity-60 transition-all duration-100 hover:opacity-90">
+            class="mr-4 cursor-pointer text-3xl opacity-60 transition-all duration-100 hover:opacity-90">
         </ion-icon>
-        @else
-        <svg class="mr-4 h-6 w-6 cursor-pointer fill-secondary-500 text-2xl opacity-60 transition-all duration-100 hover:opacity-90 dark:fill-secondary-50"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-            <path
-                d="M120.1 208.3c-3.9-2.9-7.8-4.4-11.7-4.4H91v104.5h17.5c3.9 0 7.8-1.5 11.7-4.4 3.9-2.9 5.8-7.3 5.8-13.1v-69.7c0-5.8-2-10.2-5.8-13.1zM404.1 32H43.9C19.7 32 .1 51.6 0 75.8v360.4C.1 460.4 19.7 480 43.9 480h360.2c24.2 0 43.8-19.6 43.9-43.8V75.8c-.1-24.2-19.7-43.8-43.9-43.8zM154.2 291.2c0 18.8-11.6 47.3-48.4 47.3h-46.4V173h47.4c35.4 0 47.4 28.5 47.4 47.3l0 70.9zm100.7-88.7H201.6v38.4h32.6v29.6H201.6v38.4h53.3v29.6h-62.2c-11.2 .3-20.4-8.5-20.7-19.7V193.7c-.3-11.2 8.6-20.4 19.7-20.7h63.2l0 29.5zm103.6 115.3c-13.2 30.8-36.9 24.6-47.4 0l-38.5-144.8h32.6l29.7 113.7 29.6-113.7h32.6l-38.5 144.8z" />
-        </svg>
-
-        @endif
     </a>
-
+    @endif
 </div>
