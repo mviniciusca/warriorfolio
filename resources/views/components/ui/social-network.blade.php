7a84758a557200ab5c9aa87849c536222c27c69b
Marcos Coelho <mviniciusca@gmail.com>
2024-11-14 16:13:53 -0300

    wip

--- resources/views/components/ui/social-network.blade.php
+++ resources/views/components/ui/social-network.blade.php
@@ -1,40 +1,10 @@
 <div>
 
     @foreach ($items as $social )
+    @if($$social['is_active'])
     <x-ui.icon :href="$social['profile_link']" :name="$social['social_network']" />
+    @endif
     @endforeach
-
-
-    {{-- @if($social->twitter)
-    <x-ui.icon :href="$social->twitter" name="logo-twitter" />
-    @endif
-    @if($social->linkedin)
-    <x-ui.icon :href="$social->linkedin" name="logo-linkedin" />
-    @endif
-    @if($social->facebook)
-    <x-ui.icon :href="$social->facebook" name="logo-facebook" />
-    @endif
-    @if($social->dribbble)
-    <x-ui.icon :href="$social->dribbble" name="logo-dribbble" />
-    @endif
-    @if($social->github)
-    <x-ui.icon :href="$social->github" name="logo-github" />
-    @endif
-    @if($social->instagram)
-    <x-ui.icon :href="$social->instagram" name="logo-instagram" />
-    @endif
-    @if($social->twitch)
-    <x-ui.icon :href="$social->twitch" name="logo-twitch" />
-    @endif
-    @if($social->youtube)
-    <x-ui.icon :href="$social->youtube" name="logo-youtube" />
-    @endif
-    @if($social->medium)
-    <x-ui.icon :href="$social->medium" name="logo-medium" />
-    @endif
-    @if($social->devto)
-    <x-ui.icon :href="$social->devto" name="devto" />
-    @endif --}}
 
     {{-- Empty Section --}}
     {{-- @if(empty($social->twitter) && empty($social->linkedin) && empty($social->facebook) && empty($social->dribbble)
