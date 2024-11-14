c9d6df6aa0e628bbfaec56ed884cf8f5b787b3c7
Marcos Coelho <mviniciusca@gmail.com>
2024-11-14 16:17:20 -0300

    wip

--- resources/views/components/ui/social-network.blade.php
+++ resources/views/components/ui/social-network.blade.php
@@ -1,17 +1,14 @@
 <div>
 
     @foreach ($items as $social )
-    @if($$social['is_active'])
+    @if($social['is_active'])
     <x-ui.icon :href="$social['profile_link']" :name="$social['social_network']" />
     @endif
     @endforeach
 
     {{-- Empty Section --}}
-    {{-- @if(empty($social->twitter) && empty($social->linkedin) && empty($social->facebook) && empty($social->dribbble)
-    &&
-    empty($social->github) && empty($social->instagram) && empty($social->twitch) && empty($social->youtube) &&
-    empty($social->medium) && empty($social->devto))
-    <x-ui.empty-section :auth="'Go to your Dashboard and update your Social Network.'" />
-    @endif --}}
+    @if($items == null)
+    <x-ui.empty-section :auth="__('Go to your Dashboard and update your Social Network.')" />
+    @endif
 
 </div>
