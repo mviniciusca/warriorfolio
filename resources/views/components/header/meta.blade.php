54bd52d31c01daf93e8e225eb460c273aa1c2193
Marcos Coelho <mviniciusca@gmail.com>
2024-11-14 21:42:59 -0300

    wip

--- resources/views/components/header/meta.blade.php
+++ resources/views/components/header/meta.blade.php
@@ -1,2 +1,26 @@
 <title>{{ $app['name'] . ' - ' . $meta['meta_title'] }}</title>
-<meta name="description" content="{{ $meta['meta_description'] }}">
+
+@if($meta !== null)
+@if($meta['meta_author'])
+<meta name="author" content="{{ $meta['meta_author'] }}">
+@endif
+<meta name="keywords" content="{{ $meta['meta_keywords'] ? $meta['meta_keywords'] : __('Warriorfolio v2') }}">
+<meta name="description"
+    content="{{ $meta['meta_description'] ? $meta['meta_description'] : __('Build the what\'s next!') }}">
+@endif
+
+@if($design !== null)
+<link rel="icon"
+    href="{{ $design['favicon'] ?  asset('storage/' . $design['favicon']) : asset('img/core/favicon.png')  }}"
+    type="image/x-icon">
+@endif
+
+@if($google !== null)
+<meta name="google-site-verification" content="{{ $google['meta_google_site_verification'] }}" />
+<meta name="robots" content="{{ $meta['meta_robots'] }}" />
+{!! $google['google_analytics'] !!}
+@endif
+
+@if($scripts !== null)
+{!! $scripts['header_scripts'] !!}
+@endif
