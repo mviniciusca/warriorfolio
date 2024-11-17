d6e850d5f903ac8c1e2cb4c0ed5e98959edb6326
Marcos Coelho <mviniciusca@gmail.com>
2024-11-17 16:24:06 -0300

    wip

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -1,9 +1,16 @@
 @isset($design['background_image_visibility'])
 
-<div id="default-background"
-    class="{{ $design['background_image'] ? '' : 'animate-pulse' }} w-full h-[1200px] absolute -z-50 -mt-36 {{ $design['animation'] ? 'animate-opacity opacity-30' : null }}
- {{ $design['background_image_size'] . ' ' . $design['background_image_position'] . ' ' . $design['background_image_repeat'] }}"
+<div id="default-background" class="
+w-full h-[1000px] absolute -z-50 -mt-36
+{{ $design['background_image'] ? '' : 'animate-pulse' }}
+{{ $design['animation'] ? 'animate-opacity opacity-10' : null }}
+{{ $design['background_image_size'] . ' ' . 
+        $design['background_image_position'] . ' ' . $design['background_image_repeat'] }}"
     style="background-image: url('{{ asset('storage/' . $design['background_image']) }}')">
+</div>
+<div id="bg-grip" class="gripper absolute w-full h-[1000px]">
+    <img class="dark:hidden w-full mt-36" src="{{ asset('img/core/bg/bg-grip.png') }}" />
+    <img class="dark:block w-full mt-36" src="{{ asset('img/core/bg/bg-grip-dark.png') }}" />
 </div>
 
 @endif
