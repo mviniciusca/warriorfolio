55995c7f926e0e750f6d00192ec544de46bbe203
Marcos Coelho <mviniciusca@gmail.com>
2024-11-17 01:41:44 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditClientSection.php
+++ app/Filament/Resources/SettingResource/Pages/EditClientSection.php
@@ -2,56 +2,73 @@
 
 namespace App\Filament\Resources\SettingResource\Pages;
 
+use App\Filament\Resources\SettingResource;
 use Filament\Actions;
-use Filament\Forms\Form;
 use Filament\Forms\Components\Group;
-use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
-use App\Filament\Resources\SettingResource;
+use Illuminate\Contracts\Support\Htmlable;
 
 class EditClientSection extends EditRecord
 {
     protected static string $resource = SettingResource::class;
+
     protected static ?string $navigationIcon = 'heroicon-o-building-office';
+
     public static function getNavigationLabel(): string
     {
-        return __('Clients Section');
+        return __('Customers Section');
     }
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Customers Section');
+    }
+
+    public function getSubheading(): string | Htmlable | null
+    {
+        return __('Define your Customers section here.');
+    }
+
     public function form(Form $form): Form
     {
         return $form->schema([
             Section::make()
                 ->columns(2)
                 ->schema([
-                    Group::make([
-                        Toggle::make('clients')
-                            ->label('Show Clients Section')
-                            ->helperText('Show or hide the clients section on the website.'),
-                    ])->relationship('module'),
+                    Group::make()
+                        ->relationship('module')
+                        ->schema([
+                            Toggle::make('clients')
+                                ->label(__('Show Customers Section'))
+                                ->helperText(__('Show or hide the Customers section on the website.')),
+                        ]),
                 ]),
-            Section::make('Clients Section')
+            Section::make(__('Customers Section'))
                 ->relationship('layout')
-                ->description('This section is used to display your clients to the public.')
+                ->description(__('This section is used to display your Customers to the public.'))
                 ->icon('heroicon-o-building-office')
+                ->columns(2)
                 ->schema([
                     Toggle::make('clients_section_fill')
-                        ->label('Fill Section Background')
-                        ->helperText('Fill the background of this section with a secondary default color.'),
+                        ->label(__('Fill Section Background'))
+                        ->helperText(__('Fill the background of this section with a secondary default color.')),
                     Textarea::make('clients_section_title')
-                        ->label('Clients Section Text')
-                        ->helperText('HTML allowed. Use the class text-highlight to highlight a word in the text')
+                        ->label(__('Customers Section Text'))
+                        ->helperText(__('HTML allowed. Use the class text-highlight to highlight a word in the text'))
                         ->columnSpanFull()
                         ->rows(3)
                         ->maxLength(255),
                     Textarea::make('clients_section_subtitle_text')
-                        ->label('Clients Section Subtitle')
-                        ->helperText('you also can use the class text-highlight to highlight a word in the subtitle')
+                        ->label(__('Customers Section Subtitle'))
+                        ->helperText(__('You also can use the class text-highlight to highlight a word in the subtitle'))
                         ->columnSpanFull()
                         ->rows(3)
                         ->maxLength(255),
-                ])->columns(2),
+                ]),
         ]);
     }
 }
