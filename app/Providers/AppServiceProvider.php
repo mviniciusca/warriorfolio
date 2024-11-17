929425a768d21a8c09ea34540f0a3ceb2c481a97
Marcos Coelho <mviniciusca@gmail.com>
2024-11-17 17:47:01 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,40 +25,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Schema::hasTable('maintenances')) {
-            $maintenance = Maintenance::query()
-                ->select(['is_active', 'is_discovery'])
-                ->first();
-            view()->share([
-                'discovery'   => $maintenance->is_discovery,
-                'maintenance' => $maintenance->is_active,
-            ]);
+        $maintenance = optional(Maintenance::first(['is_active', 'is_discovery']));
+        view()->share([
+            'discovery'   => $maintenance->is_discovery ?? false,
+            'maintenance' => $maintenance->is_active ?? false,
+        ]);
 
-            if (Schema::hasTable('cores')) {
-                $data = Core::query()
-                    ->select(
-                        [
-                            'about',
-                            'clients',
-                            'contact',
-                            'footer',
-                            'header',
-                            'hero',
-                            'newsletter',
-                            'portfolio',
-                        ]
-                    )->first();
-                view()->share([
-                    'about_core'      => $data->about,
-                    'clients_core'    => $data->clients,
-                    'contact_core'    => $data->contact,
-                    'footer_core'     => $data->footer,
-                    'header_core'     => $data->header,
-                    'hero_core'       => $data->hero,
-                    'newsletter_core' => $data->newsletter,
-                    'portfolio_core'  => $data->portfolio,
-                ]);
-            }
+        if (Schema::hasTable('cores')) {
+            $data = Core::first([
+                'about',
+                'clients',
+                'contact',
+                'footer',
+                'header',
+                'hero',
+                'newsletter',
+                'portfolio',
+            ]);
+            view()->share([
+                'about_core'      => $data->about,
+                'clients_core'    => $data->clients,
+                'contact_core'    => $data->contact,
+                'footer_core'     => $data->footer,
+                'header_core'     => $data->header,
+                'hero_core'       => $data->hero,
+                'newsletter_core' => $data->newsletter,
+                'portfolio_core'  => $data->portfolio,
+            ]);
         }
     }
 }
