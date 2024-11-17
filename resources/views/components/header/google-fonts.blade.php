a94350979ae6ebbd04ea3ca1507fc81fb6f56de7
Marcos Coelho <mviniciusca@gmail.com>
2024-11-17 00:39:00 -0300

    wip

--- resources/views/components/header/google-fonts.blade.php
+++ resources/views/components/header/google-fonts.blade.php
@@ -1,18 +1,19 @@
-@if($fonts['google']['fonts_code'] && $fonts['google']['font_name'])
-{!! $fonts['google']['fonts_code'] !!}
+@if(data_get($fonts, 'google.fonts_code') && data_get($fonts, 'google.font_name'))
+{!! data_get($fonts, 'google.fonts_code') !!}
 <style>
     body {
-        font-family: "{{ $fonts['google']['font_name'] }}",
+        font-family: "{{ data_get($fonts, 'google.font_name') }}",
         sans-serif;
     }
 </style>
 @else
-<link rel=" preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+<link   href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
 <style>
     body {
         font-family: 'Inter', sans-serif;
     }
-</style>
+</style>  
+
 @endif
