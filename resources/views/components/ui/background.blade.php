293f3f232ac910760cdef99fe59fa9a1e86e1e17
Marcos Coelho <mviniciusca@gmail.com>
2024-11-16 23:27:19 -0300

    wip

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -1,7 +1,7 @@
 @isset($design['background_image_visibility'])
 
 <div id="default-background"
-    class="{{ $design['background_image'] ? '' : 'animate-pulse' }} w-full h-[1200px] absolute -z-50 -mt-36 {{ $design['animation'] ? 'animate-opacity opacity-5' : null }}
+    class="{{ $design['background_image'] ? '' : 'animate-pulse' }} w-full h-[1200px] absolute -z-50 -mt-36 {{ $design['animation'] ? 'animate-opacity opacity-30' : null }}
  {{ $design['background_image_size'] . ' ' . $design['background_image_position'] . ' ' . $design['background_image_repeat'] }}"
     style="background-image: url('{{ asset('storage/' . $design['background_image']) }}')">
 </div>
