063ace79897434a2970b2555c88bc2e5888bbb2f
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 06:20:32 -0300

    wip

--- resources/views/components/themes/hero/default-theme.blade.php
+++ resources/views/components/themes/hero/default-theme.blade.php
@@ -1,52 +1,60 @@
 @props(['hero'])
 
-<div class="text-center">
-    @if($hero->hero['section_title'])
-    <h1
-        class="animate__animated animate__fadeInUp animate max-w-2xl mb-4 text-4xl font-extrabold tracking-tighter leading-none md:text-5xl xl:text-6xl dark:text-white">
-        {!! $hero->hero['section_title'] !!}
-    </h1>
-    @endif
+<section>
+    <div class="text-center">
+        @if($hero->hero['section_title'])
+        <h1
+            class="animate__animated animate__fadeInUp animate max-w-2xl mb-4 text-4xl font-extrabold tracking-tighter leading-none md:text-5xl xl:text-6xl dark:text-white">
+            {!! $hero->hero['section_title'] !!}
+        </h1>
+        @endif
 
-    @if($hero->hero['section_subtitle'])
-    <h4
-        class="animate__animated animate__fadeInUp animate__delay-1s mb-8 text-lg leading-tight tracking-tight shadow-current lg:text-xl">
-        {!! $hero->hero['section_subtitle'] !!}
-    </h4>
-    @endif
+        @if($hero->hero['section_subtitle'])
+        <h4
+            class="animate__animated animate__fadeInUp animate__delay-1s mb-8 text-lg leading-tight tracking-tight shadow-current lg:text-xl">
+            {!! $hero->hero['section_subtitle'] !!}
+        </h4>
+        @endif
 
-    {{-- Hero Section: Buttons --}}
-    <div class="animate__animated animate__fadeInUp animate__delay-2s z-10 flex justify-center gap-4">
+        {{-- Hero Section: Buttons --}}
+        <div class="animate__animated animate__fadeInUp animate__delay-2s z-10 flex justify-center gap-4">
 
-        {{-- Hero Section Button --}}
-        @foreach ($hero->hero['buttons'] as $button)
-        <a target="{{ $button['button_target'] ?? '_self' }}" href="{{ $button['button_url'] ?? '#' }}">
-            @if ($button['button_style'] == 'filled')
-            <x-ui.button class="px-5 py-3">
-                {{ $button['button_title'] }}
-                @if( $button['button_target'] == '_blank')
-                <ion-icon class="ml-1" name="trending-up-outline"></ion-icon>
+            {{-- Hero Section Button --}}
+            @foreach ($hero->hero['buttons'] as $button)
+            <a target="{{ $button['button_target'] ?? '_self' }}" href="{{ $button['button_url'] ?? '#' }}">
+                @if ($button['button_style'] == 'filled')
+                <x-ui.button class="px-5 py-3">
+                    {{ $button['button_title'] }}
+                    @if( $button['button_target'] == '_blank')
+                    <ion-icon class="ml-1" name="trending-up-outline"></ion-icon>
+                    @endif
+                </x-ui.button>
+                @else
+                <x-ui.button-alt class="px-5 py-3">
+                    {{ $button['button_title'] }}
+                    @if( $button['button_target'] == '_blank')
+                    <ion-icon class="ml-1" name="trending-up-outline"></ion-icon>
+                    @endif
+                </x-ui.button-alt>
                 @endif
-            </x-ui.button>
+            </a>
+            @endforeach
+        </div>
+    </div>
+    </div>
+
+    <div class="hidden animate__animated animate__fadeInUp lg:mt-0 lg:col-span-5 lg:flex">
+        {{-- Hero Section: Image --}}
+        @if(data_get($hero,'hero.featured_image'))
+        <div class="mt-8" id="hero-featured-image">
+            <img class="animate__animated animate__fadeInUp animate__delay-1s h-auto p-4 lg:max-h-max"
+                src="{{ asset('storage/' . $hero->hero['featured_image']) }}" alt="hero-section-image" />
             @else
-            <x-ui.button-alt class="px-5 py-3">
-                {{ $button['button_title'] }}
-                @if( $button['button_target'] == '_blank')
-                <ion-icon class="ml-1" name="trending-up-outline"></ion-icon>
-                @endif
-            </x-ui.button-alt>
+            <img class="animate__animated animate__fadeInUp animate__delay-1s p-4 lg:max-h-max h-auto"
+                src="{{ asset('img/core/demo/app-demo.png') }}" alt="hero-section-image" />
             @endif
-        </a>
-        @endforeach
+        </div>
+    </div>
     </div>
 
-    {{-- Hero Section: Image --}}
-    @isset($hero->hero['featured_image'])
-    <div class="mt-8 max-w-2xl justify-center mx-auto items-center hidden lg:flex" id="hero-featured-image">
-        <img class="animate__animated animate__fadeInUp animate__delay-2s h-auto p-4 lg:max-h-max"
-            src="{{ asset('storage/' . $hero->hero['featured_image']) }}" alt="hero-section-image" />
-    </div>
-    @endisset
-    {{-- End Hero Section: Image --}}
-</div>
-</div>
+</section>
--- resources/views/components/themes/hero/sierra-theme.blade.php
+++ resources/views/components/themes/hero/sierra-theme.blade.php
@@ -1,8 +1,8 @@
 @props(['hero'])
 
 <section>
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-3">
+        <div class="mr-auto place-self-center lg:col-span-1">
             <h1
                 class="animate__animated animate__fadeInUp animate max-w-2xl mb-4 text-4xl font-extrabold tracking-tighter leading-none md:text-5xl xl:text-6xl dark:text-white">
                 {!! $hero->hero['section_title'] !!}
@@ -36,14 +36,17 @@
             </div>
 
         </div>
-        <div class="hidden animate__animated animate__fadeInUp lg:mt-0 lg:col-span-5 lg:flex">
+        <div class="hidden animate__animated animate__fadeInUp  lg:mt-0 lg:col-span-2 rounded-lg lg:flex">
             {{-- Hero Section: Image --}}
-            @isset($hero->hero['featured_image'])
+            @if(data_get($hero,'hero.featured_image'))
             <div class="mt-8" id="hero-featured-image">
                 <img class="animate__animated animate__fadeInUp animate__delay-1s h-auto p-4 lg:max-h-max"
                     src="{{ asset('storage/' . $hero->hero['featured_image']) }}" alt="hero-section-image" />
+                @else
+                <img class="animate__animated animate__fadeInUp animate__delay-1s p-4 lg:max-h-max h-auto"
+                    src="{{ asset('img/core/demo/app-demo.png') }}" alt="hero-section-image" />
+                @endif
             </div>
-            @endisset
         </div>
     </div>
 </section>
