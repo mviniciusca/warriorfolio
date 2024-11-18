43271d3d9339523101601fa8fee60fce99458dc1
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 02:34:37 -0300

    wip

--- resources/views/components/contact/empty-address.blade.php
+++ resources/views/components/contact/empty-address.blade.php
@@ -2,9 +2,11 @@
 
 <x-ui.contact-icon />
 
-@if(data_get($data, 'contact.google_maps_code') == null &&
-data_get($data, 'contact.public_adress') == null &&
-data_get($data, 'contact.public_phone') == null &&
-data_get($data, 'contact.public_email') == null)
+@if(empty(array_filter([
+data_get($data, 'contact.google_maps_code'),
+data_get($data, 'contact.public_adress'),
+data_get($data, 'contact.public_phone'),
+data_get($data, 'contact.public_email')
+])))
 <x-ui.empty-section :auth="__('Go to your Dashboard and update your Contact Section.')" />
 @endif
