ddfdf099a65554a4eef427f211b8409f4bb7d4a7
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 04:01:25 -0300

    wip

--- database/migrations/2023_02_28_135313_create_projects_table.php
+++ database/migrations/2023_02_28_135313_create_projects_table.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -12,6 +13,7 @@ return new class () extends Migration {
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('image_cover');
@@ -19,7 +21,6 @@ return new class () extends Migration {
             $table->text('content')->nullable();
             $table->string('external_link')->nullable();
             $table->boolean('is_active')->default(true);
-            $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
