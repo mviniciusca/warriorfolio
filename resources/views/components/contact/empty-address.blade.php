b6676180b9006925d5cc97a85b4bd605f12b0b32
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 02:31:29 -0300

    wip

--- resources/views/components/contact/empty-address.blade.php
+++ resources/views/components/contact/empty-address.blade.php
@@ -1,8 +1,10 @@
-@props(['info'])
+@props(['data'])
 
 <x-ui.contact-icon />
 
-@if($info->contact_section_google_map == null && $info->contact_section_address == null &&
-$info->contact_section_phone == null && $info->contact_section_email == null)
-<x-ui.empty-section :auth="'Go to your Dashboard and update your Contact.'" />
+@if(data_get($data, 'contact.google_maps_code') == null &&
+data_get($data, 'contact.public_adress') == null &&
+data_get($data, 'contact.public_phone') == null &&
+data_get($data, 'contact.public_email') == null)
+<x-ui.empty-section :auth="__('Go to your Dashboard and update your Contact Section.')" />
 @endif
