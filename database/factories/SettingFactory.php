2d073e136046c9b62c9a7b99ba533b526694d8a9
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 11:28:36 -0300

    wip

--- database/factories/SettingFactory.php
+++ database/factories/SettingFactory.php
@@ -24,8 +24,8 @@ class SettingFactory extends Factory
             'meta' => [
                 'meta_title'       => 'Building the what\'s next with Laravel, Filament and Tailwind CSS.',
                 'meta_author'      => 'Marcos Coelho',
-                'meta_description' => 'Warriorfolio 2 is a free and open-source web application for creating and managing your own website.',
-                'meta_keywords'    => 'warriorfolio, warriorfolio 2, warriorfolio cms, warriorfolio cms 2, warriorfolio 2 cms',
+                'meta_description' => 'Warriorfolio is a free and open-source web application for creating and      managing your own portfolio website.',
+                'meta_keywords'    => 'warriorfolio, warriorfolio cms, open source cms, quick portfolio website',
             ],
             //
             'design' => [
