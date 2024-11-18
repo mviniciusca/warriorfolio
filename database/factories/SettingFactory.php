fdac40e5dc75afa2e5450c7a125b7a9b502ca1b8
Marcos Coelho <mviniciusca@gmail.com>
2024-11-18 11:27:13 -0300

    wip

--- database/factories/SettingFactory.php
+++ database/factories/SettingFactory.php
@@ -20,12 +20,21 @@ class SettingFactory extends Factory
             'application' => [
                 'name' => 'Warriorfolio v2.0.4',
             ],
-
+            //
             'meta' => [
                 'meta_title'       => 'Building the what\'s next with Laravel, Filament and Tailwind CSS.',
                 'meta_author'      => 'Marcos Coelho',
                 'meta_description' => 'Warriorfolio 2 is a free and open-source web application for creating and managing your own website.',
                 'meta_keywords'    => 'warriorfolio, warriorfolio 2, warriorfolio cms, warriorfolio cms 2, warriorfolio 2 cms',
+            ],
+            //
+            'design' => [
+                'animation'                   => true,
+                'background_image_visibility' => true,
+                'background_image_position'   => 'bg-center',
+                'background_image_size'       => 'bg-auto',
+                'background_image_repeat'     => 'bg-no-repeat',
+
             ],
         ];
     }
--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -1,5 +1,5 @@
-@isset($design['background_image_visibility'])
-
+@if(data_get($design,'background_image_visibility') === true)
+@if(data_get($design,'background_image'))
 <div id="default-background" class="
 w-full h-[1080px] absolute -z-50 -mt-36
 {{ $design['background_image'] ? '' : 'animate-pulse' }}
@@ -8,4 +8,10 @@ w-full h-[1080px] absolute -z-50 -mt-36
         $design['background_image_position'] . ' ' . $design['background_image_repeat'] }}"
     style="background-image: url('{{ asset('storage/' . $design['background_image']) }}')">
 </div>
+@else
+<div id="default-background"
+    class="w-full h-[1080px] animate-opacity opacity-10 absolute -z-50 -mt-36 bg-no-repeat bg-center bg-scroll bg-auto"
+    style="background-image: url('{{ asset('img/core/bg/astro-default-bg.png') }}')">
+</div>
+@endif
 @endif
