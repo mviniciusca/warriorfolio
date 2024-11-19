6a51d90abc585fc31e015f6d5db3d1c94c13f066
Marcos Coelho <mviniciusca@gmail.com>
2024-11-19 17:11:27 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Component/ButtonGroup.php
+++ app/Filament/Fabricator/PageBlocks/Component/ButtonGroup.php
@@ -18,45 +18,85 @@ class ButtonGroup extends PageBlock
             ->label('Button Group')
             ->icon('heroicon-o-square-2-stack')
             ->schema([
-                Section::make('Component: Buttons Group')->collapsed()->description('Add a button group in your application.')->icon('heroicon-o-square-2-stack')->schema([
-                    Toggle::make('is_center')->label('Group Buttons Centered')->columnSpanFull()->default(true),
-                    Fieldset::make('Primary Button (optional)')->columns(3)->schema([
-                        Toggle::make('is_active')->label('Show Button')->columnSpanFull()->default(true),
-                        TextInput::make('button_title')->label('Button Title')->helperText('Add a primary button title'),
-                        TextInput::make('button_url')->label('Button URL')->helperText('Add a URL'),
-                        TextInput::make('button_icon')->label('Button Icon (optional)')->helperText('Add a Ionicon icon name')->prefix('ion-icon'),
-                        Select::make('button_target')->label('Button Target')->options([
-                            '_self'  => 'Same Window',
-                            '_blank' => 'New Window',
-                        ])->default('_self'),
-                        Select::make('button_size')->label('Button Size')->options([
-                            'px-1 py-2' => 'Small',
-                            'py-2 px-4' => 'Medium',
-                            'py-3 px-5' => 'Large',
-                            'py-4 px-6' => 'Extra Large',
-                            'py-5 px-7' => 'Extra Extra Large',
-                        ])
-                            ->default('py-3 px-5'),
+                Section::make('Component: Buttons Group')
+                    ->collapsed()
+                    ->description('Add a button group in your application.')->icon('heroicon-o-square-2-stack')
+                    ->schema([
+                        Toggle::make('is_center')
+                            ->label('Group Buttons Centered')
+                            ->columnSpanFull()
+                            ->default(true),
+                        Fieldset::make('Primary Button (optional)')
+                            ->columns(3)
+                            ->schema([
+                                Toggle::make('is_active')
+                                    ->label('Show Button')
+                                    ->columnSpanFull()
+                                    ->default(true),
+                                TextInput::make('button_title')
+                                    ->label('Button Title')
+                                    ->helperText('Add a primary button title'),
+                                TextInput::make('button_url')
+                                    ->label('Button URL')
+                                    ->helperText('Add a URL'),
+                                TextInput::make('button_icon')
+                                    ->label('Button Icon (optional)')
+                                    ->helperText('Add a Ionicon icon name')
+                                    ->prefix('ion-icon'),
+                                Select::make('button_target')
+                                    ->label('Button Target')
+                                    ->options([
+                                        '_self'  => 'Same Window',
+                                        '_blank' => 'New Window',
+                                    ])
+                                    ->default('_self'),
+                                Select::make('button_size')
+                                    ->label('Button Size')
+                                    ->options([
+                                        'px-1 py-2' => 'Small',
+                                        'py-2 px-4' => 'Medium',
+                                        'py-3 px-5' => 'Large',
+                                        'py-4 px-6' => 'Extra Large',
+                                        'py-5 px-7' => 'Extra Extra Large',
+                                    ])
+                                    ->default('py-3 px-5'),
+                            ]),
+                        Fieldset::make('Secondary Button (optional)')
+                            ->columns(3)
+                            ->schema([
+                                Toggle::make('is_active_secondary')
+                                    ->label('Show Button')
+                                    ->columnSpanFull()
+                                    ->default(true),
+                                TextInput::make('button_title_secondary')
+                                    ->label('Button Title')
+                                    ->helperText('Add a secondary button title'),
+                                TextInput::make('button_url_secondary')
+                                    ->label('Button URL')
+                                    ->helperText('Add a URL'),
+                                TextInput::make('button_icon_secondary')
+                                    ->label('Button Icon (optional)')
+                                    ->helperText('Add a Ionicon icon name')
+                                    ->prefix('ion-icon'),
+                                Select::make('button_target_secondary')
+                                    ->label('Button Target')
+                                    ->options([
+                                        '_self'  => 'Same Window',
+                                        '_blank' => 'New Window',
+                                    ])
+                                    ->default('_self'),
+                                Select::make('button_size_secondary')
+                                    ->label('Button Size')
+                                    ->options([
+                                        'px-1 py-2' => 'Small',
+                                        'py-2 px-4' => 'Medium',
+                                        'py-3 px-5' => 'Large',
+                                        'py-4 px-6' => 'Extra Large',
+                                        'py-5 px-7' => 'Extra Extra Large',
+                                    ])
+                                    ->default('py-3 px-5'),
+                            ]),
                     ]),
-                    Fieldset::make('Secondary Button (optional)')->columns(3)->schema([
-                        Toggle::make('is_active_secondary')->label('Show Button')->columnSpanFull()->default(true),
-                        TextInput::make('button_title_secondary')->label('Button Title')->helperText('Add a secondary button title'),
-                        TextInput::make('button_url_secondary')->label('Button URL')->helperText('Add a URL'),
-                        TextInput::make('button_icon_secondary')->label('Button Icon (optional)')->helperText('Add a Ionicon icon name')->prefix('ion-icon'),
-                        Select::make('button_target_secondary')->label('Button Target')->options([
-                            '_self'  => 'Same Window',
-                            '_blank' => 'New Window',
-                        ])->default('_self'),
-                        Select::make('button_size_secondary')->label('Button Size')->options([
-                            'px-1 py-2' => 'Small',
-                            'py-2 px-4' => 'Medium',
-                            'py-3 px-5' => 'Large',
-                            'py-4 px-6' => 'Extra Large',
-                            'py-5 px-7' => 'Extra Extra Large',
-                        ])
-                            ->default('py-3 px-5'),
-                    ]),
-                ]),
             ]);
     }
 
