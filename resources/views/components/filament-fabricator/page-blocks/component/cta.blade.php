bfa0875dcd4fed45760e1f9044e42a6adb3673b5
Marcos Coelho <mviniciusca@gmail.com>
2024-11-20 11:00:18 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
@@ -2,7 +2,7 @@
 @props([ 'content' => null])
 
 
-<section class="">
+<section>
     @foreach ($content as $item )
 
     <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
