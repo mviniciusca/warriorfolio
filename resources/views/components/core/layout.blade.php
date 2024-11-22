68cbdd22c75b9812b106e70ea5cb219134b1a9f6
Marcos Coelho <mviniciusca@gmail.com>
2024-11-22 12:07:44 -0300

    wip

--- resources/views/components/core/layout.blade.php
+++ resources/views/components/core/layout.blade.php
@@ -5,7 +5,7 @@
     <div class="px-16 py-8 md:py-12">
         <div class="mx-auto max-w-7xl">
             <div class="header-title py-2">{{ $module_title }}</div>
-            <div class="subtitle mx-auto mt-2 max-w-5xl text-center">{{ $module_subtitle }}</div>
+            <div class="subtitle mx-auto mt-4 max-w-5xl text-center">{{ $module_subtitle }}</div>
             {{ $slot }}
         </div>
     </div>
