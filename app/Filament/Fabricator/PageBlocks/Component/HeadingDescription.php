8a496589b9bc6ccd87c9b8bc39fe337abe3e2c36
Marcos Coelho <mviniciusca@gmail.com>
2024-11-23 03:12:53 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Component/HeadingDescription.php
+++ app/Filament/Fabricator/PageBlocks/Component/HeadingDescription.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Fabricator\PageBlocks\Component;
 
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
@@ -25,35 +27,41 @@ class HeadingDescription extends PageBlock
                     ->collapsed()
                     ->columns(4)
                     ->schema([
-                        Toggle::make('is_active')
-                            ->label(__('Show Module'))
-                            ->inline(false)
-                            ->default(true),
-                        Toggle::make('is_center')
-                            ->label(__('Align to Center'))
-                            ->inline(false)
-                            ->default(true),
-                        Select::make('heading_text_size')
-                            ->label('Heading Text Size')
-                            ->options([
-                                'text-2xl' => __('Large 2XL'),
-                                'text-3xl' => __('Default 3XL'),
-                                'text-4xl' => __('Text 4XL'),
-                                'text-5xl' => __('Text 5XL'),
-                                'text-6xl' => __('Text 6XL'),
-                            ])
-                            ->default('text-3xl'),
-                        Select::make('content_text_size')
-                            ->label('Description Text Size')
-                            ->options([
-                                'text-xs'   => __('Extra Small'),
-                                'text-sm'   => __('Small'),
-                                'text-base' => __('Default'),
-                                'text-lg'   => __('Large'),
-                                'text-xl'   => __('Extra Large'),
-                                'text-2xl'  => __('Super Large'),
-                            ])
-                            ->default('text-base'),
+                        Group::make()
+                            ->columns(4)
+                            ->columnSpanFull()
+                            ->schema([
+                                Toggle::make('is_active')
+                                    ->label(__('Show Module'))
+                                    ->inline(false)
+                                    ->default(true),
+                                Toggle::make('is_center')
+                                    ->label(__('Align to Center'))
+                                    ->inline(false)
+                                    ->default(true),
+                                Select::make('heading_text_size')
+                                    ->label('Heading Text Size')
+                                    ->options([
+                                        'text-2xl' => __('Large 2XL'),
+                                        'text-3xl' => __('Default 3XL'),
+                                        'text-4xl' => __('Text 4XL'),
+                                        'text-5xl' => __('Text 5XL'),
+                                        'text-6xl' => __('Text 6XL'),
+                                    ])
+                                    ->default('text-3xl'),
+                                Select::make('content_text_size')
+                                    ->label('Description Text Size')
+                                    ->options([
+                                        'text-xs'   => __('Extra Small'),
+                                        'text-sm'   => __('Small'),
+                                        'text-base' => __('Default'),
+                                        'text-lg'   => __('Large'),
+                                        'text-xl'   => __('Extra Large'),
+                                        'text-2xl'  => __('Super Large'),
+                                    ])
+                                    ->default('text-base'),
+                            ]),
+
                         Textarea::make('heading')
                             ->label(__('Heading Title'))
                             ->helperText(__('HTML Allowed. Use class "tl" to highlight a word.'))
@@ -66,6 +74,7 @@ class HeadingDescription extends PageBlock
                             ->helperText(__('Short description or main content.'))
                             ->columnSpanFull()
                             ->maxLength(6000),
+                        FileUpload::make('image'),
                     ]),
             ]);
     }
--- resources/views/components/filament-fabricator/page-blocks/component/heading-description.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/heading-description.blade.php
@@ -1,25 +1,31 @@
 @aware(['page'])
 @props(['is_active' => true, 'heading' => null, 'content' => null, 'is_center' => false , 'heading_text_size' =>
-'text-3xl', 'content_text_size' => 'text-xl'])
+'text-3xl', 'content_text_size' => 'text-xl', 'image' => null])
 
 @if($is_active)
 <x-core.layout>
     <div class="mx-auto max-w-screen-xl {{ $is_center ? 'text-center':'text-left' }}">
         <div id="heading-component">
+            <div class="grid grid-cols-12 items-center">
+                <div class="{{ $image ? 'col-span-6' : 'col-span-full' }}"> @if($heading)
+                    <h2 class="heading-module-title mb-4 font-bold tracking-tighter {{ $heading_text_size }}">
+                        {!! $heading !!}
+                    </h2>
+                    @endif
 
-            @if($heading)
-            <h2 class="heading-module-title mb-4 font-bold tracking-tighter {{ $heading_text_size }}">
-                {!! $heading !!}
-            </h2>
-            @endif
+                    @if($content)
+                    <div class="heading-module-subtitle mb-4 font-light {{ $content_text_size }}">
+                        {!! $content !!}
+                    </div>
+                    @endif
+                </div>
+                @if($image)
+                <div class="col-span-6">
+                    <img class="max-w-lg" src="{{ asset('storage/' . $image) }}" </div>
+                </div>
+                @endif
 
-            @if($content)
-            <div class="heading-module-subtitle mb-4 font-light {{ $content_text_size }}">
-                {!! $content !!}
             </div>
-            @endif
-
         </div>
-    </div>
 </x-core.layout>
 @endif
