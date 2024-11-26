8782468262c6b54352fb34bd4020cf8005a5ad6a
Marcos Coelho <mviniciusca@gmail.com>
2024-11-26 08:58:21 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -6,20 +6,20 @@
 <x-core.layout>
     <section>
         <div class="mx-auto pb-8">
-            <div class="{{ $is_center ? 'text-center' : 'text-left' }} grid md:grid-cols-3 lg:grid-cols-3 gap-2">
+            <div class="{{ $is_center ? 'text-center' : 'text-left' }} grid md:grid-cols-3 lg:grid-cols-3 gap-5">
                 @foreach ($features as $key => $item)
                 @if($link)
                 <a target="{{ $is_new_window ? '_blank' : '_self' }}" href="{{ $link }}">
                     @endif
                     <div
-                        class="icon-card p-8 min-h-60 opacity-90 hover:opacity-100 transition-all duration-50 
+                        class="icon-card p-8 rounded-lg min-h-60 opacity-90 hover:opacity-100 transition-all duration-50 
                         {{ $is_animated ? 'hover:-mt-5 hover:z-10' : '' }}
-                        {{ $is_filled ? ' dark:bg-secondary-950/50 hover:dark:bg-secondary-950' : '' }} 
-                        {{ $is_border ? 'border hover:border-secondary-500 dark:border-secondary-800 border-secondary-200' : '' }}">
+                        {{ $is_filled ? ' dark:bg-secondary-950/50 hover:dark:bg-secondary-950 bg-secondary-50/50 hover:bg-secondary-100' : '' }} 
+                        {{ $is_border ? 'border hover:border-secondary-400 dark:hover:border-secondary-700 dark:border-secondary-800 border-secondary-200' : '' }}">
                         <div class="mx-auto mb-2 flex 
                         {{ $is_center ? 'items-center justify-center' : 'items-start justify-normal' }}
                        ">
-                            <ion-icon class="h-10 w-10" name="{{ $item['icon'] }}"></ion-icon>
+                            <ion-icon class="h-10 w-10 lg:h-14 lg:w-14" name="{{ $item['icon'] }}"></ion-icon>
                         </div>
                         <h3 class="mb-2 text-base leading-tight">{!! $item['title'] !!}</h3>
                         <p class="text-sm opacity-80">
