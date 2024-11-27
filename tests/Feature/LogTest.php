ac6de6aba7a9c39a74651b1799555285bf63e21e
Marcos Coelho <mviniciusca@gmail.com>
2024-11-27 12:12:47 -0300

    wip

--- tests/Feature/LogTest.php
+++ tests/Feature/LogTest.php
@@ -9,9 +9,5 @@ use Tests\TestCase;
 
 class LogTest extends TestCase
 {
-    /** @test **/
-    public function it_should_allow_access(): void
-    {
-        $this->get('/admin/logs')->assertStatus(403);
-    }
+    //
 }
