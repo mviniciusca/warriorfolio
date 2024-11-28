f4ef7b8801eceaf5b6d3a69c914a0b904018afe8
Marcos Coelho <mviniciusca@gmail.com>
2024-11-28 11:56:30 -0300

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,6 +64,6 @@ class User extends Authenticatable implements FilamentUser
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with('false', false) && $this->hasVerifiedEmail();
+        return str_ends_with('this-email', false) && $this->hasVerifiedEmail();
     }
 }
