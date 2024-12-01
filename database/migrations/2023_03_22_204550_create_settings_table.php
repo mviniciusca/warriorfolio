49010c71665b023bb8d611288738059f854bbf5c
Marcos Coelho <mviniciusca@gmail.com>
2024-12-01 20:11:29 -0300

    wip

--- database/migrations/2023_03_22_204550_create_settings_table.php
+++ database/migrations/2023_03_22_204550_create_settings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,39 +13,14 @@ return new class extends Migration {
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->json('application')->nullable();
             $table->string('name')->nullable();
-
             $table->json('design')->nullable();
-            // $table->string('logo')->nullable();
-            // $table->string('logo_dark_mode')->nullable();
-            // $table->string('logo_size')->nullable();
-            // $table->string('favicon')->nullable();
-            // $table->string('background_image')->nullable();
-            // $table->boolean('background_image_visibility')->default(true);
-            // $table->string('background_image_position')->default('bg-center');
-            // $table->string('background_image_size')->default('bg-cover');
-            // $table->string('background_image_repeat')->default('bg-no-repeat');
             $table->boolean('dark_mode')->default(true);
-
             $table->json('meta')->nullable();
-            // $table->string('meta_title')->nullable();
-            // $table->string('meta_author')->nullable();
-            // $table->string('meta_description')->nullable();
-            // $table->string('meta_keywords')->nullable();
-            // $table->string('meta_robots')->nullable();
-
             $table->json('google')->nullable();
-            // $table->string('meta_google_site_verification')->nullable();
-            // $table->text('google_fonts_code')->nullable();
-            // $table->string('google_fonts_name')->nullable();
-            // $table->string('google_analytics')->nullable();
-
             $table->json('scripts')->nullable();
-            // $table->longText('header_scripts')->nullable();
-            // $table->longText('body_scripts')->nullable();
             $table->timestamps();
         });
     }
