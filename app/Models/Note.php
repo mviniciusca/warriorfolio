c6b92b18ad36f802512194ad6d2c0d24356e31a1
Marcos Coelho <mviniciusca@gmail.com>
2025-02-06 00:28:18 -0300

    wip

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Page;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,12 +14,15 @@ class Note extends Model
 
     use SoftDeletes;
 
-    protected $table = 'pages';
-
     protected $guarded = [];
 
     protected $casts = [
         'content' => 'array',
-        'blocks'  => 'array',
     ];
+
+    public function page()
+    {
+        // belongsTo(Page, 'coluna_na_tabela_notes', 'coluna_na_tabela_pages')
+        return $this->belongsTo(Page::class, 'page_slug', 'slug');
+    }
 }
--- database/migrations/2025_02_06_023408_create_notes_table.php
+++ database/migrations/2025_02_06_023408_create_notes_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Page;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('page_slug');
+            $table->json('content');
             $table->timestamps();
+            $table->foreign('page_slug')->references('slug')->on('pages')->onDelete('cascade');
         });
     }
 
