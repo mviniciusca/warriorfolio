27ae14f4e3da53e38e5b6cac66734f58907e15e5
Marcos Coelho <mviniciusca@gmail.com>
2025-02-06 20:03:00 -0300

    wip

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListPosts extends ListRecords
 {
     protected static string $resource = PostResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Blog Posts');
+    }
 
     protected function getHeaderActions(): array
     {
