9ac418dcd99d3c69c56edce612ba561e3a6846f5
Marcos Coelho <mviniciusca@gmail.com>
2025-02-06 12:38:39 -0300

    wip

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Note;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,10 +10,4 @@ class Page extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    public function note()
-    {
-        // hasOne(Nota, 'coluna_na_tabela_notes', 'coluna_na_tabela_pages')
-        return $this->hasOne(Note::class, 'page_slug', 'slug');
-    }
 }
