bf64e66f9cbf0bb617973dbfd17625275bb48dde
Marcos Coelho <mviniciusca@gmail.com>
2025-02-07 16:16:34 -0300

    wip

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -2,20 +2,20 @@
     'published_at' => null,
 ])
 <div>
-    <section class="flex pb-8 items-center mb-6 not-italic">
-        <div class="inline-flex items-center mr-3 text-sm ">
-            @if($user->profile->avatar)
-            <x-curator-glider
-                class="relative mx-auto my-2 mr-4 max-h-28 max-w-28 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
-                :media="$user->profile->avatar" />
+    <section class="mb-6 flex items-center pb-8 not-italic">
+        <div class="mr-3 inline-flex items-center text-sm">
+            @if ($user->profile->avatar)
+                <x-curator-glider
+                    class="relative mx-auto my-2 mr-4 max-h-32 max-w-32 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
+                    :media="$user->profile->avatar" />
             @else
-            <img class="relative mx-auto my-2 max-h-40 max-w-40 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
-                src="{{ asset('img/core/profile-picture.png') }}" />
+                <img class="relative mx-auto my-2 mr-4 max-h-32 max-w-32 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
+                    src="{{ asset('img/core/profile-picture.png') }}" />
             @endif
             <div>
-                <p href="#" rel="author" class="text-xl font-bold ">{{ $user->name }}</p>
-                <p class="text-base pb-2">{{ $user->profile->job_position }} • {{ $user->profile->localization }}</p>
-                <x-ui.social-network />
+                <p href="#" rel="author" class="-mt-4 text-xl font-bold">{{ $user->name }}</p>
+                <p class="pb-2 text-base">{{ $user->profile->job_position }} • {{ $user->profile->localization }}</p>
+                <p> <x-ui.social-network /></p>
             </div>
         </div>
     </section>
