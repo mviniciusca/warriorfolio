55f2579ca78b3ce08d5af3fe1f53f4b7b6b4cc94
Marcos Coelho <mviniciusca@gmail.com>
2025-02-07 18:57:08 -0300

    wip

--- resources/views/components/blog/header/breadcrumb.blade.php
+++ resources/views/components/blog/header/breadcrumb.blade.php
@@ -2,27 +2,33 @@
 <div>
     <nav class="flex py-12" aria-label="Breadcrumb">
         <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-          <li class="inline-flex items-center">
-            <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-            {{__('Home')}}
-            </a>
-          </li>
-          <li>
-            <div class="flex items-center">
-              <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-              <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">{{ __('Posts') }}</a>
-            </div>
-          </li>
-          <li aria-current="page">
-            <div class="flex items-center">
-              <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-              <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">{{ $title }}</span>
-            </div>
-          </li>
+            <li class="inline-flex items-center">
+                <a href="{{ env('APP_URL') }}"
+                    class="inline-flex items-center text-sm font-medium hover:text-primary-600">
+                    {{ __('Home') }}
+                </a>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="mx-1 h-3 w-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <a href="{{ env('APP_URL') . '/' . 'posts' }}"
+                        class="ms-1 text-sm font-medium hover:text-primary-600 md:ms-2">{{ __('Posts') }}</a>
+                </div>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                    <svg class="mx-1 h-3 w-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <span class="ms-1 text-sm font-medium md:ms-2">{{ $title }}</span>
+                </div>
+            </li>
         </ol>
-      </nav>
+    </nav>
 </div>
