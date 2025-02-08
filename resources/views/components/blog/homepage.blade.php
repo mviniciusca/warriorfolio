b44e2276daf308153f6aa0784bb7f1d764fc34ce
Marcos Coelho <mviniciusca@gmail.com>
2025-02-08 12:24:09 -0300

    wip

--- resources/views/components/blog/homepage.blade.php
+++ resources/views/components/blog/homepage.blade.php
@@ -3,9 +3,9 @@
         @foreach ($data as $item)
             <div class="flex w-full overflow-hidden border-b border-b-secondary-200 py-8 dark:border-b-secondary-800">
                 <div class="w-2/3 p-4">
-                    <div class="mb-2 text-xl font-bold">Título do Card</div>
+                    <div class="mb-2 text-xl font-bold">{{ $item->title }}</div>
                     <p class="text-base">
-                        Esta é uma pequena descrição do card. Ela pode conter mais detalhes sobre o conteúdo do card.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis labore pariatur minus!
                     </p>
                 </div>
                 <div class="w-32">
