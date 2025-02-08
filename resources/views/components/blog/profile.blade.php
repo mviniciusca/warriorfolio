f74843a32e6a20ef9bf9cbb94e9048b834ee282f
Marcos Coelho <mviniciusca@gmail.com>
2025-02-07 23:35:17 -0300

    wip

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -1,19 +1,15 @@
-@props([
-    'published_at' => null,
-])
 <div>
-    <section class="mb-6 flex items-center pb-8 not-italic">
+    <section class="flex items-center border-b border-b-secondary-200 py-8 not-italic dark:border-b-secondary-800">
         <div class="mr-3 inline-flex items-center text-sm">
             @if ($user->profile->avatar)
-                <x-curator-glider
-                    class="relative mx-auto my-2 mr-4 max-h-32 max-w-32 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
+                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover"
                     :media="$user->profile->avatar" />
             @else
-                <img class="relative mx-auto my-2 mr-4 max-h-32 max-w-32 rounded-full bg-secondary-300 bg-gradient-to-tl from-primary-500 to-tertiary-500 object-cover p-1 dark:bg-secondary-700 lg:my-8"
+                <img class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover"
                     src="{{ asset('img/core/profile-picture.png') }}" />
             @endif
             <div>
-                <p href="#" rel="author" class="-mt-4 text-xl font-bold">{{ $user->name }}</p>
+                <p href="#" rel="author" class="text-base font-bold">{{ $user->name }}</p>
                 <p class="pb-2 text-base">{{ $user->profile->job_position }} • {{ $user->profile->localization }}</p>
                 <p> <x-ui.social-network /></p>
             </div>
