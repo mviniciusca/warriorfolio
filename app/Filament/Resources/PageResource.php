172fee43fd021af5a2063410f5f062ac48389cb3
Marcos Coelho <mviniciusca@gmail.com>
2025-02-10 21:23:08 -0300

    wip

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -25,50 +25,21 @@ class PageResource extends ResourcesPageResource
 {
     protected static ?string $model = Page::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document';
 
-    // public static function form(Form $form): Form
-    // {
-    //     return $form
-    //         ->schema([
-    //             //
-    //         ]);
-    // }
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Website Design');
+    }
 
-    // public static function table(Table $table): Table
-    // {
-    //     return $table
-    //         ->columns([
-    //             //
-    //         ])
-    //         ->filters([
-    //             //
-    //         ])
-    //         ->actions([
-    //             Tables\Actions\EditAction::make(),
-    //         ])
-    //         ->bulkActions([
-    //             Tables\Actions\BulkActionGroup::make([
-    //                 Tables\Actions\DeleteBulkAction::make(),
-    //             ]),
-    //         ]);
-    // }
+    public static function getNavigationBadge(): ?string
+    {
+        if (static::getModel()::where('style', '!=', 'blog')->count() > 0) {
+            return static::getModel()::where('style', '!=', 'blog')->count();
+        }
 
-    // public static function getRelations(): array
-    // {
-    //     return [
-    //         //
-    //     ];
-    // }
-
-    // public static function getPages(): array
-    // {
-    //     return [
-    //         'index'  => Pages\ListPages::route('/'),
-    //         'create' => Pages\CreatePage::route('/create'),
-    //         'edit'   => Pages\EditPage::route('/{record}/edit'),
-    //     ];
-    // }
+        return null;
+    }
 
     public static function table(Table $table): Table
     {
