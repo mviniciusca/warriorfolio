765bca078c1e05c661f8d975b49a0bbffc3dcaac
Marcos Coelho <mviniciusca@gmail.com>
2025-02-12 15:08:07 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/blog/homepage.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/blog/homepage.blade.php
@@ -1,9 +1,10 @@
 @props(['profile ' => null, 'page' => null])
 <x-core.layout>
     <div class="mx-auto">
+        <x-blog.header.category />
         <div class="-mx-4 flex flex-wrap">
             <!-- Coluna principal -->
-            <div class="order-2 w-full px-4 lg:w-2/3">
+            <div class="order-2 w-full border-l border-l-secondary-200 pl-16 pr-4 dark:border-l-secondary-800 lg:w-2/3">
                 <x-blog.homepage />
             </div>
             <!-- Coluna lateral -->
