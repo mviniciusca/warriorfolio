8ddf4fa5aef4be845613187de16d3e0ed59f95d8
Marcos Coelho <mviniciusca@gmail.com>
2025-02-13 20:09:57 -0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,26 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
 const colors = require('tailwindcss/colors')
 export default {
-  // presets: [preset],
-  content: [
-    './app/Filament/**/*.php',
-    './resources/views/filament/**/*.blade.php',
-    './vendor/filament/**/*.blade.php',
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.purple, // accent color
-        secondary: colors.zinc, // dark color.
-        tertiary: colors.pink, // highlight effects color
-      }
+    // presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.purple, // accent color
+                secondary: colors.zinc, // dark color.
+                tertiary: colors.pink, // highlight effects color
+            }
+        },
     },
-  },
-  plugins: [
-  ],
+    plugins: [
+    ],
 }
 
