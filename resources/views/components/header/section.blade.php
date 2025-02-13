043bbd3b29a0d978f3ee293bbc8dcb9046370d42
Marcos Coelho <mviniciusca@gmail.com>
2025-02-13 17:52:46 -0300

    wip

--- resources/views/components/header/section.blade.php
+++ resources/views/components/header/section.blade.php
@@ -20,7 +20,7 @@
                 <button data-collapse-toggle="mobile-menu-2" type="button"
                     class="ml-1 inline-flex items-center rounded-lg p-2 text-sm hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:hover:bg-secondary-700 dark:focus:ring-secondary-600 lg:hidden"
                     aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
+                    <span class="sr-only">{{ __('Open main menu') }}</span>
                     <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
@@ -34,7 +34,7 @@
                     </svg>
                 </button>
             </div>
-
+            {{-- Navigation --}}
             @if ($navigation)
                 <div class="nav-bar hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto"
                     id="mobile-menu-2">
@@ -42,12 +42,12 @@
                 </div>
             @endif
         </div>
-
+        {{-- Darkmode --}}
         <div class="flex flex-wrap items-center">
             <livewire:dark-mode wire:key='header-dark-mode' />
         </div>
-
     </div>
+    {{-- Light Beam --}}
     <div class="mx-auto my-4">
         <img class="-z-50 animate-pulse" src="{{ asset('img/core/core-ui-elements/beams/pink-beam.png') }}"
             alt="light-beam" id="menu-gipper-board">
