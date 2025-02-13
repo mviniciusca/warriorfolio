0d8c465fdb2bc493fc2bf4e38b2bcd396c7e0663
Marcos Coelho <mviniciusca@gmail.com>
2025-02-13 17:31:43 -0300

    wip

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -1,16 +1,18 @@
+@props(['page' => null])
+
 <div>
-    <section class="flex items-center border-b border-b-secondary-200 py-8 not-italic dark:border-b-secondary-800">
-        <div class="mr-3 inline-flex items-center text-sm">
+    <section class="my-12 flex items-start border-b border-b-secondary-200 not-italic dark:border-b-secondary-800">
+        <div class="mr-3 inline-flex items-start py-4 text-sm">
             @if ($user->profile->avatar)
-                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover"
+                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-16 max-w-14 rounded-full object-cover"
                     :media="$user->profile->avatar" />
             @else
                 <img class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover"
                     src="{{ asset('img/core/profile-picture.png') }}" />
             @endif
             <div>
-                <p href="#" rel="author" class="text-base font-bold">{{ $user->name }}</p>
-                <p class="pb-2 text-base">{{ $user->profile->job_position }} • {{ $user->profile->localization }}</p>
+                <p href="#" rel="author" class="text-base">{{ $user->name }}</p>
+                <p class="pb-2 text-xs">{{ $user->profile->job_position }} - {{ $user->profile->localization }}</p>
                 <p> <x-ui.social-network /></p>
             </div>
         </div>
