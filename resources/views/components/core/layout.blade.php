b10e0d4d2d4ae7aebac40d8073840d286efcb5b2
Marcos Coelho <mviniciusca@gmail.com>
2025-02-13 16:14:07 -0300

    wip

--- resources/views/components/core/layout.blade.php
+++ resources/views/components/core/layout.blade.php
@@ -1,15 +1,15 @@
 @aware(['page'])
 @props(['module_title' => null, 'module_subtitle' => null])
 
-<section {{ $attributes->merge(['class' => 'w-full ']) }}>
-    <div class="mx-auto px-8 md:px-12 lg:px-16 max-w-7xl {{ $module_title ? 'py-8' : 'py-4' }}">
+<section {{ $attributes->merge(['class' => 'w-full']) }}>
+    <div class="{{ $module_title ? 'py-8' : 'py-4' }} mx-auto max-w-7xl px-8 md:px-12 lg:px-16">
 
-        @if($module_title)
-        <div class="header-title py-2">{{ $module_title }}</div>
+        @if ($module_title)
+            <div class="header-title py-2">{{ $module_title }}</div>
         @endif
 
-        @if($module_subtitle)
-        <div class="subtitle mx-auto mt-4 max-w-5xl text-center">{{ $module_subtitle }}</div>
+        @if ($module_subtitle)
+            <div class="subtitle mx-auto mt-4 max-w-5xl text-center">{{ $module_subtitle }}</div>
         @endif
         <div class="section-container" id="app-container">
             {{ $slot }}
