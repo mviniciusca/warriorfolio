c9017eba0b3c4c34d0b290011fb25803b17ec893
Marcos Coelho <mviniciusca@gmail.com>
2025-02-14 17:14:30 -0300

    wip

--- resources/views/components/hero/section.blade.php
+++ resources/views/components/hero/section.blade.php
@@ -17,9 +17,9 @@
 
                 {{-- Static Slider --}}
                 @if (data_get($hero, 'hero.slider_is_active'))
-                    <div class="flex max-w-7xl mb-16 mt-4 mx-auto flex-wrap justify-around gap-4">
+                    <div class="mx-auto mb-16 mt-4 flex max-w-7xl flex-wrap justify-around gap-4">
                         @foreach (collect($hero->hero['slider_content'])->flatten(1) as $item)
-                            <img class="{{ $hero->hero['is_invert'] ? 'dark:invert' : null }} opacity-50 hover:opacity-100 transition-all px-4 duration-100"
+                            <img class="{{ $hero->hero['is_invert'] ? 'dark:invert' : null }} px-4 opacity-50 transition-all duration-100 hover:opacity-100"
                                 src=" {{ asset('storage/' . $item['slider_image']) }}" />
                         @endforeach
                     </div>
