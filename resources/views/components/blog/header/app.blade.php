0a0f6a3ea44ff4771b32cfb0b671e2e971870497
Marcos Coelho <mviniciusca@gmail.com>
2025-02-14 15:39:10 -0300

    wip

--- resources/views/components/blog/header/app.blade.php
+++ resources/views/components/blog/header/app.blade.php
@@ -2,11 +2,11 @@
 
 @if ($data->blog['is_active'] ?? false)
     <section id="blog-header-components"
-        class="p-21 w-full rounded-xl bg-secondary-200/20 p-5 py-12 dark:bg-secondary-950/15">
+        class="p-21 w-full overflow-hidden rounded-xl bg-secondary-200/20 p-5 py-8 dark:bg-secondary-950/15 md:py-12">
         <div class="flex flex-initial items-center gap-2">
             @if ($data->blog['is_logo_active'] ?? true)
                 <div
-                    class="h-32 w-32 overflow-hidden rounded-lg bg-secondary-200/50 object-cover object-center p-2 shadow-lg dark:bg-secondary-800">
+                    class="hidden h-16 w-16 overflow-hidden rounded-lg bg-secondary-200/50 object-cover object-center p-2 shadow-lg dark:bg-secondary-800 sm:block sm:h-32 sm:w-32">
                     @if ($data->blog['logo'] ?? false)
                         <x-curator-glider class="h-28 w-28 overflow-hidden object-cover object-center"
                             :media="$data->blog['logo']" />
@@ -18,7 +18,7 @@
             @endif
             <div id="blog-name-group">
                 @isset($data->blog['name'])
-                    <p class="text-5xl font-semibold leading-none tracking-tighter">
+                    <p class="text-3xl font-semibold leading-none tracking-tighter md:text-4xl lg:text-5xl">
                         {!! $data->blog['name'] !!}
                     </p>
                 @endisset
