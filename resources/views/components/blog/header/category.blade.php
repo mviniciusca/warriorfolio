b122144a37402b26c3f968bfe35efb21fc3c9da1
Marcos Coelho <mviniciusca@gmail.com>
2025-02-14 18:14:31 -0300

    wip

--- resources/views/components/blog/header/category.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="my-8 border-b border-b-secondary-200 pb-4 dark:border-b-secondary-800">
-    @foreach ($data as $category)
-        <span class="mr-4 w-full text-sm">{{ $category->name }}</span>
-    @endforeach
-</div>
