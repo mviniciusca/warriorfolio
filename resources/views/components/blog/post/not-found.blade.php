7b9e758ebd7e1b2e7935aa1a26aded4b301c7e20
Marcos Coelho <mviniciusca@gmail.com>
2025-02-15 17:52:29 -0300

    wip

--- resources/views/components/blog/post/not-found.blade.php
+++ resources/views/components/blog/post/not-found.blade.php
@@ -14,7 +14,7 @@
                 </p>
                 <a href="#"
                     class="my-4 inline-flex rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Back to Homepage
+                    {{ __('Back to Homepage') }}
                 </a>
             </div>
         </div>
