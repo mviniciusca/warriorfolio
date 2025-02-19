fc8f84e00cb6ad1b6f5fc1e9d68f7bbe70b33d83
Marcos Coelho <mviniciusca@gmail.com>
2025-02-19 09:18:39 -0300

    wip

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -11,6 +11,7 @@ use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
@@ -63,16 +64,22 @@ class PostResource extends Resource
             ->columns(4)
             ->schema([
                 Section::make(__('Blog Post'))
+                    ->description(__('Write, edit and manage your Blog Post.'))
                     ->columnSpan(3)
                     ->icon('heroicon-o-pencil')
                     ->schema([
                         TextInput::make('title')
+                            ->label(__('Post Title'))
                             ->live(onBlur: true)
+                            ->helperText(__('Title of your post.'))
                             ->prefixIcon('heroicon-o-pencil')
                             ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug',
                                 env('APP_BLOG_PATH').Str::slug($state).env('APP_BLOG_URL_END')))
                             ->required()
                             ->maxLength(255),
+                        Textarea::make('resume')
+                            ->helperText(__('Optional'))
+                            ->label(__('Short Description')),
                         Hidden::make('style')
                             ->default('blog'),
                         Hidden::make('blocks')
@@ -80,25 +87,41 @@ class PostResource extends Resource
                             ->default([['data' => [], 'type' => 'blog.post']])
                             ->required(),
                         RichEditor::make('content')
+                            ->required()
+                            ->helperText(__('Content of your post.'))
                             ->columnSpanFull(),
                         TextInput::make('slug')
                             ->prefixIcon('heroicon-o-link')
+                            ->dehydrated()
+                            ->label(__('Post Slug'))
                             ->required()
+                            ->helperText(__('Automatic generated.'))
                             ->maxLength(255),
                     ]),
                 Group::make()
                     ->columnSpan(1)
                     ->schema([
-                        CuratorPicker::make('img_cover')
-                            ->label(__('Cover')),
-                        Toggle::make('is_active')
-                            ->label(__('Status'))
-                            ->label(__('Published'))
-                            ->default(true),
-                        Select::make('category_id')
-                            ->label(__('Category'))
-                            ->required()
-                            ->options(Category::all()->pluck('name', 'id')),
+                        Section::make(__('Featured Image'))
+                            ->icon('heroicon-o-photo')
+                            ->schema([
+                                CuratorPicker::make('img_cover')
+                                    ->helperText(__('Image cover.'))
+                                    ->label(__('Cover')),
+                            ]),
+                        Section::make(__('Settings'))
+                            ->icon('heroicon-o-cog-6-tooth')
+                            ->schema([
+                                Select::make('category_id')
+                                    ->label(__('Category'))
+                                    ->helperText(__('Main category of your post.'))
+                                    ->required()
+                                    ->options(Category::all()->pluck('name', 'id')),
+                                Toggle::make('is_active')
+                                    ->label(__('Status'))
+                                    ->label(__('Published'))
+                                    ->helperText(__('Visibility status of your post.'))
+                                    ->default(true),
+                            ]),
                     ]),
             ]);
     }
