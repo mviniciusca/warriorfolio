b4c14e0673bcfbd3d423485ce7af75dae647379a
Marcos Coelho <mviniciusca@gmail.com>
2025-02-19 14:39:37 -0300

    wip

--- resources/views/components/about/section.blade.php
+++ resources/views/components/about/section.blade.php
@@ -1,43 +1,43 @@
-@if($module->about)
-<x-core.layout class="{{ data_get($data, 'about.section_fill') ? 'bg-secondary-100 dark:bg-secondary-950' : null }}"
-    id="about">
+@if ($module->about)
+    <x-core.layout class="{{ data_get($data, 'about.section_fill') ? 'bg-secondary-100 dark:bg-secondary-950' : null }}"
+        id="about">
 
-    @if($data->about['section_title'])
-    <x-slot name="module_title">
-        {!! $data->about['section_title'] !!}
-    </x-slot>
-    @endif
+        @if ($data->about['section_title'])
+            <x-slot name="module_title">
+                {!! $data->about['section_title'] !!}
+            </x-slot>
+        @endif
 
-    @if($data->about['section_subtitle'])
-    <x-slot name="module_subtitle">
-        {!! $data->about['section_subtitle'] !!}
-    </x-slot>
-    @endif
+        @if ($data->about['section_subtitle'])
+            <x-slot name="module_subtitle">
+                {!! $data->about['section_subtitle'] !!}
+            </x-slot>
+        @endif
 
-    <div class="mt-4 flex flex-wrap lg:mt-12" id="about-you-module">
+        <div class="mt-4 flex flex-wrap lg:mt-12" id="about-you-module">
 
-        {{-- Profile Section --}}
-        <div class="w-full p-4 text-center lg:w-1/4 lg:p-8" id="profile">
-            <x-about.profile :$profile />
+            {{-- Profile Section --}}
+            <div class="w-full p-4 text-center lg:w-1/4 lg:p-8" id="profile">
+                <x-about.profile :$profile />
+            </div>
+
+            {{-- About Section --}}
+            <div class="w-full p-4 leading-loose md:w-2/3 lg:w-2/4 lg:p-8" id="about-you">
+                {!! $profile->about !!}
+
+                @if (!$profile->about)
+                    <x-ui.empty-section :auth="'Go to your Dashboard and create a New About.'" />
+                @endif
+
+            </div>
+
+            {{-- Courses and Graduations Section --}}
+            <div class="w-full p-4 md:w-1/3 lg:w-1/4 lg:p-8" id="courses-and-graduations">
+                <x-about.courses :$courses />
+            </div>
+
         </div>
 
-        {{-- About Section --}}
-        <div class="w-full p-4 leading-loose md:w-2/3 lg:w-2/4 lg:p-8" id="about-you">
-            {!! $profile->about !!}
-
-            @if(!$profile->about)
-            <x-ui.empty-section :auth="'Go to your Dashboard and create a New About.'" />
-            @endif
-
-        </div>
-
-        {{-- Courses and Graduations Section --}}
-        <div class="w-full p-4 md:w-1/3 lg:w-1/4 lg:p-8" id="courses-and-graduations">
-            <x-about.courses :$courses />
-        </div>
-
-    </div>
-
-    <x-hero.slider :$sliders />
-</x-core.layout>
+        <x-hero.slider :$sliders />
+    </x-core.layout>
 @endif
