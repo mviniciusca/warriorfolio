dfa9b0d98cbfab0d33223e1e71f2e05f3270d10e
Marcos Coelho <mviniciusca@gmail.com>
2025-02-20 20:14:00 -0300

    wip

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -1,7 +1,7 @@
 @props(['page' => null])
 
 <div>
-    <section class="my-12 flex items-start border-b border-b-secondary-200 not-italic dark:border-b-secondary-800">
+    <section class="my-3 flex items-start border-b border-b-secondary-200 not-italic dark:border-b-secondary-800">
         <div class="mr-3 inline-flex items-start py-4 text-sm">
             @if ($user->profile->avatar)
                 <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-16 max-w-14 rounded-full object-cover"
@@ -11,8 +11,9 @@
                     src="{{ asset('img/core/profile-picture.png') }}" />
             @endif
             <div>
-                <p href="#" rel="author" class="text-base">{{ $user->name }}</p>
-                <p class="pb-2 text-xs">{{ $user->profile->job_position }} - {{ $user->profile->localization }}</p>
+                <p href="#" rel="author" class="font-mono text-base uppercase">{{ $user->name }}</p>
+                <p class="pb-2 font-mono text-xs uppercase">{{ $user->profile->job_position }} -
+                    {{ $user->profile->localization }}</p>
                 <p> <x-ui.social-network /></p>
             </div>
         </div>
--- resources/views/components/filament-fabricator/page-blocks/blog/post.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/blog/post.blade.php
@@ -7,40 +7,34 @@
             <article
                 class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-3xl">
                 <x-blog.header.breadcrumb :$title />
-                <header class="not-format">
-                    <h1 class="leading-tighter mt-5 text-3xl font-extrabold tracking-tighter lg:mb-4 lg:text-4xl">
+                <section class="not-format">
+                    <p class="py-2 font-mono uppercase">
+                        {{ \Carbon\Carbon::parse($page->created_at)->format('F d, Y ') }}
+                    </p>
+
+                    {{-- Title --}}
+                    <h1
+                        class="leading-tighter mt-2 text-3xl font-extrabold tracking-tight dark:text-white/90 lg:mb-4 lg:text-5xl">
                         {{ $page->title }}
                     </h1>
-                    <h2 class="pb-8 text-xl leading-tight tracking-tight text-secondary-400 dark:text-secondary-500">
+                    {{-- Subtitle --}}
+                    <h2 class="text-md pb-4 leading-tight tracking-tight">
                         {{ $page->resume }}
                     </h2>
-                    <div class="flex items-center justify-between">
-                        <span class="flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                            </svg>
+                    {{-- Info --}}
 
-                            {{ __('Published in ') . $page->category->name }}</span>
-                        <span class="flex items-center gap-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                            </svg>
-                            {{ \Carbon\Carbon::parse($page->created_at)->format('M d, Y ') }}
-                        </span>
-                    </div>
-                </header>
+                </section>
+                {{-- Profile --}}
                 <x-blog.profile :$page />
-                <div class="pb-12 pt-4">
+                <div class="py-4">
                     {!! $page->content !!}
                 </div>
+                {{-- Article --}}
                 <x-blog.post.articles :page="$page" />
             </article>
         </div>
     </main>
+    {{-- Not Found --}}
 @else
     <x-blog.post.not-found />
 @endif
