8f69ec2e009fd1920f481d10c84c072164495dbc
Marcos Coelho <mviniciusca@gmail.com>
2025-02-20 16:47:47 -0300

    wip

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,27 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Z3d0X\FilamentFabricator\Models\Concerns\HandlesPageUrls;
-use Z3d0X\FilamentFabricator\Models\Contracts\Page as Contract;
 use Z3d0X\FilamentFabricator\Models\Page;
 
-class Post extends Page implements Contract
+class Post extends Page
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
-
-    use HandlesPageUrls;
-
-    public function __construct(array $attributes = [])
-    {
-        if (blank($this->table)) {
-            $this->setTable(config('filament-fabricator.table_name', 'pages'));
-        }
-
-        parent::__construct($attributes);
-    }
 
     public function category(): BelongsTo
     {
