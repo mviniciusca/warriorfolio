bd3c118e6dad19f539c50fa3d6bb3bd3211ad359
Marcos Coelho <mviniciusca@gmail.com>
2025-02-21 16:41:35 -0300

    wip

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'button', 'icon' => 'arrow-forward-outline'])
 
 <button type="{{ $type }}"
-    class="mt-4 inline-flex items-center gap-1 rounded-md bg-primary-600 px-3 py-3 text-center text-sm font-medium text-white transition-all duration-100 hover:opacity-90 focus:ring-4 focus:ring-primary-500 dark:focus:ring-primary-950">
+    class="mt-4 inline-flex items-center gap-1 rounded-md bg-primary-700 px-3 py-3 text-center text-sm font-medium text-white transition-all duration-100 hover:opacity-90 focus:ring-4 focus:ring-primary-500 dark:focus:ring-primary-950">
 
     <span>
         {{ $slot }}
