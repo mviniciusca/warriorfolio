34bab77f8c408a17b6c1ab49928b58e024dfb69e
Marcos Coelho <mviniciusca@gmail.com>
2025-02-21 01:29:53 -0300

    wip

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -1,17 +1,17 @@
-@if(data_get($design,'background_image_visibility') === true)
-@if(data_get($design,'background_image'))
-<div id="default-background" class="
-w-full h-[1080px] absolute -z-50 -mt-52
-{{ $design['background_image'] ? '' : 'animate-pulse' }}
-{{ $design['animation'] ? 'animate-opacity opacity-10' : '' }}
-{{ $design['background_image_size'] . ' ' . 
-        $design['background_image_position'] . ' ' . $design['background_image_repeat'] }}"
-    style="background-image: url('{{ asset('storage/' . $design['background_image']) }}')">
-</div>
-@else
-<div id="default-background"
-    class="w-full h-[1080px] animate-opacity opacity-10 absolute -z-50 -mt-52 bg-no-repeat bg-center bg-scroll bg-auto"
-    style="background-image: url('{{ asset('img/core/bg/astro-default-bg.png') }}')">
-</div>
-@endif
+@if (data_get($design, 'background_image_visibility') === true)
+    @if (data_get($design, 'background_image'))
+        <div id="default-background"
+            class="{{ $design['background_image'] ? '' : 'animate-pulse' }} {{ $design['animation'] ? 'animate-opacity opacity-10' : '' }} {{ $design['background_image_size'] .
+                ' ' .
+                $design['background_image_position'] .
+                ' ' .
+                $design['background_image_repeat'] }} absolute -z-50 -mt-52 h-[1080px] w-full"
+            style="background-image: url('{{ asset('storage/' . $design['background_image']) }}')">
+        </div>
+    @else
+        <div id="default-background"
+            class="{{ $design['animation'] ? 'animate-opacity opacity-10' : '' }} absolute -z-50 -mt-52 h-[1080px] w-full bg-auto bg-scroll bg-center bg-no-repeat"
+            style="background-image: url('{{ asset('img/core/demo/default-bg.png') }}')">
+        </div>
+    @endif
 @endif
