07e2553e2162514af0b8673fc1f880d5e9139f83
Marcos Coelho <mviniciusca@gmail.com>
2025-02-21 20:57:26 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,7 @@ use App\Models\Course;
 use App\Models\Mail;
 use App\Models\Newsletter;
 use App\Models\Page;
+use App\Models\Post;
 use App\Models\Setting;
 use App\Models\User;
 use Database\Seeders\DocumentationPageSeeder;
@@ -48,6 +49,36 @@ class DatabaseSeeder extends Seeder
             LandingPageSeeder::class,
             DocumentationPageSeeder::class,
             BlogSeeder::class,
+        ]);
+
+        $post = Post::create([
+            'category_id' => 1,
+            'resume'      => 'Welcome to Your First Note!',
+            'content'     => 'Hello there! 🌟
+            <p></p>
+This is the beginning of something new—your very first note! Whether you\'re jotting down thoughts, capturing ideas, or simply reminding yourself of something important, this space is yours to explore.
+<p></p>
+Feel free to write freely, without worrying about perfection. Notes can be messy, creative, practical, or poetic—they\'re a reflection of you. Use them to brainstorm, plan your day, track goals, or even vent when needed. Every journey starts with a single step, and this note is yours.
+<p></p>
+Who knows? Looking back someday, you might smile at how far you\'ve come.
+<p></p>
+Here\'s to many more notes ahead! 🚀',
+        ]);
+
+        Page::insert([
+            'post_id'    => $post->id,
+            'title'      => 'First Note',
+            'slug'       => 'blog/post/first-note.html',
+            'created_at' => now(),
+            'updated_at' => now(),
+            'style'      => 'blog',
+            'layout'     => 'default',
+            'blocks'     => json_encode([
+                [
+                    'data' => [],
+                    'type' => 'blog.post',
+                ],
+            ]),
         ]);
     }
 }
