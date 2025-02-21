e88668ea7a6961805a4132eea0a9af8169a309cb
Marcos Coelho <mviniciusca@gmail.com>
2025-02-21 03:40:22 -0300

    wip

--- resources/views/components/footer/section.blade.php
+++ resources/views/components/footer/section.blade.php
@@ -1,9 +1,9 @@
-@props(['setting'])
+@props(['setting' => null, 'navigation' => null])
 
 @if ($module->footer)
-    <div class="{{ data_get($data, 'footer.section_fill') ? 'section-filled' : null }}">
+    <div class="{{ data_get($data, 'footer.section_fill') ? 'section-filled' : '' }}">
         <x-core.layout>
-            <x-footer.content-module :setting='$setting' />
+            <x-footer.content-module :$setting :$navigation :$social />
         </x-core.layout>
     </div>
 @endif
