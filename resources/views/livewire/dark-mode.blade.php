71eb2487c6c5c37a533ce24bda6e59299b1cd46b
Marcos Coelho <mviniciusca@gmail.com>
2025-02-22 14:13:29 -0300

    wip

--- resources/views/livewire/dark-mode.blade.php
+++ resources/views/livewire/dark-mode.blade.php
@@ -3,10 +3,10 @@
     <label class="relative -mt-5 inline-flex w-auto cursor-pointer items-center">
         <input type="checkbox" wire:model.live='active' class="peer sr-only" checked>
         <span class="ms-3 text-sm font-medium text-secondary-900 dark:text-secondary-300">
-            @if($active)
-            <x-ui.sun-icon />
+            @if ($active)
+                <x-ui.sun-icon />
             @else
-            <x-ui.moon-icon />
+                <x-ui.moon-icon />
             @endif
         </span>
     </label>
