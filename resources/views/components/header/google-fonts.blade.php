37c68bef28713f16733d5827da695019576c6034
Marcos Coelho <mviniciusca@gmail.com>
2025-02-22 15:13:06 -0300

    wip

--- resources/views/components/header/google-fonts.blade.php
+++ resources/views/components/header/google-fonts.blade.php
@@ -1,18 +1,20 @@
-@if(data_get($fonts, 'google.fonts_code') && data_get($fonts, 'google.font_name'))
-{!! data_get($fonts, 'google.fonts_code') !!}
-<style>
-    body {
-        font-family: "{{ data_get($fonts, 'google.font_name') }}",
-        sans-serif;
-    }
-</style>
+@if (data_get($fonts, 'google.fonts_code') && data_get($fonts, 'google.font_name'))
+    {!! data_get($fonts, 'google.fonts_code') !!}
+    <style>
+        body {
+            font-family: "{{ data_get($fonts, 'google.font_name') }}",
+                sans-serif;
+        }
+    </style>
 @else
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
-<style>
-    body {
-        font-family: 'Inter', sans-serif;
-    }
-</style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
 @endif
