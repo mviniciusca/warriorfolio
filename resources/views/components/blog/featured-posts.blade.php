3263dfd1233d5e8b1a52ed3cbb9cb072006cae00
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 02:07:53 -0300

    wip

--- resources/views/components/blog/featured-posts.blade.php
+++ resources/views/components/blog/featured-posts.blade.php
@@ -1,19 +1,23 @@
-<x-core.layout>
-    <x-slot name="module_title">
-
-        <x-slot name="button_header">
-            Visit Blog
+@if ($info['module_is_active'])
+    <x-core.layout>
+        <x-slot name="module_title">
+            {{ $info['header_title'] }}
+            <x-slot name="button_header">
+                <a href="{{ $info['button_url'] }}">
+                    {{ $info['button'] }}
+                </a>
+            </x-slot>
         </x-slot>
-    </x-slot>
-    <x-slot name="module_subtitle">
-        Latest posts from our Blog.
-    </x-slot>
-    <section class="mt-8">
-        <div class="grid gap-8 lg:grid-cols-2">
-            @foreach ($posts as $item)
-                <x-blog.post.card-block :$item />
-            @endforeach
-        </div>
-        </div>
-    </section>
-</x-core.layout>
+        <x-slot name="module_subtitle">
+            {{ $info['header_subtitle'] }}
+        </x-slot>
+        <section class="mt-8">
+            <div class="grid gap-8 lg:grid-cols-2">
+                @foreach ($posts as $item)
+                    <x-blog.post.card-block :$item />
+                @endforeach
+            </div>
+            </div>
+        </section>
+    </x-core.layout>
+@endif
