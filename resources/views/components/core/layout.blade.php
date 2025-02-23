c9fd985fe2175c90781f597881353156d506fd8c
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 00:16:27 -0300

    wip

--- resources/views/components/core/layout.blade.php
+++ resources/views/components/core/layout.blade.php
@@ -3,13 +3,15 @@
 
 <section {{ $attributes->merge(['class' => 'w-full']) }}>
     <div class="{{ $module_title ? 'py-16' : 'py-8' }} mx-auto max-w-7xl px-8 md:px-12 lg:px-16">
-
         @if ($module_title)
-            <div class="header-title py-2">{{ $module_title }}</div>
+            <div class="header-title py-2">
+                <p> {{ $module_title }}</p>
+            </div>
         @endif
-
         @if ($module_subtitle)
-            <div class="subtitle mx-auto mt-4 max-w-3xl text-center text-lg">{{ $module_subtitle }}</div>
+            <div class="subtitle mx-auto mt-4 max-w-2xl text-center">
+                {{ $module_subtitle }}
+            </div>
         @endif
         <div class="section-container" id="app-container">
             {{ $slot }}
