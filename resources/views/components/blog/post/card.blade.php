d9b54e04bdd04b568ac6d161403791e798675eb2
Marcos Coelho <mviniciusca@gmail.com>
2025-02-22 23:25:45 -0300

    wip

--- resources/views/components/blog/post/card.blade.php
+++ resources/views/components/blog/post/card.blade.php
@@ -7,6 +7,23 @@
         <div
             class="flex w-full justify-between overflow-hidden border-b border-b-secondary-100 py-8 dark:border-b-secondary-800">
             <div class="w-2/3">
+
+                <div class="mb-4 flex items-center gap-1 font-mono text-xs uppercase">
+                    <span>
+                        @if ($item->user->profile->avatar)
+                            <x-curator-glider class="mx-1 h-7 w-7 rounded-full" :media="$item->user->profile->avatar" />
+                        @else
+                            <img class="mx-1 h-7 w-7 rounded-full invert-0 dark:invert"
+                                src="{{ asset('img/core/profile-picture.png') }}" />
+                        @endif
+                    </span>
+                    <span>
+                        {{ $item->user->name . ' • ' . $item->created_at->diffForHumans() }}
+                    </span>
+                </div>
+
+
+
                 <div class="text-md mb-2 font-bold leading-tight tracking-tighter md:text-2xl">
                     {{ Str::words($item->title, 18, '...') }}
                 </div>
@@ -20,7 +37,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                         </svg>
-                        <p>{{ $item->user->name . __(' in ') . $item->post->category->name }}</p>
+                        <p>{{ $item->post->category->name }}</p>
                     </span>
                     <span class="flex items-center gap-1 font-mono uppercase">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -28,7 +45,9 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                         </svg>
-                        {{ \Carbon\Carbon::parse($item->created_at)->format('F d, Y') }}</span>
+                        <p>{{ \Carbon\Carbon::parse($item->created_at)->format('F d, Y') }}</p>
+                    </span>
+
                 </span>
             </div>
             <div
