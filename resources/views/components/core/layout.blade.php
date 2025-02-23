047660107f0a1c59445b9c69157811de00850bb8
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 00:35:56 -0300

    wip

--- resources/views/components/core/layout.blade.php
+++ resources/views/components/core/layout.blade.php
@@ -1,11 +1,21 @@
 @aware(['page'])
-@props(['module_title' => null, 'module_subtitle' => null])
+@props([
+    'module_title' => null,
+    'module_subtitle' => null,
+    'button_header' => null,
+    'icon' => 'arrow-forward-sharp',
+])
 
 <section {{ $attributes->merge(['class' => 'w-full']) }}>
     <div class="{{ $module_title ? 'py-16' : 'py-8' }} mx-auto max-w-7xl px-8 md:px-12 lg:px-16">
         @if ($module_title)
-            <div class="header-title py-2">
-                <p> {{ $module_title }}</p>
+            <div class="header-title {{ $button_header ? 'flex justify-between flex-initial ' : '' }} py-4">
+                <p>{{ $module_title }}</p>
+                @if ($button_header)
+                    <x-ui.button-alt :$icon>
+                        {{ $button_header }}
+                    </x-ui.button-alt>
+                @endif
             </div>
         @endif
         @if ($module_subtitle)
