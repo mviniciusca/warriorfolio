a1c258ef2bb48399b5673bae472c0178cb096648
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 02:14:18 -0300

    wip

--- resources/views/components/blog/featured-posts.blade.php
+++ resources/views/components/blog/featured-posts.blade.php
@@ -1,23 +1,25 @@
-@if ($info['module_is_active'])
-    <x-core.layout>
-        <x-slot name="module_title">
-            {{ $info['header_title'] }}
-            <x-slot name="button_header">
-                <a href="{{ $info['button_url'] }}">
-                    {{ $info['button'] }}
-                </a>
+    @if ($info['module_is_active'] ?? false)
+        <x-core.layout>
+            <x-slot name="module_title">
+                {{ $info['header_title'] ?? null }}
+                @if ($info['button'])
+                    <x-slot name="button_header">
+                        <a href="{{ $info['button_url'] ?? null }}">
+                            {{ $info['button'] ?? null }}
+                        </a>
+                    </x-slot>
+                @endif
             </x-slot>
-        </x-slot>
-        <x-slot name="module_subtitle">
-            {{ $info['header_subtitle'] }}
-        </x-slot>
-        <section class="mt-8">
-            <div class="grid gap-8 lg:grid-cols-2">
-                @foreach ($posts as $item)
-                    <x-blog.post.card-block :$item />
-                @endforeach
-            </div>
-            </div>
-        </section>
-    </x-core.layout>
-@endif
+            <x-slot name="module_subtitle">
+                {{ $info['header_subtitle'] ?? null }}
+            </x-slot>
+            <section class="mt-8">
+                <div class="grid gap-8 lg:grid-cols-2">
+                    @foreach ($posts as $item)
+                        <x-blog.post.card-block :$item />
+                    @endforeach
+                </div>
+                </div>
+            </section>
+        </x-core.layout>
+    @endif
