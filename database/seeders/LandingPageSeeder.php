ca88b63ebff3877efba759478dc798700384ccb5
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 19:35:16 -0300

    wip

--- database/seeders/LandingPageSeeder.php
+++ database/seeders/LandingPageSeeder.php
@@ -20,7 +20,10 @@ class LandingPageSeeder extends Seeder
                 'slug'       => '/',
                 'created_at' => now(),
                 'updated_at' => now(),
-                'blocks'     => '[{"data": {"vertical_space": "xs"}, "type": "design.empty-separator"}, {"data": {"is_active": true, "is_center": true, "bumper_tag": "New", "bumper_icon": null, "bumper_link": null, "bumper_role": "primary", "is_animated": true, "bumper_title": "Warriorfolio is here 🎉 Let\'s fly now! 🚀", "bumper_target": "_self"}, "type": "component.info-bumper"}, {"data": {"active": null}, "type": "hero"},{"data": {"vertical_space": "xs"}, "type": "design.empty-separator"}, {"data": {"active": null}, "type": "courses"}, {"data": {"color": "pink"}, "type": "design.blur-beam"}, {"data": {"active": null}, "type": "projects"}, {"data": {"active": null}, "type": "client"}, {"data": {"active": null}, "type": "newsletter"}, {"data": {"color": "blur"}, "type": "design.blur-beam"}, {"data": {"active": null}, "type": "contact"}, {"data": {"color": "pink"}, "type": "design.blur-beam"}]',
+                'blocks'     => '[{"data": {"vertical_space": "xs"}, "type": "design.empty-separator"}, {"data": {"is_active": true, "is_center": true, "bumper_tag": "New", "bumper_icon": null, "bumper_link": null, "bumper_role": "primary", "is_animated": true, "bumper_title": "Warriorfolio is here 🎉 Let\'s fly now! 🚀", "bumper_target": "_self"}, "type": "component.info-bumper"}, {"data": {"active": null}, "type": "hero"},{"data": {"vertical_space": "xs"}, "type": "design.empty-separator"}, {"data": [], "type": "blog.featured-posts"},
+
+
+                {"data": {"active": null}, "type": "courses"}, {"data": {"color": "pink"}, "type": "design.blur-beam"}, {"data": {"active": null}, "type": "projects"}, {"data": {"active": null}, "type": "client"}, {"data": {"active": null}, "type": "newsletter"}, {"data": {"color": "blur"}, "type": "design.blur-beam"}, {"data": {"active": null}, "type": "contact"}, {"data": {"color": "pink"}, "type": "design.blur-beam"}]',
             ]);
     }
 }
