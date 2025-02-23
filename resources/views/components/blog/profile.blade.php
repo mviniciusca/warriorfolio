a5f2f9287d6ca4db38e8b72e498cb80ff5a29088
Marcos Coelho <mviniciusca@gmail.com>
2025-02-22 23:52:31 -0300

    wip

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -4,10 +4,10 @@
     <section class="my-3 flex items-start border-b border-b-secondary-200 not-italic dark:border-b-secondary-800">
         <div class="mr-3 inline-flex items-start py-4 text-sm">
             @if ($page->user->profile->avatar)
-                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-14 max-w-14 rounded-md object-cover"
+                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-14 max-w-14 rounded-full object-cover"
                     :media="$page->user->profile->avatar" />
             @else
-                <img class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-md object-cover invert-0 dark:invert"
+                <img class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover invert-0 dark:invert"
                     src="{{ asset('img/core/profile-picture.png') }}" />
             @endif
             <div>
