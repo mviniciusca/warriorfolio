f0c63c1b0459b066e2de9265ea95e832eeaca0ef
Marcos Coelho <mviniciusca@gmail.com>
2025-02-23 13:08:11 -0300

    wip

--- resources/views/components/core/alert.blade.php
+++ resources/views/components/core/alert.blade.php
@@ -2,15 +2,13 @@
 
 {{-- Messaging Hub --}}
 @foreach ($alerts as $alert)
-<livewire:alert wire:key="$alert->id" :style='$alert->style' :id="$alert->title" :message="$alert->message"
-    :is_active="$alert->is_active" :is_dismissible="$alert->is_dismissible" :button_text="$alert->button_text" />
+    <livewire:alert wire:key="$alert->id" :style='$alert->style' :id="$alert->title" :message="$alert->message" :is_active="$alert->is_active"
+        :is_dismissible="$alert->is_dismissible" :button_text="$alert->button_text" />
 @endforeach
 
 
 {{-- Core Message : Discovery Mode Alert --}}
-@if($maintenance && ($discovery && auth()->user()))
-<livewire:alert :icon="'earth-outline'" :style="'banner'" :id="'discovery-alert'"
-    :message="'Discovery Mode is Active. The website is Maintenance Mode but still visible to you under your login session.'"
-    :is_active="true" />
+@if ($maintenance && ($discovery && auth()->user()))
+    <livewire:alert :icon="'earth-outline'" :style="'banner'" :id="'discovery-alert'" :message="'Discovery Mode is Active. The website is Maintenance Mode but still visible to you under your login session.'" :is_active="true" />
 @endif
 {{-- End Core Message : Discovery Mode Alert --}}
--- resources/views/livewire/alert.blade.php
+++ resources/views/livewire/alert.blade.php
@@ -3,94 +3,94 @@
 <div>
     <div id="wrapper-{{ $id }}">
 
-        @if($display && $is_active)
+        @if ($display && $is_active)
 
-        @if($style == 'default')
-        <div id="{{ $id }}" tabindex="-1"
-            class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-0 z-50 mx-auto w-full border-t bg-secondary-100 bg-contain bg-center bg-no-repeat dark:border-t-secondary-800 dark:bg-secondary-950">
-            <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2 text-xs">
-                <div class="alert-content">
-                    {!! $message !!}
+            @if ($style == 'default')
+                <div id="{{ $id }}" tabindex="-1"
+                    class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-0 z-50 mx-auto w-full border-t bg-secondary-100 bg-contain bg-center bg-no-repeat dark:border-t-secondary-800 dark:bg-secondary-950">
+                    <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2 text-xs">
+                        <div class="alert-content">
+                            {!! $message !!}
+                        </div>
+                        <div class="my-4">
+                            @if ($is_dismissible)
+
+                                @if ($button_text)
+                                    <x-ui.button wire:click="close">
+                                        {{ $button_text }}
+                                    </x-ui.button>
+                                @else
+                                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
+                                @endif
+
+                            @endif
+                        </div>
+                    </div>
                 </div>
-                <div class="my-4">
-                    @if ($is_dismissible)
-
-                    @if($button_text)
-                    <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
-                        {{ $button_text }}
-                    </x-ui.button>
-                    @else
-                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
-                    @endif
-
-                    @endif
-                </div>
-            </div>
-        </div>
-        @endif
-
-        @if($style == 'bumper')
-        <div id="{{ $id }}" tabindex="-1"
-            class="animate__animated animate__fadeInDown animate__delay-1s fixed top-0 z-50 mx-auto w-full rounded-lg bg-secondary-100 bg-contain bg-center bg-no-repeat text-xs dark:border-b dark:border-b-secondary-800 dark:bg-secondary-950">
-            <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2">
-                <div class="alert-content">
-                    {!! $message !!}
-                </div>
-                <div class="my-4">
-                    @if ($is_dismissible)
-                    @if($button_text)
-                    <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
-                        {{ $button_text }}
-                    </x-ui.button>
-                    @else
-                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
-                    @endif
-                    @endif
-                </div>
-            </div>
-        </div>
-        @endif
-
-
-        @if($style == 'banner')
-        <x-ui.alert icon='notifications-outline'>
-            <div class="alert-content">
-                {!! $message !!}
-            </div>
-            @if($is_dismissible)
-            @if($button_text)
-            <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
-                {{ $button_text }}
-            </x-ui.button>
-            @else
-            <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
             @endif
-            @endif
-        </x-ui.alert>
-        @endif
 
-        @if($style == 'toast')
-        <div id="{{ $id }}" tabindex="-1"
-            class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-5 right-5 z-50 mx-auto max-w-xl bg-secondary-50 text-xs dark:bg-secondary-800">
-            <div
-                class="mx-auto flex max-w-3xl flex-wrap items-center justify-between gap-2 rounded-lg border border-secondary-200 px-3 py-2 dark:border-secondary-700">
-                <div class="alert-content">
-                    {!! $message !!}
+            @if ($style == 'bumper')
+                <div id="{{ $id }}" tabindex="-1"
+                    class="animate__animated animate__fadeInDown animate__delay-1s fixed top-0 z-50 mx-auto w-full rounded-lg bg-secondary-100 bg-contain bg-center bg-no-repeat text-xs dark:border-b dark:border-b-secondary-800 dark:bg-secondary-950">
+                    <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2">
+                        <div class="alert-content">
+                            {!! $message !!}
+                        </div>
+                        <div class="my-4">
+                            @if ($is_dismissible)
+                                @if ($button_text)
+                                    <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
+                                        {{ $button_text }}
+                                    </x-ui.button>
+                                @else
+                                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
+                                @endif
+                            @endif
+                        </div>
+                    </div>
                 </div>
-                <div class="py-2">
+            @endif
+
+
+            @if ($style == 'banner')
+                <x-ui.alert icon='notifications-outline'>
+                    <div class="alert-content">
+                        {!! $message !!}
+                    </div>
                     @if ($is_dismissible)
-                    @if($button_text)
-                    <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
-                        {{ $button_text }}
-                    </x-ui.button>
-                    @else
-                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
+                        @if ($button_text)
+                            <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
+                                {{ $button_text }}
+                            </x-ui.button>
+                        @else
+                            <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
+                        @endif
                     @endif
-                    @endif
+                </x-ui.alert>
+            @endif
+
+            @if ($style == 'toast')
+                <div id="{{ $id }}" tabindex="-1"
+                    class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-5 right-5 z-50 mx-auto max-w-xl bg-secondary-50 text-xs dark:bg-secondary-800">
+                    <div
+                        class="mx-auto flex max-w-3xl flex-wrap items-center justify-between gap-2 rounded-lg border border-secondary-200 px-3 py-2 dark:border-secondary-700">
+                        <div class="alert-content">
+                            {!! $message !!}
+                        </div>
+                        <div class="py-2">
+                            @if ($is_dismissible)
+                                @if ($button_text)
+                                    <x-ui.button class="rounded-md px-2 py-1 text-xs" wire:click="close">
+                                        {{ $button_text }}
+                                    </x-ui.button>
+                                @else
+                                    <x-ui.ionicon class="cursor-pointer" icon="close-outline" wire:click="close" />
+                                @endif
+                            @endif
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        @endif
+            @endif
 
 
         @endif
