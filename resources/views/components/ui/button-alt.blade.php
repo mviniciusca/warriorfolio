01b24e1f0b2b8e78783e4be139fe3ef24bd41f7d
Marcos Coelho <mviniciusca@gmail.com>
2025-02-24 23:37:57 -0300

    wip

--- resources/views/components/ui/button-alt.blade.php
+++ resources/views/components/ui/button-alt.blade.php
@@ -2,7 +2,7 @@
 
 <button type="{{ $type }}"
     {{ $attributes->class([
-        'inline-flex items-center gap-1 rounded-lg border border-secondary-200 bg-white bg-opacity-50 p-3 text-center text-sm font-medium text-primary-500 transition-all duration-100 hover:bg-primary-50 hover:opacity-80 focus:ring-4 focus:ring-primary-300 dark:border-secondary-700 dark:bg-secondary-700 dark:bg-opacity-50 dark:text-white dark:focus:ring-primary-900',
+        'inline-flex items-center gap-1 rounded-lg border border-secondary-200 text-secondary-700 bg-white bg-opacity-50 p-3 text-center text-sm font-medium transition-all duration-100 hover:bg-secondary-50 hover:opacity-80 focus:ring-4 focus:ring-primary-300 dark:border-secondary-700 dark:bg-secondary-700 dark:bg-opacity-50 dark:text-white dark:focus:ring-primary-900',
         'px-2 py-1 text-xs' => $size === 'xs',
         'p-2 text-sm' => $size !== 'xs',
     ]) }}>
