b1934a93f5f1749cbb4d0a884115a89c66889cb3
Marcos Coelho <mviniciusca@gmail.com>
2025-02-25 00:46:31 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/info-bumper.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/info-bumper.blade.php
@@ -19,10 +19,10 @@
             <a target="{{ $bumper_target }}" href="{{ $bumper_link ?? '#' }}">
         @endif
 
-        <div class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between rounded-full bg-secondary-100 px-1 py-1 pr-4 text-sm text-secondary-700 hover:bg-secondary-200 dark:bg-secondary-800 dark:text-white dark:hover:bg-secondary-700"
+        <div class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between rounded-full bg-secondary-50 px-1 py-1 pr-4 text-sm shadow-lg hover:bg-secondary-100 dark:bg-secondary-950 dark:hover:bg-secondary-900"
             role="{{ $bumper_role }}">
             <span
-                class="{{ $is_animated ? 'animate-pulse' : 'animate-none' }} {{ $bumper_role == 'danger' ? 'bg-red-700' : ($bumper_role == 'info' ? 'bg-indigo-500' : 'bg-primary-700') }} mr-3 rounded-full px-4 py-1.5 text-xs text-white">
+                class="{{ $is_animated ? 'animate-pulse' : 'animate-none' }} {{ $bumper_role == 'danger' ? 'bg-red-700' : ($bumper_role == 'info' ? 'bg-indigo-500' : 'bg-primary-500') }} mr-3 rounded-full px-4 py-1.5 text-xs text-white">
                 {!! $bumper_tag !!}
             </span>
             <span class="text-sm font-medium">{!! $bumper_title !!}</span>
