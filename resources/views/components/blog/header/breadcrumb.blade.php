7ac24e08b9f6e31f82014d45d4e250a61b3f073e
Marcos Coelho <mviniciusca@gmail.com>
2025-02-24 23:42:19 -0300

    wip

--- resources/views/components/blog/header/breadcrumb.blade.php
+++ resources/views/components/blog/header/breadcrumb.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     <div class="mb-8 inline-block">
-        <a class="flex items-center gap-1 font-mono transition-all duration-150 hover:-ml-5 hover:opacity-50 active:opacity-20"
+        <a class="flex items-center gap-1 font-mono transition-all duration-150 hover:opacity-50 active:opacity-20 md:hover:-ml-5"
             href="{{ env('APP_URL') . '/blog' }}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6">
