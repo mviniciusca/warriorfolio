8a4e0a8d5a495d3ff2f82d9546e92a1078126a6d
Marcos Coelho <mviniciusca@gmail.com>
2025-02-25 00:46:48 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/info-bumper.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/info-bumper.blade.php
@@ -12,13 +12,10 @@
 ])
 
 @if ($is_active)
-
     <div class="{{ $is_center ? 'text-center' : 'text-left' }} mx-auto max-w-7xl pb-4 pt-16">
-
         @if ($bumper_link && $bumper_target)
             <a target="{{ $bumper_target }}" href="{{ $bumper_link ?? '#' }}">
         @endif
-
         <div class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between rounded-full bg-secondary-50 px-1 py-1 pr-4 text-sm shadow-lg hover:bg-secondary-100 dark:bg-secondary-950 dark:hover:bg-secondary-900"
             role="{{ $bumper_role }}">
             <span
@@ -28,11 +25,8 @@
             <span class="text-sm font-medium">{!! $bumper_title !!}</span>
             <x-ui.ionicon :icon="$bumper_icon" class="ml-3" />
         </div>
-
         @if ($bumper_link && $bumper_target)
             </a>
         @endif
-
     </div>
-
 @endif
