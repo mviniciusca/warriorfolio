aaebd6628f123abec777021d600ff775bb89a2ba
Marcos Coelho <mviniciusca@gmail.com>
2025-02-26 21:19:03 -0300

    wip

--- database/factories/MailFactory.php
+++ database/factories/MailFactory.php
@@ -22,21 +22,21 @@ class MailFactory extends Factory
             'email'   => 'test@warriormail.dev',
             'subject' => 'Welcome to Warriorfolio 2.1.0 🎉',
             'body'    => 'We\'re excited to have you onboard! 🎉
-<br/>
-This is a test email from Warriroflio, making sure everything is working smoothly.
-<p></p>
-✅ What\'s new?
-<p></p>
-Your personal blog is ready! 📝 <br/>
-New Mail View Layout. ✨<br/>
-Dark mode is now the default. 🌙<br/>
-Improved performance and smoother experience. ⚡ <br/>
-Customizable background images across the app! 🌍<br/>
-Start exploring and let us know what you think! If you have any questions, we\'re here to help.
-<p></p>
-Happy exploring! 🚀
-<p></p>
-— Marcos Coelho',
+                                <br/>
+                                This is a test email from Warriroflio, making sure everything is working smoothly.
+                                <p></p>
+                                ✅ What\'s new?
+                                <p></p>
+                                Your personal blog is ready! 📝 <br/>
+                                New Mail View Layout. ✨<br/>
+                                Dark mode is now the default. 🌙<br/>
+                                Improved performance and smoother experience. ⚡ <br/>
+                                Customizable background images across the app! 🌍<br/>
+                                Start exploring and let us know what you think! If you have any questions, we\'re here to help.
+                                <p></p>
+                                Happy exploring! 🚀
+                                <p></p>
+                                — Marcos Coelho',
             'is_read'      => false,
             'is_important' => false,
             'created_at'   => now(),
