92e353595555eb753657e0f7bfe9d12afabc4fed
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 17:59:05 -0300

    wip

--- resources/views/components/header/meta.blade.php
+++ resources/views/components/header/meta.blade.php
@@ -1,27 +1,35 @@
-<title>{{ ($app['name'] ?? env('APP_NAME')) . ' ' . ($meta['meta_title'] ?? '') }}</title>
+<title>{{ ($app['name'] ?? env('APP_NAME', '')) . ($meta['meta_title'] ?? '') }}</title>
+
 @isset($meta['meta_author'])
-    <meta name="author" content="{{ $meta['meta_author'] }}">
+    <meta name="author" content="{{ e($meta['meta_author']) }}">
 @endisset
+
 @isset($meta['meta_keywords'])
-    <meta name="keywords" content="{{ $meta['meta_keywords'] }}">
+    <meta name="keywords" content="{{ e($meta['meta_keywords']) }}">
 @endisset
+
 @isset($meta['meta_description'])
-    <meta name="description" content="{{ $meta['meta_description'] }}">
+    <meta name="description" content="{{ e($meta['meta_description']) }}">
 @endisset
-@isset($design['favicon'])
-    <link rel="icon"
-        href="{{ $design['favicon'] ? asset('storage/' . $design['favicon']) : asset('img/core/favicon.png') }}"
-        type="image/x-icon">
-@endisset
+
+@if (!empty($design['favicon']))
+    <link rel="icon" href="{{ asset('storage/' . $design['favicon']) }}" type="image/x-icon">
+@else
+    <link rel="icon" href="{{ asset('img/core/favicon.png') }}" type="image/x-icon">
+@endif
+
 @isset($google['meta_google_site_verification'])
-    <meta name="google-site-verification" content="{{ $google['meta_google_site_verification'] }}" />
+    <meta name="google-site-verification" content="{{ e($google['meta_google_site_verification']) }}">
 @endisset
+
 @isset($meta['meta_robots'])
-    <meta name="robots" content="{{ $meta['meta_robots'] }}" />
+    <meta name="robots" content="{{ e($meta['meta_robots']) }}">
 @endisset
+
 @isset($google['google_analytics'])
     {!! $google['google_analytics'] !!}
 @endisset
+
 @isset($scripts['header_scripts'])
     {!! $scripts['header_scripts'] !!}
 @endisset
