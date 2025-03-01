5253077a305f11840bfcc15c75b9fa9cca7775c8
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 18:01:59 -0300

    wip

--- resources/views/components/header/google-fonts.blade.php
+++ resources/views/components/header/google-fonts.blade.php
@@ -2,16 +2,13 @@
     {!! data_get($fonts, 'google.fonts_code') !!}
     <style>
         body {
-            font-family: "{{ data_get($fonts, 'google.font_name') }}",
-                sans-serif;
+            font-family: "{{ e(data_get($fonts, 'google.font_name')) }}", sans-serif;
         }
     </style>
 @else
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" as="style">
     <style>
         body {
             font-family: 'Inter', sans-serif;
