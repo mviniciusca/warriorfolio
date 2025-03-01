8cf982bd8fa649ea68cf0967ace00b3db8766244
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 18:31:56 -0300

    wip

--- resources/views/components/maintenance/section.blade.php
+++ resources/views/components/maintenance/section.blade.php
@@ -1,6 +1,6 @@
 @props(['maintenance' => null])
 
-<section class="h-screen w-full bg-secondary-50 text-secondary-600 dark:bg-secondary-900 dark:text-secondary-50">
+<section class="h-screen w-full">
     <div class="mx-auto grid h-full max-w-screen-xl items-center px-4 py-8 lg:px-6 lg:py-16">
         <div class="mt-5 flex flex-col items-center md:flex-row">
             <div class="absolute left-0 right-0 top-0 mt-5 inline-block items-center justify-center text-center">
