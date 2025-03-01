d0ab4d4c45860e91f20032bf928e056ead3aad8a
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 07:40:54 -0300

    wip

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -5,51 +5,10 @@ namespace App\Models;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Z3d0X\FilamentFabricator\Models\Concerns\HandlesPageUrls;
-use Z3d0X\FilamentFabricator\Models\Contracts\Page as Contract;
 
-class Page extends Model implements Contract
+class Page extends \Z3d0X\FilamentFabricator\Models\Page
 {
-    use HandlesPageUrls;
-    use HasFactory;
-
-    public function __construct(array $attributes = [])
-    {
-        if (blank($this->table)) {
-            $this->setTable(config('filament-fabricator.table_name', 'pages'));
-        }
-
-        parent::__construct($attributes);
-    }
-
-    protected $guarded = [];
-
-    protected $casts = [
-        'blocks'    => 'array',
-        'parent_id' => 'integer',
-    ];
-
-    public function parent(): BelongsTo
-    {
-        return $this->belongsTo(static::class, 'parent_id');
-    }
-
-    public function children(): HasMany
-    {
-        return $this->hasMany(static::class, 'parent_id');
-    }
-
-    public function allChildren(): HasMany
-    {
-        return $this->children()
-            ->select('id', 'slug', 'title', 'parent_id')
-            ->with('allChildren:id,slug,title,parent_id');
-    }
-
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
