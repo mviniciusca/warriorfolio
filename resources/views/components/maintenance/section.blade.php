ae4994363cd4aefb5bcf3f2d37669526f834457f
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 18:41:08 -0300

    wip

--- resources/views/components/maintenance/section.blade.php
+++ resources/views/components/maintenance/section.blade.php
@@ -3,7 +3,7 @@
 <section class="maintenance-mode h-screen w-full">
     <div class="mx-auto grid h-full max-w-screen-xl items-center px-4 py-8 lg:px-6 lg:py-16">
         <div class="mt-5 flex flex-col items-center md:flex-row">
-            <div class="absolute left-0 right-0 top-0 mt-5 inline-block items-center justify-center text-center">
+            <div class="absolute left-0 right-0 top-0 mt-12 inline-block items-center justify-center text-center">
                 <livewire:darkMode wire:key='maintenance-dark-mode' />
             </div>
             <div id="col-a"
@@ -24,6 +24,7 @@
             </div>
             @if ($maintenance->is_contact)
                 <div id="col-b" class="w-full p-8 md:w-1/2">
+                    <p class="mb-4 text-base font-bold">{{ __('Get in Touch') }}</p>
                     @livewire('mail.create-mail')
                 </div>
             @endif
