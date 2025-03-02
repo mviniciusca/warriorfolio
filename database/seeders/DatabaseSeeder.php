5a36fe0d76b0aa0114f2939808eb501125a60663
Marcos Coelho <mviniciusca@gmail.com>
2025-03-02 14:39:42 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -56,20 +56,20 @@ class DatabaseSeeder extends Seeder
             'user_id'     => 1,
             'resume'      => 'Welcome to Your First Note!',
             'content'     => 'Hello there! 🌟
-            <p></p>
-This is the beginning of something new—your very first note! Whether you\'re jotting down thoughts, capturing ideas, or simply reminding yourself of something important, this space is yours to explore.
-<p></p>
-Feel free to write freely, without worrying about perfection. Notes can be messy, creative, practical, or poetic—they\'re a reflection of you. Use them to brainstorm, plan your day, track goals, or even vent when needed. Every journey starts with a single step, and this note is yours.
-<p></p>
-Who knows? Looking back someday, you might smile at how far you\'ve come.
-<p></p>
-Here\'s to many more notes ahead! 🚀',
+                            <p></p>
+                            This is the beginning of something new—your very first note! Whether you\'re jotting down thoughts, capturing ideas, or simply reminding yourself of something important, this space is yours to explore.
+                            <p></p>
+                            Feel free to write freely, without worrying about perfection. Notes can be messy, creative, practical, or poetic—they\'re a reflection of you. Use them to brainstorm, plan your day, track goals, or even vent when needed. Every journey starts with a single step, and this note is yours.
+                            <p></p>
+                            Who knows? Looking back someday, you might smile at how far you\'ve come.
+                            <p></p>
+                            Here\'s to many more notes ahead! 🚀',
         ]);
 
         Page::insert([
             'post_id'    => $post->id,
             'user_id'    => 1,
-            'title'      => 'First Note 📝',
+            'title'      => 'Welcome to your First Note 📝',
             'slug'       => 'blog/post/first-note.html',
             'created_at' => now(),
             'updated_at' => now(),
