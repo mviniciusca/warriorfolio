0b210f09926f46aab81b4453b5a6229cee9dffb6
Marcos Coelho <mviniciusca@gmail.com>
2025-03-01 21:47:55 -0300

    wip

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -21,19 +21,19 @@ class ProfileFactory extends Factory
             'job_position' => 'Laravel Product Developer',
             'skills'       => 'PHP, Laravel, Filament, Tailwind, Livewire',
             'about'        => '
-🚀 Welcome to Warriorfolio! Showcase Your Journey! <p></p>
-Welcome to Warriorfolio—your personal space to share your journey, skills, and achievements. Whether you\'re a developer, designer, writer, or entrepreneur, this is where your story comes to life.<p></p>🔹 Bio <p></p>
-Introduce yourself! Share a bit about your background, passions, and what drives you. Warriorfolio is the perfect place to express who you are and what you do.<p></p>
-🎓 Courses & Education<p></p>
-Showcase your learning journey! Add degrees, certifications, bootcamps, or online courses that have helped shape your expertise. Continuous learning is key to growth!
-<p></p>
-🛠️ Skills
-<p></p>
-Highlight the technologies, tools, and frameworks you excel in. Whether it\'s front-end development with HTML, CSS, and JavaScript, back-end expertise in Node.js and Python, or database management and API integrations—this is your space to shine.
-<p></p>
-Start building your Warriorfolio today and let the world see your journey!
-<p></p>
-— The Warriorfolio Team',
+                                🚀 Welcome to Warriorfolio! Showcase Your Journey! <p></p>
+                                Welcome to Warriorfolio—your personal space to share your journey, skills, and achievements. Whether you\'re a developer, designer, writer, or entrepreneur, this is where your story comes to life.<p></p>🔹 Bio <p></p>
+                                Introduce yourself! Share a bit about your background, passions, and what drives you. Warriorfolio is the perfect place to express who you are and what you do.<p></p>
+                                🎓 Courses & Education<p></p>
+                                Showcase your learning journey! Add degrees, certifications, bootcamps, or online courses that have helped shape your expertise. Continuous learning is key to growth!
+                                <p></p>
+                                🛠️ Skills
+                                <p></p>
+                                Highlight the technologies, tools, and frameworks you excel in. Whether it\'s front-end development with HTML, CSS, and JavaScript, back-end expertise in Node.js and Python, or database management and API integrations—this is your space to shine.
+                                <p></p>
+                                Start building your Warriorfolio today and let the world see your journey!
+                                <p></p>
+                                — The Warriorfolio Team',
         ];
     }
 }
