21628a2c34fa17c07d53b2a12ff2b5def67f80f0
Marcos Coelho <mviniciusca@gmail.com>
2025-03-02 14:36:38 -0300

    wip

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Create Note');
+    }
 }
