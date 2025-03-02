5aad50baf4906405223f8b8dac615e31565edabe
Marcos Coelho <mviniciusca@gmail.com>
2025-03-02 13:32:13 -0300

    wip

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -22,22 +22,24 @@ class StatsOverview extends BaseWidget
                 ->value(view('components.badge', ['status' => Maintenance::first()]))
                 ->description(Maintenance::first()->is_active ? __('Maintenance Mode') : __('Website is Live'))
                 ->color('primary'),
-
             Stat::make(__('Mailing List'), Newsletter::counter())
                 ->icon('heroicon-o-megaphone')
+                ->url(route('filament.admin.resources.newsletters.index'))
                 ->color('primary')
                 ->description(__('Subscribers'))
                 ->chart(Newsletter::chartSubscribers()),
-
-            Stat::make(__('Inbox'), Mail::where('is_read', false)->count())
+            Stat::make(__('Inbox'), Mail::where('is_read', false)
+                ->where('is_sent', '=', false)
+                ->count())
+                ->url(route('filament.admin.resources.mails.index'))
                 ->icon('heroicon-o-inbox')
                 ->color('primary')
                 ->description(__('Unread Messages'))
                 ->chart(Mail::chartInbox()),
-
             Stat::make('Projects', Project::published()->count())
                 ->icon('heroicon-o-rocket-launch')
-                ->description('Published Projects')
+                ->url(route('filament.admin.resources.projects.index'))
+                ->description(__('Published Projects'))
                 ->color('primary'),
         ];
     }
