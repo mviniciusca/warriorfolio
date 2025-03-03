3879ed0ac02ff59fb5fd484bd3de7a6521f2ee50
Marcos Coelho <mviniciusca@gmail.com>
2025-03-03 17:35:43 -0300

    wip

---
+++ resources/views/components/blog/post/share.blade.php
@@ -0,0 +1,45 @@
+@aware(['page'])
+
+<div
+    class="flex flex-initial items-center gap-1 border-b border-t border-b-secondary-100 border-t-secondary-100 py-4 dark:border-b-secondary-800 dark:border-t-secondary-800">
+
+    <p class="mr-4 font-mono text-xs uppercase">Share</p>
+
+    <!-- Facebook -->
+    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(url()->current()) }}&quote={{ urlencode($page->title) }}"
+        target="_blank">
+        <x-ui.icon :name="'facebook'" />
+    </a>
+
+    <!-- Twitter -->
+    <a href="https://twitter.com/intent/tweet?text={{ urlencode($page->title) }}&url={{ urlencode(url()->current()) }}"
+        target="_blank">
+        <x-ui.icon :name="'twitter'" />
+    </a>
+
+    <!-- WhatsApp -->
+    <a href="https://wa.me/?text={{ urlencode($page->title . ' - ' . url()->current()) }}" target="_blank">
+        <x-ui.icon :name="'whatsapp'" />
+    </a>
+
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(url()->current()) }}&title={{ urlencode($page->title) }}"
+        target="_blank">
+        <x-ui.icon :name="'linkedin'" />
+    </a>
+
+
+    <!-- Pinterest -->
+    <a href="https://pinterest.com/pin/create/button/?url={{ urlencode(url()->current()) }}&description={{ urlencode($page->title) }}"
+        target="_blank">
+        <x-ui.icon :name="'pinterest'" />
+    </a>
+
+
+    <!-- Reddit -->
+    <a href="https://www.reddit.com/submit?url={{ urlencode(url()->current()) }}&title={{ urlencode($page->title) }}"
+        target="_blank">
+        <x-ui.icon :name="'reddit'" />
+    </a>
+
+
+</div>
--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -1,11 +1,15 @@
-@props(['name' => '', 'href' => '#'])
+@props(['name' => '', 'href' => null])
 
 <div class="inline-flex">
-    <a href="https://{{ $href ?? '#' }}" target="_blank">
-        @if($name)
+    @if ($href)
+        <a href="https://{{ $href }}" target="_blank">
+    @endif
+    @if ($name)
         <ion-icon name="{{ 'logo-' . $name }}"
-            class="mr-4 cursor-pointer text-2xl opacity-70 transition-all duration-100 hover:opacity-90">
+            class="mr-4 text-2xl transition-all duration-100 hover:opacity-90 active:opacity-30">
         </ion-icon>
-    </a>
+        @if ($href)
+            </a>
+        @endif
     @endif
 </div>
