91aa4a878e797e99e8901f44014356bf91d9b322
Marcos Coelho <mviniciusca@gmail.com>
2025-03-03 12:59:21 -0300

    wip

--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -21,13 +21,26 @@ class EditPost extends EditRecord
     {
         return [
             Action::make('view_post')
+                ->hidden(fn ($record) => ! is_null($record?->deleted_at))
                 ->label(__('View Post'))
                 ->url(env('APP_URL').'/'.$this->record->slug)
                 ->openUrlInNewTab(true)
                 ->icon('heroicon-o-arrow-top-right-on-square')
-                ->color('success'),
+                ->size('xs')
+                ->color('info'),
             Actions\DeleteAction::make()
+                ->size('xs')
+                ->label(__('Move to Trash'))
+                ->outlined()
                 ->icon('heroicon-o-trash'),
+            Actions\ForceDeleteAction::make()
+                ->label(__('Delete from Trash'))
+                ->icon('heroicon-o-trash')
+                ->size('xs'),
+            Actions\RestoreAction::make()
+                ->icon('heroicon-o-arrow-uturn-left')
+                ->label(__('Restore'))
+                ->size('xs'),
         ];
     }
 }
--- database/migrations/2023_02_28_215132_create_pages_table.php
+++ database/migrations/2023_02_28_215132_create_pages_table.php
@@ -10,15 +10,32 @@ return new class extends Migration {
     {
         Schema::create(config('filament-fabricator.table_name', 'pages'), function (Blueprint $table) {
             $table->id();
-            $table->string('title')->index();
-            $table->string('slug')->unique();
-            $table->string('layout')->default('default')->index();
-            $table->boolean('is_active')->default(true);
+            $table->string('title')
+                ->index();
+            $table->string('slug')
+                ->unique();
+            $table->string('layout')
+                ->default('default')->index();
+            $table->boolean('is_active')
+                ->default(true);
             $table->json('blocks');
-            $table->string('style')->default('default');
-            $table->foreignId('parent_id')->nullable()->constrained('pages')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignIdFor(Post::class)->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('style')
+                ->default('default');
+            $table->foreignId('parent_id')
+                ->nullable()
+                ->constrained('pages')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignIdFor(Post::class)->nullable()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
             $table->timestamps();
             $table->softDeletes();
         });
