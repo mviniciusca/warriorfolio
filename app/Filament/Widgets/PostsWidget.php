63fd468ba4c222e66e1323a4ddcdefc858df49e1
Marcos Coelho <mviniciusca@gmail.com>
2025-03-03 13:50:55 -0300

    wip

--- app/Filament/Widgets/PostsWidget.php
+++ app/Filament/Widgets/PostsWidget.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Page;
 use App\Models\Post;
 use Filament\Actions\CreateAction;
 use Filament\Forms\Components\Toggle;
@@ -13,10 +14,14 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class PostsWidget extends BaseWidget
 {
     protected static ?int $sort = 0;
+
+    protected static ?string $model = Page::class;
 
     protected int|string|array $columnSpan = 'full';
 
@@ -26,18 +31,20 @@ class PostsWidget extends BaseWidget
             ->striped()
             ->paginated(false)
             ->searchable(false)
-            ->recordClasses(fn (Post $record) => match ($record->is_active) {
+            ->recordClasses(fn (Page $record) => match ($record->is_active) {
                 0       => 'opacity-50 dark:opacity-30',
                 default => null,
             })
             ->heading(__('Latest Posts'))
             ->description(__('Your latest posts from your blog.'))
             ->query(
-                Post::query()
+                Page::query()
+                    ->with('post')
+                    ->where('style', '=', 'blog')
                     ->latest('created_at')
-                    ->with('page')
                     ->take(5)
             )
+            ->recordUrl(fn (Page $record) => route('filament.admin.resources.posts.edit', $record->id))
             ->headerActions(
                 [
                     ViewAction::make('new')
@@ -55,12 +62,11 @@ class PostsWidget extends BaseWidget
                         ->size('xs'),
                 ]
             )
-            ->recordUrl(fn (Post $record) => route('filament.admin.resources.posts.edit', $record->page->id))
             ->columns([
-                TextColumn::make('page.title')
+                TextColumn::make('title')
                     ->label(__('Post Title'))
                     ->limit(50),
-                TextColumn::make('category.name')
+                TextColumn::make('post.category.name')
                     ->label(__('Category'))
                     ->badge(),
                 IconColumn::make('is_active')
@@ -68,6 +74,14 @@ class PostsWidget extends BaseWidget
                     ->alignCenter()
                     ->boolean()
                     ->label(__('Published')),
+            ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
             ]);
     }
 }
