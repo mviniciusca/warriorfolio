2402d93b18a99a4ca4ac829dfd6bc84b48220b63
Marcos Coelho <mviniciusca@gmail.com>
2025-03-07 17:33:42 -0300

    [fix] fix  button padding in layout section

--- resources/views/components/core/layout.blade.php
+++ resources/views/components/core/layout.blade.php
@@ -9,13 +9,15 @@
 <section {{ $attributes->merge(['class' => 'w-full antialiased']) }}>
     <div class="{{ $module_title ? 'py-16' : 'py-8' }} mx-auto max-w-7xl px-8 md:px-12 lg:px-16">
         @if ($module_title)
-            <div class="header-title {{ $button_header ? 'flex justify-between flex-initial ' : '' }} py-4">
-                <p>{{ $module_title }}</p>
-                @if ($button_header)
-                    <x-ui.button-alt :$icon>
-                        {{ $button_header }}
-                    </x-ui.button-alt>
-                @endif
+            <div class="{{ $button_header ? 'flex justify-between items-center flex-initial' : '' }}">
+                <p class="header-title">{{ $module_title }}</p>
+                <span class="pt-8 md:pt-12 lg:pt-16">
+                    @if ($button_header)
+                        <x-ui.button-alt :$icon>
+                            {{ $button_header }}
+                        </x-ui.button-alt>
+                    @endif
+                </span>
             </div>
         @endif
         @if ($module_subtitle)
