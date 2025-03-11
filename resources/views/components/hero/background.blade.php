9b9fa6b82a0526627941d5631a4862d39143a215
Marcos Coelho <mviniciusca@gmail.com>
2025-03-11 07:57:16 -0300

    [ch] pattern background opacity changed to 10 when dark mode is active

--- resources/views/components/hero/background.blade.php
+++ resources/views/components/hero/background.blade.php
@@ -3,7 +3,7 @@
 @if ($hero->hero['is_active'])
     @if ($hero->hero['is_pattern_bg'])
         <div id="hero-background-image"
-            class="{{ $hero->hero['bg_size'] ?? 'bg-cover' }} {{ $hero->hero['bg_position'] ?? 'bg-center' }} {{ $hero->hero['bg_repeat'] ?? 'bg-no-repeat' }} bg-hero absolute -z-50 m-auto h-[600px] w-full bg-fixed opacity-5 filter dark:invert"
+            class="{{ $hero->hero['bg_size'] ?? 'bg-cover' }} {{ $hero->hero['bg_position'] ?? 'bg-center' }} {{ $hero->hero['bg_repeat'] ?? 'bg-no-repeat' }} bg-hero absolute -z-50 m-auto h-[600px] w-full bg-fixed opacity-5 filter dark:opacity-10 dark:invert"
             style="background-image: url('{{ asset('img/core/core-ui-elements/patterns/' . ($hero->hero['pattern_name'] ?? 'cross') . '.svg') }}')">
         </div>
     @elseif ($hero->hero['bg_image'] ?? false)
