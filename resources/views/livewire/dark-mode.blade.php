0e53f3dcca11c27784284bcd76c39c9d5cfd9d79
Marcos Coelho <mviniciusca@gmail.com>
2025-03-11 01:12:49 -0300

    [ch] dark mode component updated

--- resources/views/livewire/dark-mode.blade.php
+++ resources/views/livewire/dark-mode.blade.php
@@ -2,11 +2,12 @@
 <div>
     <label class="relative -mt-5 inline-flex w-auto cursor-pointer items-center">
         <input type="checkbox" wire:model.live='active' class="peer sr-only" checked>
-        <span class="ms-3 text-sm font-medium text-secondary-900 dark:text-secondary-300">
+        <span
+            class="ms-3 flex items-center gap-1 rounded-full bg-transparent p-1 text-sm font-medium text-secondary-900 transition-all duration-100 hover:opacity-30 active:opacity-10 dark:text-secondary-300 dark:hover:opacity-70">
             @if ($active)
-                <x-ui.sun-icon />
+                <p class="font-mono text-xs uppercase">{{ __('Light') }}</p> <x-ui.sun-icon />
             @else
-                <x-ui.moon-icon />
+                <p class="font-mono text-xs uppercase">{{ __('Dark') }}</p> <x-ui.moon-icon />
             @endif
         </span>
     </label>
