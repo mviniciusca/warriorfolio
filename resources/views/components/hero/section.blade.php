932a3108b6fd13abbef8b208b5598fc63855a37d
Marcos Coelho <mviniciusca@gmail.com>
2025-03-11 09:34:16 -0300

    The static slider in the hero section now matches the size when the filled section is active.

--- resources/views/components/hero/section.blade.php
+++ resources/views/components/hero/section.blade.php
@@ -28,7 +28,7 @@
             {{-- Static Slider --}}
             @if (data_get($hero, 'hero.slider_is_active'))
             <div
-                class="mx-auto mt-8 flex w-full flex-wrap justify-center gap-4 md:justify-around lg:mt-12 lg:mb-24 md:mb-12 mb-6">
+                class="mx-auto mt-4 flex w-full flex-wrap justify-center gap-4 md:justify-around lg:mt-6 {{ ($hero->hero['is_filled'] ?? false) ? 'lg:mb-24 md:mb-12 mb-6' : 'mb-1' }}">
                 @foreach (collect($hero->hero['slider_content'])->flatten(1) as $item)
                 <img class="{{ $hero->hero['is_invert'] ? 'dark:invert' : null }} max-h-8 px-4 opacity-60 transition-all duration-100 hover:opacity-100 md:max-h-14"
                     src=" {{ asset('storage/' . $item['slider_image']) }}" />
