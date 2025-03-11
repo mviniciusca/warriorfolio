28f7909b55edb246eb6899684ae9e8af9416f500
Marcos Coelho <mviniciusca@gmail.com>
2025-03-11 15:36:13 -0300

    [ch] Changed the font size of the datetime on the notes post page.

--- resources/views/components/filament-fabricator/page-blocks/blog/post.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/blog/post.blade.php
@@ -3,42 +3,43 @@
 
 <x-core.layout :with_padding="false">
     @if ($page->post->is_active)
-        <main class="antialiased">
-            <div class="mx-auto flex max-w-screen-xl justify-between">
-                <article
-                    class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-3xl">
-                    <x-blog.header.breadcrumb :$title />
-                    <section class="not-format">
-                        <p class="py-2 font-mono uppercase">
-                            {{ \Carbon\Carbon::parse($page->created_at)->format('F d, Y ') . __('in ') . $page->post->category->name }}
-                        </p>
-                        {{-- Title --}}
-                        <h1
-                            class="mt-2 text-3xl font-bold leading-snug tracking-normal dark:text-white/90 lg:mb-4 lg:text-4xl">
-                            {{ $page->title }}
-                        </h1>
-                        {{-- Subtitle --}}
-                        <h2 class="text-md pb-4 leading-tight tracking-tight">
-                            {{ $page->post->resume }}
-                        </h2>
-                        {{-- Info --}}
-                    </section>
-                    {{-- Profile --}}
-                    <x-blog.profile :$page />
-                    {{-- Share --}}
-                    <div class="pb-8 pt-4">
-                        <x-blog.post.share />
-                    </div>
-                    <div class="content py-4 font-serif text-xl leading-relaxed">
-                        {!! $page->post->content !!}
-                    </div>
-                    {{-- Article --}}
-                    <x-blog.post.articles :page="$page" />
-                </article>
-            </div>
-        </main>
-        {{-- Not Found --}}
+    <main class="antialiased">
+        <div class="mx-auto flex max-w-screen-xl justify-between">
+            <article
+                class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-3xl">
+                <x-blog.header.breadcrumb :$title />
+                <section class="not-format">
+                    <p class="py-2 font-mono text-sm uppercase">
+                        {{ \Carbon\Carbon::parse($page->created_at)->format('F d, Y ') . __('in ') .
+                        $page->post->category->name }}
+                    </p>
+                    {{-- Title --}}
+                    <h1
+                        class="mt-2 text-3xl font-bold leading-snug tracking-normal dark:text-white/90 lg:mb-4 lg:text-4xl">
+                        {{ $page->title }}
+                    </h1>
+                    {{-- Subtitle --}}
+                    <h2 class="text-md pb-4 leading-tight tracking-tight">
+                        {{ $page->post->resume }}
+                    </h2>
+                    {{-- Info --}}
+                </section>
+                {{-- Profile --}}
+                <x-blog.profile :$page />
+                {{-- Share --}}
+                <div class="pb-8 pt-4">
+                    <x-blog.post.share />
+                </div>
+                <div class="content py-4 font-serif text-xl leading-relaxed">
+                    {!! $page->post->content !!}
+                </div>
+                {{-- Article --}}
+                <x-blog.post.articles :page="$page" />
+            </article>
+        </div>
+    </main>
+    {{-- Not Found --}}
     @else
-        <x-blog.post.not-found />
+    <x-blog.post.not-found />
     @endif
 </x-core.layout>
