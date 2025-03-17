4961c7e3a8be0f2c111f0cdcb34fe579fa2d45f7
Marcos Coelho <mviniciusca@gmail.com>
2025-03-17 09:53:18 -0300

    [ref] add saturn ui to info bumper component

--- resources/views/components/ui/info-bumper.blade.php
+++ resources/views/components/ui/info-bumper.blade.php
@@ -17,10 +17,10 @@
         @if ($bumper_link && $bumper_target)
             <a target="{{ $bumper_target }}" href="{{ $bumper_link ?? '#' }}">
         @endif
-            <div class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between rounded-full bg-secondary-50 px-1 py-1 pr-4 text-sm shadow-lg hover:bg-secondary-100 dark:bg-secondary-950 dark:hover:bg-secondary-900"
+            <div class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between border border-black/10 dark:border-white/10 rounded-full bg-secondary-50 px-1 py-1 pr-4 text-sm shadow-lg hover:bg-secondary-100 dark:bg-secondary-950 dark:hover:bg-secondary-900"
                 role="{{ $bumper_role }}">
                 <span
-                    class="{{ $is_animated ? 'animate-pulse' : 'animate-none' }} {{ $bumper_role === 'danger' ? 'bg-danger-700' : ($bumper_role === 'info' ? 'bg-info-500' : 'bg-primary-600') }} mr-3 rounded-full px-2 py-1 text-white text-xs">
+                    class="{{ $is_animated ? 'animate-pulse' : 'animate-none' }} {{ $bumper_role === 'danger' ? 'bg-danger-600' : ($bumper_role === 'info' ? 'bg-info-600' : 'saturn-ui-primary') }} mr-2 rounded-full px-2 py-1 text-white text-xs">
                     {!! $bumper_tag !!}
                 </span>
                 <span class="text-sm">{!! $bumper_title !!}</span>
