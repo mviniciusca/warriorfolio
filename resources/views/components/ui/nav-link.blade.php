3e26f8dea44ec83d38a628ced7289c32cbeabe99
Marcos Coelho <mviniciusca@gmail.com>
2025-03-21 19:20:20 -0300

    fix: adjust padding in nav-link button for improved layout

--- resources/views/components/ui/nav-link.blade.php
+++ resources/views/components/ui/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['href' => '#', 'text' => '', 'target' => '_self', 'is_menu_highlighted' => false])
 
 <a href="{{ $href }}" target="{{ $target }}">
-    <button class="flex mt-4 lg:mt-0 flex-initial mr-1 max-h-5 cursor-pointer items-center gap-1 rounded-none py-1 px-2 text-xs uppercase transition-all duration-300 hover:bg-black hover:text-white active:opacity-10 dark:hover:bg-white dark:hover:text-black
+    <button class="flex mt-4 lg:mt-0 flex-initial mr-1 max-h-5 cursor-pointer items-center gap-1 rounded-none py-1 px-1 text-xs uppercase transition-all duration-300 hover:bg-black hover:text-white active:opacity-10 dark:hover:bg-white dark:hover:text-black
         {{ ($is_menu_highlighted ?? false) ? 'bg-white dark:bg-black' : '' }}
         ">
         <p>{!! $text !!}</p>
