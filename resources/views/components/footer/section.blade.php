1d40c34d5801027cb75d4f9ae981f7109a911529
Marcos Coelho <mviniciusca@gmail.com>
2025-03-20 23:49:54 -0300

    fix: improve footer section rendering with null checks and optional properties

--- resources/views/components/footer/section.blade.php
+++ resources/views/components/footer/section.blade.php
@@ -10,18 +10,21 @@ App\View\Components\Footer\Section.php
 --}}
 
 
+
 @props([
+    'data' => null,
+    'module' => null,
     'setting' => null,
     'navigation' => null,
     'social' => null,
+    'is_section_filled_inverted' => false,
 ])
-
-@if ($module->footer)
-    <div class="{{ data_get($data, 'footer.section_fill') ? 'section-filled' : '' }} bg-cover bg-center bg-no-repeat"
-        style="background-image: url({{ asset('img/core/demo/default-bg-footer.png') }})">
-        <x-core.layout :with_padding='false'>
-            {{-- Footer Section --}}
-            <x-footer.content-module :$setting :$navigation :$social />
-        </x-core.layout>
-    </div>
+@if ($module->footer ?? false)
+    <div class="{{ ($data->footer['section_fill'] ?? false) ? 'section-filled' : '' }} bg-cover bg-center bg-no-repeat"
+            style="background-image: url({{ asset('img/core/demo/default-bg-footer.png') }})">
+            <x-core.layout :with_padding='false'>
+                {{-- Footer Section --}}
+                <x-footer.content-module :$setting :$navigation :$social />
+            </x-core.layout>
+        </div>
 @endif
