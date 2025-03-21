c2f7c1b18c3b7a59d93d10ab1fbc4b0d559cc641
Marcos Coelho <mviniciusca@gmail.com>
2025-03-21 11:37:36 -0300

    feat: implement mobile navigation component and update header layout

---
+++ resources/views/components/ui/mobile-navigation.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <button data-collapse-toggle="mobile-menu-2" type="button"
+        class="ml-2 inline-flex items-center rounded-lg p-2 text-sm hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:hover:bg-secondary-950 dark:focus:ring-secondary-800 lg:hidden"
+        aria-controls="mobile-menu-2" aria-expanded="false">
+        <span class="sr-only">{{ __('Open Main Menu') }}</span>
+        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"></path>
+        </svg>
+        <svg class="hidden h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+        </svg>
+    </button>
+</div>
--- resources/views/components/ui/nav-link.blade.php
+++ resources/views/components/ui/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 <a href="{{ $href }}" target="{{ $target }}">
     <button
-        class="flex flex-initial cursor-pointer items-center gap-1 rounded-xl bg-transparent px-2 text-xs uppercase transition-all duration-200 hover:bg-black hover:text-white active:opacity-10 dark:hover:bg-white dark:hover:text-black">
+        class="flex mt-2 lg:mt-0 flex-initial cursor-pointer items-center gap-1 rounded-xl bg-transparent px-2 text-xs uppercase transition-all duration-200 hover:bg-black hover:text-white active:opacity-10 dark:hover:bg-white dark:hover:text-black">
         {!! $text !!}
         @if ($target === '_blank')
             <ion-icon class="h-5 w-5" name="trending-up-outline"></ion-icon>
