ce91025e88edaec85d17ac8b4baf2ac66887c77a
Marcos Coelho <mviniciusca@gmail.com>
2025-03-22 17:01:02 -0300

    fix: enhance chatbox visibility check and update WhatsApp link configuration

--- resources/views/components/ui/chatbox.blade.php
+++ resources/views/components/ui/chatbox.blade.php
@@ -1,11 +1,13 @@
-@if ($chatbox->visible)
+@if ($chatbox->visible ?? false)
     <div>
-        <a target="new"
-            href="{{ env('WHATSAPP_WEB_URL') . env('MOBILE_COUNTRY_CODE') . $chatbox->telephone }}/?text={{ $chatbox->message }}">
+        <a target="_blank"
+            href="{{ config('warriorfolio.whatsapp_web_url', env('WHATSAPP_WEB_URL')) . config('warriorfolio.mobile_country_code', env('MOBILE_COUNTRY_CODE')) . $chatbox->telephone }}/?text={{ $chatbox->message }}">
+
             <div class="chatbox align-center {{ $chatbox->animation_style }} fixed bottom-5 right-5 z-50 flex cursor-pointer items-center overflow-hidden rounded-full p-3 text-center text-primary-50 transition-all duration-100 hover:opacity-80"
-                style="background-color: {{ $chatbox->color }}">
-                <ion-icon class="text-4xl" name="{{ $chatbox->icon }}"></ion-icon>
+                style="background-color: {{ $chatbox->color ?? '#25D366' }}">
+                <ion-icon class="text-3xl" name="{{ $chatbox->icon ?? 'logo-whatsapp' }}"></ion-icon>
             </div>
+
         </a>
     </div>
 @endif
