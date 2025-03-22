4f9e164edc438fb475f078a7f0e2927b229d59fb
Marcos Coelho <mviniciusca@gmail.com>
2025-03-22 16:37:12 -0300

    fix: update alert component styling and add documentation for clarity

--- resources/views/components/ui/alert.blade.php
+++ resources/views/components/ui/alert.blade.php
@@ -1,11 +1,16 @@
+{{--
+
+This alert component is responsible for rendering the alert section of the website.
+Used to display important messages to the user, like success messages, error messages, etc.
+
+--}}
+
 @props(['icon' => null])
 
-<div
-    {{ $attributes->merge([
-        'class' =>
-            'z-50 text-sm mx-auto w-full border border-secondary-200 dark:border-secondary-800 bg-secondary-100 dark:bg-secondary-950',
-    ]) }}>
-    <div class="py-5">
+<div {{ $attributes->merge([
+    'class' => 'z-50 text-sm bg-black dark:text-black text-white mx-auto w-full dark:bg-white',
+]) }}>
+    <div class="py-4">
         <div class="mx-auto max-w-7xl">
             <div class="flex flex-wrap items-center gap-2">
                 @if ($icon)
