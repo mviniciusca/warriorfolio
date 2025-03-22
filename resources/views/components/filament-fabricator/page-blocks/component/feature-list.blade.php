eced7a51afbd53e7a39686fc016b370ff936b778
Marcos Coelho <mviniciusca@gmail.com>
2025-03-22 11:18:41 -0300

    fix: enhance feature list link styling and adjust opacity for better visibility

--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -38,7 +38,7 @@
         class="{{ $is_content_center ? 'text-center justify-center' : 'text-left' }} {{ $columns == 2 ? 'grid grid-cols-1 md:grid-cols-2 gap-4' : '' }} {{ $columns == 3 ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4' : '' }} {{ $columns == 4 ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6' : '' }} grid grid-cols-1 gap-4">
         @foreach ($features as $key => $item)
         @if ($item['link'] ?? false)
-        <a target="{{ $item['is_new_window'] ? '_blank' : '_self' }}" href="{{ $item['link'] ?? '#' }}">
+        <a class="cursor-pointer hover:opacity-50 active:opacity-20 transition-all duration-300" target="{{ $item['is_new_window'] ? '_blank' : '_self' }}" href="{{ $item['link'] ?? '#' }}">
             @endif
                     <div
                         class="
@@ -47,7 +47,7 @@
                          {{ ($is_card_filled && ($is_section_filled_inverted ?? false)) ? 'bg-secondary-900/70 dark:bg-secondary-50/70' : '' }}
                         {{ ($is_border && ($is_section_filled_inverted ?? false)) ? 'border dark:border-secondary-800/20 border-secondary-300/10' : '' }}
                         {{ ($is_border && (!$is_section_filled_inverted ?? false)) ? 'border dark:border-secondary-800/70 border-secondary-300' : '' }}
-                            icon-card duration-100 overflow-hidden rounded-lg bg-contain bg-top bg-no-repeat p-4 transition-all hover:opacity-100">
+                            icon-card duration-300 overflow-hidden rounded-lg bg-contain bg-top bg-no-repeat p-4 transition-all">
                         @if ($is_light_fx)
                             <div class="-mt-4 h-4 animate-pulse bg-contain bg-top bg-no-repeat"
                                 style="background-image: url({{ asset('img/core/core-ui-elements/beams/blur-beam.png') }})">
@@ -60,7 +60,7 @@
                                 name="{{ $item['icon'] }}" />
                         </div>
                         <h4 class="my-1 text-base font-semibold leading-tight">{!! $item['title'] !!}</h4>
-                        <p class="{{ $columns != 1 ? 'py-2' : 'pb-2' }} text-sm leading-snug opacity-70"> {!! $item['description'] !!}</p>
+                        <p class="{{ $columns != 1 ? 'py-2' : 'pb-2' }} text-sm leading-snug opacity-80"> {!! $item['description'] !!}</p>
                     </div>
                     @if ($item['link'] ?? false)
                         </a>
