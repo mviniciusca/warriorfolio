7a652387d0b56b8b38a7c0371c8f9e1d9979d705
Marcos Coelho <mviniciusca@gmail.com>
2025-03-24 15:53:51 -0300

    fix: remove unnecessary max-w-3xl class from hero section subtitle

--- resources/views/components/hero/heading.blade.php
+++ resources/views/components/hero/heading.blade.php
@@ -16,7 +16,7 @@
 @endif
 @if ($hero->hero['section_subtitle'] ?? false)
     <h2
-        class="animate__animated animate__fadeInUp animate__delay-1s mb-6 max-w-3xl hero-subtitle leading-tight tracking-tight shadow-current {{ ($hero->hero['bg_image'] ?? null) && ($hero->hero['is_highlight'] ?? null) ? 'bg-white  dark:bg-black px-2 max-w-auto' : '' }}">
+        class="animate__animated animate__fadeInUp animate__delay-1s mb-6 hero-subtitle leading-tight tracking-tight shadow-current {{ ($hero->hero['bg_image'] ?? null) && ($hero->hero['is_highlight'] ?? null) ? 'bg-white  dark:bg-black px-2 max-w-auto' : '' }}">
         {!! $hero->hero['section_subtitle'] !!}
     </h2>
 @endif
