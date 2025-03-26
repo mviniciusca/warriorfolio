527774b40cafbcc8096c9532734542a5d95e0341
Marcos Coelho <mviniciusca@gmail.com>
2025-03-26 16:36:23 -0300

    fix: adjust avatar size and styling for better display and consistency

--- resources/views/components/blog/profile.blade.php
+++ resources/views/components/blog/profile.blade.php
@@ -4,8 +4,8 @@
     <section class="flex items-start border-b-none border-b-secondary-200/50 dark:border-b-secondary-800/50">
         <div class="mr-3 inline-flex items-start py-4 text-sm">
             @if ($page->user->profile->avatar)
-                <x-curator-glider class="relative mx-auto my-2 mr-4 max-h-14 max-w-14 rounded-full object-cover"
-                    :media="$page->user->profile->avatar" />
+                <x-curator-glider class="relative mx-auto my-2 mr-4 h-16 w-16 rounded-full object-cover"
+                    style="object-fit: cover;" :media="$page->user->profile->avatar" />
             @else
                 <img class="relative mx-auto my-2 mr-4 max-h-16 max-w-16 rounded-full object-cover invert-0 dark:invert"
                     src="{{ asset('img/core/profile-picture.png') }}" />
@@ -17,7 +17,7 @@
                 <p class="mb-2 text-xs">
                     {!! $page->user->profile->job_position !!} - {{ $page->user->profile->localization }}
                 </p>
-                <p> <x-ui.social-network /></p>
+                <p> <x-ui.social-network :justify="'start'" /></p>
             </div>
         </div>
     </section>
