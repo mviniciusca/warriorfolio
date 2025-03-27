b1d244744f0b8a61638bfc401f0e657f8addadae
Marcos Coelho <mviniciusca@gmail.com>
2025-03-26 22:35:06 -0300

    fix: update ionicons version and correct button group markup for consistent structure

--- resources/views/components/hero/button-group.blade.php
+++ resources/views/components/hero/button-group.blade.php
@@ -1,36 +1,36 @@
 @props([
-'title' => null,
-'url' => null,
-'icon' => null,
-'style' => 'filled',
-'target' => '_self',
-'class' => null,
+    'title' => null,
+    'url' => null,
+    'icon' => null,
+    'style' => 'filled',
+    'target' => '_self',
+    'class' => null,
 ])
 
 <div>
     {{-- Filled Button --}}
     @if($style === 'filled' && $title)
-    @if ($url)
-    <a target="{{ $target }}" href="{{ $url }}">
-        @endif
-        <x-ui.button :icon="$icon" :style="'filled'" class="{{ $class }}">
-            {{ $title }}
-        </x-ui.button>
         @if ($url)
-    </a>
-    @endif
+            <a target="{{ $target }}" href="{{ $url }}">
+        @endif
+            <x-ui.button :icon="$icon" :style="'filled'" class="{{ $class }}">
+                {{ $title }}
+            </x-ui.button>
+            @if ($url)
+                </a>
+            @endif
     @endif
 
     {{-- Outlined Button --}}
     @if($style === 'outlined' && $title)
-    @if ($url)
-    <a target="{{ $target }}" href="{{ $url }}">
-        @endif
-        <x-ui.button :icon="$icon" :style="'outlined'" class="{{ $class }}">
-            {{ $title }}
-        </x-ui.button>
         @if ($url)
-    </a>
-    @endif
+            <a target="{{ $target }}" href="{{ $url }}">
+        @endif
+            <x-ui.button :icon="$icon" :style="'outlined'" class="{{ $class }}">
+                {{ $title }}
+            </x-ui.button>
+            @if ($url)
+                </a>
+            @endif
     @endif
 </div>
