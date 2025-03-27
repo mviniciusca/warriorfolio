fc33f75a078c5fca24cc62177281185f85c78a51
Marcos Coelho <mviniciusca@gmail.com>
2025-03-27 18:44:47 -0300

    refactor: enhance documentation in Post model for clarity and detail

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,25 +8,44 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
     protected $guarded = [];
 
+    /**
+     * Get the category that the post belongs to.
+     *
+     * @return BelongsTo
+     */
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
+    /**
+     * Get the pages associated with the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function page()
     {
         return $this->hasMany(Page::class);
     }
 
+    /**
+     * Get the user that owns the post.
+     *
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
