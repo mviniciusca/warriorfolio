deca3730b2e2431e1cb5b6894cbdee2eb0b35d90
Marcos Coelho <mviniciusca@gmail.com>
2025-03-27 07:44:18 -0300

    fix: update ionicons script source to use jsDelivr for consistency and latest version

--- resources/views/components/header/body-scripts.blade.php
+++ resources/views/components/header/body-scripts.blade.php
@@ -12,8 +12,7 @@ There is no data passed to this component.
 @isset($scripts['body_scripts'])
     {!! $scripts['body_scripts'] !!}
 @endisset
-<script type="module" src="https://unpkg.com/ionicons@7.4.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.4.0/dist/ionicons/ionicons.js"></script>
 <script type="module" src="https://cdn.jsdelivr.net/npm/ionicons@latest/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://cdn.jsdelivr.net/npm/ionicons@latest/dist/ionicons/ionicons.js"></script>
 @filamentScripts
 @vite('resources/js/app.js')
