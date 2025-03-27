673ba94c22be86c8bb3ee5a485f0c9b9d2166c22
Marcos Coelho <mviniciusca@gmail.com>
2025-03-27 09:48:14 -0300

    refactor: improve button component structure for readability and maintainability

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -18,16 +18,19 @@ Props:
     'icon' => 'chevron-forward-sharp',
     'style' => 'filled',
     'color' => false,
-    'is_section_filled_inverted' => null
+    'is_section_filled_inverted' => null,
 ])
 
 {{-- Filled with Color Primary bg-primary-600 text-white Button --}}
 
-@if($style === 'filled' && $color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-primary-600 hover:bg-primary-700 text-xs md:text-sm dark:hover:bg-primary-500
-        transition-all duration-300 text-white dark:bg-primary-700 dark:text-white flex flex-wrap gap-2 border border-transparent
-        py-2 px-4 rounded-md items-center justify-center active:opacity-30'
-    ]) }}>
+@if ($style === 'filled' && $color)
+    <button
+        {{ $attributes->class([
+            'bg-primary-600 hover:bg-primary-700 text-xs md:text-sm dark:hover:bg-primary-500
+                        transition-all duration-300 text-white dark:bg-primary-700 dark:text-white flex flex-wrap gap-2 border border-transparent
+                        py-2 px-4 rounded-md items-center justify-center active:opacity-30',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
@@ -39,10 +42,13 @@ Props:
 
 {{--  Outlined with Color Primary bg-primary-600 text-white Button --}}
 
-@if($style === 'outlined' && $color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-primary-600/15 hover:bg-primary-700/20 text-xs md:text-sm hover:text-white
-        dark:hover:text-white dark:hover:bg-primary-500/20 transition-all duration-300 dark:bg-primary-700/20 text-white dark:text-white flex flex-wrap gap-2 border border-primary-600/50 py-2 px-4 dark:border-primary-700/50 rounded-md items-center hover:bg-primary-700/20 active:opacity-30 justify-center'
-    ]) }}>
+@if ($style === 'outlined' && $color)
+    <button
+        {{ $attributes->class([
+            'bg-primary-600/15 hover:bg-primary-700/20 text-xs md:text-sm hover:text-white
+                        dark:hover:text-white dark:hover:bg-primary-500/20 transition-all duration-300 dark:bg-primary-700/20 text-white dark:text-white flex flex-wrap gap-2 border border-primary-600/50 py-2 px-4 dark:border-primary-700/50 rounded-md items-center hover:bg-primary-700/20 active:opacity-30 justify-center',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
@@ -52,11 +58,13 @@ Props:
     </button>
 @endif
 
-
 {{-- Filled Button --}}
-@if($style === 'filled' && !$is_section_filled_inverted && !$color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-black hover:bg-secondary-700  text-xs md:text-sm dark:hover:bg-secondary-200 transition-all duration-300 text-white dark:bg-white dark:text-black flex flex-wrap gap-2 border border-transparent py-2 px-4 rounded-md items-center active:opacity-30 justify-center'
-    ]) }}>
+@if ($style === 'filled' && !$is_section_filled_inverted && !$color)
+    <button
+        {{ $attributes->class([
+            'bg-black hover:bg-secondary-700 text-xs md:text-sm dark:hover:bg-secondary-200 transition-all duration-300 text-white dark:bg-white dark:text-black flex flex-wrap gap-2 border border-transparent py-2 px-4 rounded-md items-center active:opacity-30 justify-center',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
@@ -67,11 +75,14 @@ Props:
 @endif
 
 {{-- Filled Button Inverted --}}
-@if($style === 'filled' && $is_section_filled_inverted && !$color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-white hover:bg-secondary-200 text-xs md:text-sm dark:hover:bg-black
-        transition-all duration-300 text-black dark:bg-black dark:text-white flex flex-wrap gap-2 border border-black/50
-        py-2 px-4 rounded-md items-center active:opacity-30 justify-center'
-    ]) }}>
+@if ($style === 'filled' && $is_section_filled_inverted && !$color)
+    <button
+        {{ $attributes->class([
+            'bg-white hover:bg-secondary-200 text-xs md:text-sm dark:hover:bg-black
+                        transition-all duration-300 text-black dark:bg-black dark:text-white flex flex-wrap gap-2 border border-black/50
+                        py-2 px-4 rounded-md items-center active:opacity-30 justify-center',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
@@ -82,12 +93,15 @@ Props:
 @endif
 
 {{-- Outlined Button --}}
-@if($style === 'outlined' && !$is_section_filled_inverted && !$color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-white/15 hover:bg-black/5 text-xs md:text-sm hover:text-black
-        dark:hover:text-white dark:hover:bg-white/5 transition-all duration-300 dark:bg-black/10 text-black dark:text-white
-        flex flex-wrap gap-2 border border-black/50 py-2 px-4 dark:border-white/10 rounded-md items-center
-        hover:bg-secondary-200/20 active:opacity-30 justify-center'
-    ])}}>
+@if ($style === 'outlined' && !$is_section_filled_inverted && !$color)
+    <button
+        {{ $attributes->class([
+            'bg-white/15 hover:bg-black/5 text-xs md:text-sm hover:text-black
+                        dark:hover:text-white dark:hover:bg-white/5 transition-all duration-300 dark:bg-black/10 text-black dark:text-white
+                        flex flex-wrap gap-2 border border-black/50 py-2 px-4 dark:border-white/10 rounded-md items-center
+                        hover:bg-secondary-200/20 active:opacity-30 justify-center',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
@@ -98,12 +112,15 @@ Props:
 @endif
 
 {{-- Outlined Button Inverted --}}
-@if($style === 'outlined' && $is_section_filled_inverted && !$color)
-    <button type="{{ $type }}" {{ $attributes->class(['bg-black/15 hover:bg-white/5 hover:text-black
-        dark:hover:text-white dark:hover:bg-black/5 transition-all duration-300 dark:bg-white/10 text-black dark:text-white
-        flex flex-wrap gap-2 border border-white/50 py-2 px-4 dark:border-black/50 rounded-md items-center
-        hover:bg-secondary-200/20 active:opacity-30 justify-center'
-    ])}}>
+@if ($style === 'outlined' && $is_section_filled_inverted && !$color)
+    <button
+        {{ $attributes->class([
+            'bg-black/15 hover:bg-white/5 hover:text-black
+                        dark:hover:text-white dark:hover:bg-black/5 transition-all duration-300 dark:bg-white/10 text-black dark:text-white
+                        flex flex-wrap gap-2 border border-white/50 py-2 px-4 dark:border-black/50 rounded-md items-center
+                        hover:bg-secondary-200/20 active:opacity-30 justify-center',
+        ]) }}
+        type="{{ $type }}">
         <span>
             {{ $slot }}
         </span>
