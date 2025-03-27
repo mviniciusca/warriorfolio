0eddf3b118b11a9e0bb542788ee0705d88fba735
Marcos Coelho <mviniciusca@gmail.com>
2025-03-27 09:49:22 -0300

    fix: reorder attributes in hero button-group component for improved readability

--- resources/views/components/themes/hero/default-theme.blade.php
+++ resources/views/components/themes/hero/default-theme.blade.php
@@ -11,25 +11,24 @@ App\View\Components\Themes\Hero\Default-Theme.php
 
 @props(['hero', 'icon' => null, 'browser_border_url' => $hero->hero['browser_border_url'] ?? null])
 
-<section id="default-hero-section" class="relative w-full py-8">
+<section class="relative w-full py-8" id="default-hero-section">
     <div class="container mx-auto">
         {{-- Hero Section Heading --}}
         <div class="mx-auto mb-4 grid items-center justify-center text-center">
             <x-hero.heading :$hero />
         </div>
         {{-- Hero Section Button --}}
-        <div class="animate__animated animate__fadeInUp animate__delay-1s flex justify-center gap-4 mb-8 w-full">
+        <div class="animate__animated animate__fadeInUp animate__delay-1s mb-8 flex w-full justify-center gap-4">
             @foreach ($hero->hero['buttons'] as $button)
-                <x-hero.button-group :color="$button['color'] ?? null" :title="$button['button_title']"
-                    :style="$button['button_style']" :target="$button['button_target']" :url="$button['button_url']"
-                    :icon="$button['icon']" />
+                <x-hero.button-group :color="$button['color'] ?? null" :icon="$button['icon']" :style="$button['button_style']" :target="$button['button_target']"
+                    :title="$button['button_title']" :url="$button['button_url']" />
             @endforeach
         </div>
         {{-- Social Network --}}
-        @if(($hero->hero['social_network_module_is_active'] ?? null) ?? false)
-            <div id="social-network-module" class="mb-8">
-                <x-ui.social-network size="big" :justify="'center'"
-                    class="animate__animated animate__fadeInDown animate__delay-1s" />
+        @if ($hero->hero['social_network_module_is_active'] ?? (null ?? false))
+            <div class="mb-8" id="social-network-module">
+                <x-ui.social-network :justify="'center'" class="animate__animated animate__fadeInDown animate__delay-1s"
+                    size="big" />
             </div>
         @endif
         {{-- Hero Section: Featured Image --}}
