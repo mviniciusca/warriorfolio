f326dd4a9552cdda76a3a2e30926511e546e3e42
Marcos Coelho <mviniciusca@gmail.com>
2025-03-27 21:13:29 -0300

    refactor: improve HTML structure and formatting in default layout

--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -1,36 +1,36 @@
 @aware(['page'])
 
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
-    class="{{ session('theme', 'light') == 'dark' ? 'dark' : '' }}">
+<html class="{{ session('theme', 'light') == 'dark' ? 'dark' : '' }}"
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <x-core.modules.header />
-</head>
+    <head>
+        <x-core.modules.header />
+    </head>
 
-<body class="default-theme app-core flex min-h-screen flex-col" id="app-wrapper">
+    <body class="default-theme app-core flex min-h-screen flex-col" id="app-wrapper">
 
-    @if (!$maintenance || ($discovery && auth()->user()))
-        <x-core.modules.over />
-        @if ($header_core)
-            <header class="header-section" id="app-header">
-                <x-header.section />
-            </header>
+        @if (!$maintenance || ($discovery && auth()->user()))
+            <x-core.modules.over />
+            @if ($header_core)
+                <header class="header-section" id="app-header">
+                    <x-header.section />
+                </header>
+            @endif
+            <main class="main-section flex-grow" id="app-main">
+                <x-filament-fabricator::page-blocks :blocks="$page->blocks" />
+                <x-core.modules.section />
+            </main>
+            @if ($footer_core)
+                <footer class="footer-section mt-auto" id="app-footer">
+                    <x-footer.section />
+                </footer>
+            @endif
         @endif
-        <main class="main-section flex-grow" id="app-main">
-            <x-filament-fabricator::page-blocks :blocks="$page->blocks" />
-            <x-core.modules.section />
-        </main>
-        @if ($footer_core)
-            <footer class="footer-section mt-auto" id="app-footer">
-                <x-footer.section />
-            </footer>
+        @if ($maintenance && (!$discovery || !auth()->user()))
+            <x-maintenance.section />
         @endif
-    @endif
-    @if ($maintenance && (!$discovery || !auth()->user()))
-        <x-maintenance.section />
-    @endif
-    <x-header.body-scripts />
-</body>
+        <x-header.body-scripts />
+    </body>
 
 </html>
