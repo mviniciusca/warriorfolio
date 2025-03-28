cfef2ae3e614c7a2bf18682c6dd6834ed0d7fad6
Marcos Coelho <mviniciusca@gmail.com>
2025-03-28 15:56:55 -0300

    refactor: format newsletter section title for improved readability

--- resources/views/components/newsletter/section.blade.php
+++ resources/views/components/newsletter/section.blade.php
@@ -18,7 +18,9 @@ App\Livewire\Newsletter.php
             class="bg-dots relative items-center overflow-hidden rounded-3xl border border-secondary-100 bg-white px-8 py-16 text-left dark:border-secondary-900 dark:bg-black">
             <div class="absolute inset-0 z-0 bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-70"></div>
             <div class="relative z-10 mx-auto max-w-md">
-                <h2 class="header-title newsletter-header-title font-bold">{!! $data->layout->mailing['section_title'] ?? null !!}</h2>
+                <h2 class="header-title newsletter-header-title font-bold">
+                    {!! $data->layout->mailing['section_title'] ?? null !!}
+                </h2>
                 <p class="my-2 text-sm">{!! $data->layout->mailing['section_subtitle'] ?? null !!}</p>
                 <div class="mx-auto mt-1 grid">
                     <livewire:newsletter />
