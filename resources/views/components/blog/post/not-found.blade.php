d3fbc597d5c74aaba12d04725b6c70ef4d66f135
Marcos Coelho <mviniciusca@gmail.com>
2025-04-01 20:37:20 -0300

    refactor: enhance 404 not found page layout and update button component for improved navigation

--- resources/views/components/blog/post/not-found.blade.php
+++ resources/views/components/blog/post/not-found.blade.php
@@ -2,7 +2,7 @@
     'title' => 'Page Not Found',
 ])
 <section class="flex w-full items-center justify-center">
-    <div class="mx-auto text-center">
+    <div class="mx-auto grid justify-center text-center">
         <svg class="mx-auto my-8 size-6" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg">
             <path d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" stroke-linecap="round"
@@ -15,9 +15,10 @@
         <p class="mb-4 text-sm text-black dark:text-white">
             {{ __('Page not found.') }}
         </p>
-        <a class="inline-block rounded border border-black px-3 py-1 text-sm text-black hover:bg-black hover:text-white dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black"
-            href="{{ url('/') }}">
-            {{ __('Go Home') }}
-        </a>
+        <x-ui.button style="outlined">
+            <a href="{{ url()->previous() }}">
+                {{ __('Go Back') }}
+            </a>
+        </x-ui.button>
     </div>
 </section>
