765503dfcac8b7e6134db834f0625b716ef25592
Marcos Coelho <mviniciusca@gmail.com>
2025-04-03 13:46:10 -0300

    refactor: optimize background image handling for dark and light modes with improved height management

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -21,36 +21,33 @@ default image will be displayed.
     'bg_animation' => $design['animation'] ?? false,
     'bg_dark_mode' => $design['dark_mode_background_image'] ?? false,
     'bg_light_mode' => $design['background_image'] ?? false,
+    'bg_height' => 'h-[900px]',
 ])
 
 @if ($bg_is_active ?? false)
 
-    {{--  Overlay --}}
-    <div
-        class="absolute -z-50 -mt-12 h-[1080px] w-full bg-gradient-to-b from-gray-900/50 via-transparent to-gray-900/50 dark:from-gray-800/50 dark:via-transparent dark:to-gray-800/50">
-    </div>
-    <div
-        class="absolute bottom-0 -z-40 -mb-10 h-96 w-full bg-gradient-to-b from-transparent via-white to-white dark:via-secondary-950 dark:to-secondary-950">
-    </div>
-
     {{-- Dark Mode Background --}}
-    @if ($bg_dark_mode)
+    @if ($bg_dark_mode && $bg_light_mode)
+        {{-- Dark Mode Background --}}
         <img alt="Dark Mode Background"
-            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 hidden h-[1080px] w-full object-cover dark:block"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} {{ $bg_height }} -z-50 hidden w-full object-cover dark:block"
             id="dark-background" src="{{ asset('storage/' . $bg_dark_mode) }}">
         <img alt="Light Mode Background"
-            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 block h-[1080px] w-full object-cover dark:hidden"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} {{ $bg_height }} -z-50 block w-full object-cover dark:hidden"
             id="dark-background" src="{{ asset('storage/' . $bg_light_mode) }}">
     @endif
-    {{-- Light Mode Background / Default Background --}}
+    {{-- Light Mode Background --}}
     @if ($bg_light_mode && !$bg_dark_mode)
         <img alt="Light Mode Background"
-            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 block h-[1080px] w-full object-cover"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} {{ $bg_height }} -z-50 block w-full object-cover"
             id="default-background" src="{{ asset('storage/' . $bg_light_mode) }}">
-    @else
+    @endif
+
+    {{-- Default Background --}}
+    @if (!$bg_light_mode && !$bg_dark_mode)
         <img alt="Default Background"
-            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} absolute -z-50 -mt-52 h-[1080px] w-full animate-pulse bg-auto bg-scroll bg-center bg-no-repeat object-cover blur-xl backdrop-filter"
-            id="default-background" src="{{ asset('img/core/bg/saturn-ui-more-lights.png') }}">
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} {{ $bg_height }} -z-50 block w-full object-cover"
+            id="default-background" src="{{ asset('storage/' . 'img/core/bg/saturn-ui-more-lights.png') }}">
     @endif
 
 @endif
