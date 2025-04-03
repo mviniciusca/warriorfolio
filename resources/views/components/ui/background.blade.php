38de0cf9e7c30de503991d05c5af36814b22fede
Marcos Coelho <mviniciusca@gmail.com>
2025-04-03 12:47:14 -0300

    refactor: streamline background image handling with improved variable management for dark and light modes

--- resources/views/components/ui/background.blade.php
+++ resources/views/components/ui/background.blade.php
@@ -11,10 +11,21 @@ default image will be displayed.
 
 @props([
     'design' => null,
+    'bg_is_active' => $design['background_image_visibility'] ?? false,
+    'bg_size' => $design['background_image_size'] ?? 'bg-auto',
+    'bg_position' => $design['background_image_position'] ?? 'bg-top',
+    'bg_repeat' => $design['background_image_repeat'] ?? 'bg-no-repeat',
+    'bg_grayscale' => $design['bg_grayscale'] ?? false,
+    'bg_blur' => $design['bg_blur'] ?? 'blur-none',
+    'bg_fixed' => $design['bg_fixed'] ?? false,
+    'bg_animation' => $design['animation'] ?? false,
+    'bg_dark_mode' => $design['dark_mode_background_image'] ?? false,
+    'bg_light_mode' => $design['background_image'] ?? false,
 ])
 
-@if ($design['background_image_visibility'] ?? false)
+@if ($bg_is_active ?? false)
 
+    {{--  Overlay --}}
     <div
         class="absolute -z-50 -mt-12 h-[1080px] w-full bg-gradient-to-b from-gray-900/50 via-transparent to-gray-900/50 dark:from-gray-800/50 dark:via-transparent dark:to-gray-800/50">
     </div>
@@ -22,20 +33,23 @@ default image will be displayed.
         class="absolute bottom-0 -z-40 -mb-10 h-96 w-full bg-gradient-to-b from-transparent via-white to-white dark:via-secondary-950 dark:to-secondary-950">
     </div>
 
-    @if (($design['background_image'] ?? false) || ($design['dark_mode_background_image'] ?? false))
-        @if ($design['dark_mode_background_image'] ?? false)
-            <img alt="Dark Mode Background"
-                class="{{ $design['animation'] ? 'animate-opacity opacity-10' : '' }} {{ $design['background_image_size'] ?? 'bg-auto' }} {{ $design['background_image_position'] ?? 'bg-top' }} {{ $design['background_image_repeat'] ?? 'bg-no-repeat' }} {{ $design['bg_grayscale'] ? 'grayscale' : 'grayscale-0' }} {{ $design['bg_blur'] ?? '' }} {{ $design['bg_fixed'] ? 'fixed' : 'absolute' }} -z-50 -mt-12 hidden h-[1080px] w-full object-cover dark:block"
-                id="dark-background" src="{{ asset('storage/' . $design['dark_mode_background_image']) }}">
-        @endif
-        @if ($design['background_image'] ?? false)
-            <img alt="Light Mode Background"
-                class="{{ $design['animation'] ? 'animate-opacity opacity-10' : '' }} {{ $design['background_image_size'] ?? 'bg-auto' }} {{ $design['background_image_position'] ?? 'bg-top' }} {{ $design['background_image_repeat'] ?? 'bg-no-repeat' }} {{ $design['bg_grayscale'] ? 'grayscale' : 'grayscale-0' }} {{ $design['bg_blur'] ?? '' }} {{ $design['bg_fixed'] ? 'fixed' : 'absolute' }} -z-50 -mt-12 block h-[1080px] w-full object-cover dark:hidden"
-                id="default-background" src="{{ asset('storage/' . $design['background_image']) }}">
-        @endif
+    {{-- Dark Mode Background --}}
+    @if ($bg_dark_mode)
+        <img alt="Dark Mode Background"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 hidden h-[1080px] w-full object-cover dark:block"
+            id="dark-background" src="{{ asset('storage/' . $bg_dark_mode) }}">
+        <img alt="Light Mode Background"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 block h-[1080px] w-full object-cover dark:hidden"
+            id="dark-background" src="{{ asset('storage/' . $bg_light_mode) }}">
+    @endif
+    {{-- Light Mode Background / Default Background --}}
+    @if ($bg_light_mode && !$bg_dark_mode)
+        <img alt="Light Mode Background"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} {{ $bg_size }} {{ $bg_position }} {{ $bg_repeat }} {{ $bg_grayscale ? 'grayscale' : 'grayscale-0' }} {{ $bg_blur }} {{ $bg_fixed ? 'fixed' : 'absolute' }} -z-50 -mt-12 block h-[1080px] w-full object-cover"
+            id="default-background" src="{{ asset('storage/' . $bg_light_mode) }}">
     @else
         <img alt="Default Background"
-            class="{{ $design['animation'] ? 'animate-opacity opacity-10' : '' }} absolute -z-50 -mt-52 h-[1080px] w-full animate-pulse bg-auto bg-scroll bg-center bg-no-repeat object-cover blur-xl backdrop-filter"
+            class="{{ $bg_animation ? 'animate-opacity opacity-10' : '' }} absolute -z-50 -mt-52 h-[1080px] w-full animate-pulse bg-auto bg-scroll bg-center bg-no-repeat object-cover blur-xl backdrop-filter"
             id="default-background" src="{{ asset('img/core/bg/saturn-ui-more-lights.png') }}">
     @endif
 
