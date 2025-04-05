99e8595c4159cc71f71404bfd231b8a074ac93f6
Marcos Coelho <mviniciusca@gmail.com>
2025-04-05 17:07:06 -0300

    refactor: update ProjectResource table method to use ImageColumn and adjust relationships

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -21,6 +21,7 @@ use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Table;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
@@ -208,26 +209,23 @@ class ProjectResource extends Resource
     {
         return $table
             ->query(Page::query()
-                ->where('style', 'project'))
+                ->where('style', '=', 'project'))
             ->columns([
-                CuratorColumn::make('image_cover')
-                    ->size(80)
-                    ->label('Cover'),
+                ImageColumn::make('project.image_cover')
+                    ->label('')
+                    ->size(100),
                 Tables\Columns\TextColumn::make('project.page.title')
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('category.name')
+                Tables\Columns\TextColumn::make('project.category.name')
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\ToggleColumn::make('is_active')
+                Tables\Columns\ToggleColumn::make('project.is_active')
                     ->label('Published')
                     ->sortable()
                     ->alignCenter(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->alignRight()
-                    ->sortable()
-                    ->dateTime('d/m/Y H:i'),
-            ])->defaultSort('created_at', 'desc')
+            ])
+            ->defaultSort('created_at', 'desc')
             ->filters([
                 //
             ])
@@ -257,131 +255,3 @@ class ProjectResource extends Resource
         ];
     }
 }
-
-//  ->schema([
-//                 Group::make()
-//                     ->columnSpan(3)
-//                     ->schema([
-//                         Section::make(__('Project Information'))
-//                             ->icon('heroicon-o-information-circle')
-//                             ->columns(2)
-//                             ->schema([
-//                                 Hidden::make('user_id')
-//                                     ->dehydrated()
-//                                     ->default(Auth::user()?->id),
-//                                 Hidden::make('style')
-//                                     ->default('project'),
-//                                 Hidden::make('blocks')
-//                                     ->dehydrated()
-//                                     ->default([['data' => [], 'type' => 'portfolio.project']])
-//                                     ->required(),
-//                                 TextInput::make('title')
-//                                     ->label(__(__('Project Title')))
-//                                     ->live(true)
-//                                     ->required()
-//                                     ->autofocus()
-//                                     ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug',
-//                                         'project/'.Str::slug($state).'.html'))
-
-//                                     ->unique('pages', 'title', ignoreRecord: true)
-//                                     ->maxLength(200)
-//                                     ->prefixIcon('heroicon-o-rocket-launch')
-//                                     ->helperText(__('The name of the project. Max: 200 characters.')),
-//                                 TextInput::make('slug')
-//                                     ->disabled()
-//                                     ->dehydrated()
-//                                     ->placeholder('generated automatically')
-//                                     ->helperText(__('This is automatically generated from the title.'))
-//                                     ->prefixIcon('heroicon-o-link')
-//                                     ->label(__('Slug')),
-//                             ]),
-//                     ]),
-
-//                 Group::make()
-//                     ->relationship('project')
-//                     ->schema([
-//                         Textarea::make('short_description')
-//                             ->columnSpanFull()
-//                             ->rows(2)
-//                             ->maxLength(500)
-//                             ->helperText(__('A short description of the project. Max: 500 characters.'))
-//                             ->label(__('Short Description (Optional)')),
-//                         RichEditor::make('content')
-//                             ->fileAttachmentsDirectory('project/attachments')
-//                             ->columnSpanFull()
-//                             ->maxLength(5000)
-//                             ->helperText(__('The content of the project. Max: 5000 characters.'))
-//                             ->label(__('Content (Optional)')),
-//                         TextInput::make('external_link')
-//                             ->label(__('External Link (Optional)'))
-//                             ->placeholder('https://example.com')
-//                             ->url()
-//                             ->maxLength(255)
-//                             ->prefixIcon('heroicon-o-link')
-//                             ->helperText(__('The external link of the project. Max: 255 characters.'))
-//                             ->columnSpanFull(),
-//                         FileUpload::make('image_cover')
-//                             ->directory('public/project')
-//                             ->required()
-//                             ->maxFiles(1)
-//                             ->imageEditor()
-//                             ->imageEditorAspectRatios(
-//                                 [
-//                                     '16:9' => '16:9',
-//                                     '4:3'  => '4:3',
-//                                     '1:1'  => '1:1',
-//                                 ]
-//                             )
-//                             ->label(__('Cover Image')),
-//                         Section::make('Category')
-//                             ->icon('heroicon-o-tag')
-//                             ->schema([
-//                                 Select::make('category_id')
-//                                     ->relationship('category', 'name')
-//                                     ->options(Category::
-//                                     where('is_project', '=', true)
-//                                         ->pluck('name', 'id'))
-//                                     ->helperText(__('Project Category'))
-//                                     ->createOptionUsing(fn (array $data) => Category::create($data + [
-//                                         'is_blog'    => false,
-//                                         'is_project' => true,
-//                                     ])->getKey())
-//                                     ->createOptionForm([
-//                                         Section::make('Fast Create Category.')
-//                                             ->icon('heroicon-o-tag')
-//                                             ->description('Create a new category for the project. Edit other settings of this category later.')
-//                                             ->schema([
-//                                                 TextInput::make('name')
-//                                                     ->lazy()
-//                                                     ->unique()
-//                                                     ->maxLength(200)
-//                                                     ->helperText('The name of the category. Max: 200 characters.')
-//                                                     ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
-//                                                     ->required()
-//                                                     ->label(__('Category Name')),
-//                                                 TextInput::make('slug')
-//                                                     ->disabled()
-//                                                     ->unique()
-//                                                     ->maxLength(200)
-//                                                     ->dehydrated()
-//                                                     ->placeholder(__('generated automatically'))
-//                                                     ->label('Slug'),
-//                                             ])->columns(2),
-//                                     ])
-//                                     ->optionsLimit(10)
-//                                     ->searchable()
-//                                     ->required(),
-//                             ]),
-//                         Section::make('Visibility')
-//                             ->icon('heroicon-o-eye')
-//                             ->schema([
-//                                 Toggle::make('is_active')
-//                                     ->label('Published')
-//                                     ->helperText(__('Project visibility.'))
-//                                     ->default(true)
-//                                     ->required()
-//                                     ->inline(),
-//                             ]),
-//                     ]),
-//             ])
-//             ->columns(4);
