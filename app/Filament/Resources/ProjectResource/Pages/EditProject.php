aa8670156abdf052a03a229d1fea8e59215b4e53
Marcos Coelho <mviniciusca@gmail.com>
2025-04-05 18:08:21 -0300

    refactor: add view and visit actions to EditProject header for improved navigation

--- app/Filament/Resources/ProjectResource/Pages/EditProject.php
+++ app/Filament/Resources/ProjectResource/Pages/EditProject.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\ProjectResource\Pages;
 
 use App\Filament\Resources\ProjectResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
+use Z3d0X\FilamentFabricator\Models\Contracts\Page as PageContract;
 
 class EditProject extends EditRecord
 {
@@ -13,6 +16,20 @@ class EditProject extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ViewAction::make()
+                ->visible(config('filament-fabricator.enable-view-page')),
+            Action::make('visit')
+                ->label(__('filament-fabricator::page-resource.actions.visit'))
+                ->url(function () {
+                    /** @var PageContract $page */
+                    $page = $this->getRecord();
+
+                    return FilamentFabricator::getPageUrlFromId($page->id);
+                })
+                ->icon('heroicon-o-arrow-top-right-on-square')
+                ->openUrlInNewTab()
+                ->color('success')
+                ->visible(config('filament-fabricator.routing.enabled')),
             Actions\DeleteAction::make(),
         ];
     }
