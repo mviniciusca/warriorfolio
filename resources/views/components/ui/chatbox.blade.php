83881e6ce517db1f43ce4c2a9c34be2320ba44a5
Marcos Coelho <mviniciusca@gmail.com>
2025-04-08 18:28:43 -0300

    refactor: adjust anchor tag formatting in chatbox component

--- resources/views/components/ui/chatbox.blade.php
+++ resources/views/components/ui/chatbox.blade.php
@@ -1,7 +1,7 @@
 @if ($chatbox->visible ?? false)
     <div>
-        <a target="_blank"
-            href="{{ config('warriorfolio.whatsapp_web_url', env('WHATSAPP_WEB_URL')) . config('warriorfolio.mobile_country_code', env('MOBILE_COUNTRY_CODE')) . $chatbox->telephone }}/?text={{ $chatbox->message }}">
+        <a href="{{ config('warriorfolio.whatsapp_web_url', env('WHATSAPP_WEB_URL')) . config('warriorfolio.mobile_country_code', env('MOBILE_COUNTRY_CODE')) . $chatbox->telephone }}/?text={{ $chatbox->message }}"
+            target="_blank">
 
             <div class="chatbox align-center {{ $chatbox->animation_style }} fixed bottom-5 right-5 z-50 flex cursor-pointer items-center overflow-hidden rounded-full p-3 text-center text-primary-50 transition-all duration-100 hover:opacity-80"
                 style="background-color: {{ $chatbox->color ?? '#25D366' }}">
