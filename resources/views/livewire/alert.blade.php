1d84cb0a19f18b3d93d538ebe0275116c0686f6d
Marcos Coelho <mviniciusca@gmail.com>
2025-04-14 18:43:24 -0300

    refactor: reorder attributes in alert component for consistency

--- resources/views/livewire/alert.blade.php
+++ resources/views/livewire/alert.blade.php
@@ -6,8 +6,8 @@
         @if ($display && $is_active)
 
             @if ($style == 'default')
-                <div id="{{ $id }}" tabindex="-1"
-                    class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-0 z-50 mx-auto w-full border-t bg-secondary-100 bg-contain bg-center bg-no-repeat dark:border-t-secondary-800 dark:bg-secondary-950">
+                <div class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-0 z-50 mx-auto w-full border-t bg-secondary-100 bg-contain bg-center bg-no-repeat dark:border-t-secondary-800 dark:bg-secondary-950"
+                    id="{{ $id }}" tabindex="-1">
                     <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2 text-xs">
                         <div class="alert-content">
                             {!! $message !!}
@@ -30,8 +30,8 @@
             @endif
 
             @if ($style == 'bumper')
-                <div id="{{ $id }}" tabindex="-1"
-                    class="animate__animated animate__fadeInDown animate__delay-1s fixed top-0 z-50 mx-auto w-full rounded-lg bg-secondary-100 bg-contain bg-center bg-no-repeat text-xs dark:border-b dark:border-b-secondary-800 dark:bg-secondary-950">
+                <div class="animate__animated animate__fadeInDown animate__delay-1s fixed top-0 z-50 mx-auto w-full rounded-lg bg-secondary-100 bg-contain bg-center bg-no-repeat text-xs dark:border-b dark:border-b-secondary-800 dark:bg-secondary-950"
+                    id="{{ $id }}" tabindex="-1">
                     <div class="mx-auto flex max-w-7xl items-center justify-between gap-2 px-5 py-2">
                         <div class="alert-content">
                             {!! $message !!}
@@ -51,7 +51,6 @@
                 </div>
             @endif
 
-
             @if ($style == 'banner')
                 <x-ui.alert icon='notifications-outline'>
                     <div class="alert-content">
@@ -70,8 +69,8 @@
             @endif
 
             @if ($style == 'toast')
-                <div id="{{ $id }}" tabindex="-1"
-                    class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-5 right-5 z-50 mx-auto max-w-xl bg-secondary-50 text-xs dark:bg-secondary-800">
+                <div class="animate__animated animate__fadeInUp animate__delay-1s fixed bottom-5 right-5 z-50 mx-auto max-w-xl bg-secondary-50 text-xs dark:bg-secondary-800"
+                    id="{{ $id }}" tabindex="-1">
                     <div
                         class="mx-auto flex max-w-3xl flex-wrap items-center justify-between gap-2 rounded-lg border border-secondary-200 px-3 py-2 dark:border-secondary-700">
                         <div class="alert-content">
@@ -91,7 +90,6 @@
                     </div>
                 </div>
             @endif
-
 
         @endif
 
