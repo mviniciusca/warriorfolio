df1f84807ed5a34e942379baa927b29758d718b2
Marcos Coelho <mviniciusca@gmail.com>
2025-04-26 14:59:06 -0300

    refactor: improve layout structure and formatting in blog header and notes components

--- resources/views/components/blog/header/app.blade.php
+++ resources/views/components/blog/header/app.blade.php
@@ -1,32 +1,32 @@
 @props(['data' => null])
 
 @if ($data->blog['is_active'] ?? false)
-    <section id="blog-header-components" class="w-full overflow-hidden rounded-xl">
-        <div class="flex flex-initial items-center gap-5">
-            @if ($data->blog['is_logo_active'] ?? true)
-                <div
-                    class="hidden h-16 w-16 overflow-hidden rounded-lg bg-secondary-100/50 object-cover object-center p-2 dark:bg-secondary-900 sm:block sm:h-24 sm:w-24">
-                    @if ($data->blog['logo'] ?? false)
-                        <x-curator-glider
-                            class="h-20 w-20 rounded-md object-cover object-center {{ ($data->blog['is_invert_logo'] ?? false) ? 'invert-0 dark:invert' : ''}}"
-                            style="aspect-ratio: 1/1; object-fit: cover;" :media="$data->blog['logo']" />
-                    @else
-                        <img class="invert dark:invert-0" src="{{ asset('img/core/profile-picture.png') }}" alt="warriorfolio-logo">
-                    @endif
-                </div>
+<section id="blog-header-components" class="w-full overflow-hidden rounded-xl">
+    <div class="flex flex-initial items-center gap-5">
+        @if ($data->blog['is_logo_active'] ?? true)
+        <div
+            class="hidden h-16 w-16 overflow-hidden rounded-lg bg-secondary-100/50 object-cover object-center p-2 dark:bg-secondary-900 sm:block sm:h-24 sm:w-24">
+            @if ($data->blog['logo'] ?? false)
+            <x-curator-glider
+                class="h-20 w-20 rounded-md object-cover object-center {{ ($data->blog['is_invert_logo'] ?? false) ? 'invert-0 dark:invert' : ''}}"
+                style="aspect-ratio: 1/1; object-fit: cover;" :media="$data->blog['logo']" />
+            @else
+            <img class="invert dark:invert-0" src="{{ asset('img/core/profile-picture.png') }}" alt="warriorfolio-logo">
             @endif
-            <div id="blog-name-group">
-                @isset($data->blog['name'])
-                    <p class="text-3xl dg font-bold leading-none tracking-tighter">
-                        {!! $data->blog['name'] !!}
-                    </p>
-                @endisset
-                @isset($data->blog['description'])
-                    <p class="py-2 text-sm opacity-70">
-                        {!! $data->blog['description'] !!}
-                    </p>
-                @endisset
-            </div>
         </div>
-    </section>
+        @endif
+        <div id="blog-name-group">
+            @isset($data->blog['name'])
+            <p class="text-2xl font-medium leading-none">
+                {!! $data->blog['name'] !!}
+            </p>
+            @endisset
+            @isset($data->blog['description'])
+            <p class="py-1 text-sm opacity-80">
+                {!! $data->blog['description'] !!}
+            </p>
+            @endisset
+        </div>
+    </div>
+</section>
 @endif
--- resources/views/components/blog/notes.blade.php
+++ resources/views/components/blog/notes.blade.php
@@ -1,24 +1,25 @@
 {{-- Notes (Blog) --}}
 
 @if($module_blog ?? false)
-    <x-core.layout :with_padding="false">
-        <div class="mx-auto my-16">
-            <div class="flex flex-wrap">
-                <div class="order-2 mx-auto w-full lg:order-1 lg:w-3/4 lg:pr-24">
-                    <x-blog.homepage />
-                </div>
-                <div class="order-1 hidden w-full lg:order-2 lg:block lg:w-1/4 lg:pl-8">
-                    <aside class="px-4 text-center">
-                        @if(($profile_widget_status ?? null) ?? true)
-                            <x-about.profile :$profile />
-                        @endif
-                        <x-blog.widget.counter />
-                    </aside>
-                </div>
+<x-core.layout :with_padding="false">
+    <div class="mx-auto my-16">
+        <div class="flex flex-wrap">
+            <div class="order-2 mx-auto w-full lg:order-1 lg:w-3/4 lg:pr-24">
+                <x-blog.homepage />
             </div>
-    </x-core.layout>
+            <div class="order-1 hidden w-full lg:order-2 lg:block lg:w-1/4 lg:pl-8">
+                <aside class="px-4 text-center">
+                    @if(($profile_widget_status ?? null) ?? true)
+                    <x-about.profile :$profile />
+                    @endif
+                    <x-blog.widget.counter />
+                </aside>
+            </div>
+        </div>
+</x-core.layout>
 @else
-    <div class="grid h-screen items-center">
-        <x-ui.empty-section :message="__('Ops! Notes not available yet!')" :auth="__('Activate Notes on your Dashboard!')" />
-    </div>
+<div class="grid h-screen items-center">
+    <x-ui.empty-section :message="__('Ops! Notes not available yet!')"
+        :auth="__('Activate Notes on your Dashboard!')" />
+</div>
 @endif
