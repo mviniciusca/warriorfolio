318de3a6c81032d5702fcff221b424810e65f732
Marcos Coelho <mviniciusca@gmail.com>
2025-04-26 11:39:55 -0300

    feat: add feature card component for improved modularity and reusability

--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -3,27 +3,24 @@
 'is_active' => null,
 'is_animated' => null,
 'is_border' => null,
-'is_center' => null, // deprecated v2.1.5
 'is_color_icon' => null,
 'is_content_center' => null,
 'is_filled' => null,
 'is_light_fx' => null,
 'button_header' => null,
 'button_url' => null,
-'columns' => null ?? 3,
-'features' => null,
+'columns' => 3,
+'features' => [],
 'module_subtitle' => null,
 'module_title' => null,
 'with_padding' => true,
 'is_heading_active' => true,
 'is_card_filled' => null,
-'is_filled_inverted' => null,
 'is_section_filled_inverted' => null,
-'is_card_hidden' => false // false by default
 ])
 
 @if ($is_active)
-<x-core.layout :$is_filled :$is_section_filled_inverted :$button_header :$button_url :$is_center>
+<x-core.layout :$is_filled :$is_section_filled_inverted :$button_header :$button_url>
     @if (($module_title || $module_subtitle) && $is_heading_active)
     <div>
         <x-slot name="module_title" class="py-8 text-center">
@@ -34,36 +31,19 @@
         </x-slot>
     </div>
     @endif
-    <div class="mx-auto{{ $with_padding ? 'py-8' : 'py-12'}}">
-        <div
-            class="{{ $is_content_center ? 'text-center justify-center' : 'text-left' }} {{ $columns == 2 ? 'grid grid-cols-1 md:grid-cols-2 gap-4' : '' }} {{ $columns == 3 ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4' : '' }} {{ $columns == 4 ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6' : '' }} grid grid-cols-1 gap-4">
-            @foreach ($features as $key => $item)
+
+    <div class="mx-auto {{ $with_padding ? 'py-8' : 'py-12' }}">
+        <div @class([ 'grid gap-4' , 'text-center'=> $is_content_center,
+            'text-left' => !$is_content_center,
+            'grid-cols-1' => true,
+            'md:grid-cols-2' => $columns == 2,
+            'sm:grid-cols-2 md:grid-cols-3' => $columns == 3,
+            'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4' => $columns == 4,
+            ])>
+            @foreach ($features as $item)
             @if(!isset($item['is_card_hidden']) || empty($item['is_card_hidden']))
-            @if ($item['link'] ?? false)
-            <a class="block w-full h-full cursor-pointer transition-all duration-300 hover:opacity-50 active:opacity-20"
-                target="{{ $item['is_new_window'] ? '_blank' : '_self' }}" href="{{ $item['link'] ?? '#' }}">
-                @endif
-                <div
-                    class="flex flex-col justify-between w-full h-full {{ $columns != 1 ? 'min-h-24' : 'min-h-10' }} {{ ($is_card_filled && (!$is_section_filled_inverted ?? false)) ? 'dark:bg-secondary-900/70 bg-secondary-50/70' : '' }} {{ ($is_card_filled && ($is_section_filled_inverted ?? false)) ? 'bg-secondary-900/70 dark:bg-secondary-50/70' : '' }} {{ ($is_border && ($is_section_filled_inverted ?? false)) ? 'border dark:border-secondary-800/20 border-secondary-300/10' : '' }} {{ ($is_border && (!$is_section_filled_inverted ?? false)) ? 'border dark:border-secondary-800/70 border-secondary-300' : '' }} icon-card duration-300 overflow-hidden rounded-lg bg-contain bg-top bg-no-repeat p-4 transition-all">
-                    @if ($is_light_fx)
-                    <div class="-mt-4 h-4 animate-pulse bg-contain bg-top bg-no-repeat"
-                        style="background-image: url({{ asset('img/core/core-ui-elements/beams/blur-beam.png') }})">
-                    </div>
-                    @endif
-                    <div class="flex {{ $is_content_center ? 'justify-center' : 'justify-start' }} -mt-0 mb-2">
-                        <ion-icon
-                            class="{{ $is_animated ? 'animate-pulse' : '' }} {{ $is_color_icon ? 'text-primary-500 dark:text-primary-600' : '' }} h-8 w-8 rounded-full p-1"
-                            name="{{ $item['icon'] }}" />
-                    </div>
-                    <div class="flex flex-col flex-grow">
-                        <h4 class="my-1 text-base font-semibold leading-tight">{!! $item['title'] !!}</h4>
-                        <p class="{{ $columns != 1 ? 'py-2' : 'pb-2' }} text-sm leading-snug opacity-80"> {!!
-                            $item['description'] !!}</p>
-                    </div>
-                </div>
-                @if ($item['link'] ?? false)
-            </a>
-            @endif
+            <x-filament-fabricator.page-blocks.component.feature-card :$item :$columns :$is_content_center :$is_animated
+                :$is_color_icon :$is_card_filled :$is_section_filled_inverted :$is_border :$is_light_fx />
             @endif
             @endforeach
         </div>
