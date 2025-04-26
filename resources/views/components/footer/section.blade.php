efaf161b74138e0ec4b2e4eb0aaaae7ab699c709
Marcos Coelho <mviniciusca@gmail.com>
2025-04-26 11:55:50 -0300

    fix: adjust footer text size for improved readability

--- resources/views/components/footer/section.blade.php
+++ resources/views/components/footer/section.blade.php
@@ -25,7 +25,7 @@ App\View\Components\Footer\Section.php
                 <div class="flex flex-col items-center justify-between space-y-4 md:flex-row md:space-y-0">
                     <div class="flex items-center space-x-4">
                         <x-ui.logo />
-                        <span class="text-sm">
+                        <span class="text-xs">
                             © {{ date('Y') }} {{ data_get($data, 'application.name', config('app.name',
                             env('APP_NAME'))) }}
                         </span>
