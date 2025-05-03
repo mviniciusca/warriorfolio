64508a0d1898fa3327731103112dca24e1580f7b
Marcos Coelho <mviniciusca@gmail.com>
2025-05-03 10:38:19 -0300

    feat: implement is_active property in ScrollUp component and update view conditionally

--- app/View/Components/Ui/ScrollUp.php
+++ app/View/Components/Ui/ScrollUp.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components\Ui;
 
+use App\Models\Setting;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -11,9 +12,11 @@ class ScrollUp extends Component
     /**
      * Create a new component instance.
      */
+    public bool $is_active;
+
     public function __construct()
     {
-        //
+        $this->is_active = Setting::first(['config'])->config['scroll_up_is_active'] ?? true;
     }
 
     /**
--- resources/views/components/ui/scroll-up.blade.php
+++ resources/views/components/ui/scroll-up.blade.php
@@ -1,3 +1,4 @@
+@if($is_active)
 <div>
     <button id="scrollUp"
         class="fixed h-10 w-10 items-center justify-center border bottom-10 right-4 z-50 hidden bg-secondary-50 border-secondary-300 text-secondary-500 dark:bg-secondary-800 dark:text-white dark:border-secondary-700 rounded-full transition duration-300 hover:opacity-30 active:opacity-85">
@@ -22,3 +23,4 @@
 
 
 </div>
+@endif
