8c1c915226d2ca3bd8beb55cfb87fb584a4f71e2
Marcos Coelho <mviniciusca@gmail.com>
2025-05-06 17:14:45 -0300

    fix: streamline Project Section layout by adjusting props and improving structure

--- resources/views/components/project/section.blade.php
+++ resources/views/components/project/section.blade.php
@@ -11,10 +11,8 @@ App\View\Components\Project\Section.php
 
 
 @if($is_active)
-<x-core.layout :$button_header :$button_url :$is_filled :$is_section_filled_inverted :$with_padding
-    :module_name="'portfolio'">
-    <x-core.layout.section-header :$button_header :$button_url :$is_centered :$title :$subtitle
-        :$is_section_filled_inverted :$is_heading_visible />
+<x-core.layout :$is_filled :$with_padding :$module_name :$button_header :$button_url :$is_centered :$title :$subtitle
+    :$is_section_filled_inverted :$is_heading_visible :$button_icon :$module_slug>
     <section class="my-12" id="projects">
         @livewire('portfolio.gallery', ['is_section_filled_inverted' => $is_section_filled_inverted])
     </section>
