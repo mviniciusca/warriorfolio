f1951b8ec94640cb6358bd78720894819a03c341
Marcos Coelho <mviniciusca@gmail.com>
2025-05-06 12:01:29 -0300

    refactor: streamline Section component by removing unused properties and enhancing layout logic

--- app/View/Components/Project/Section.php
+++ app/View/Components/Project/Section.php
@@ -5,18 +5,21 @@ namespace App\View\Components\Project;
 use App\Models\Layout;
 use App\Models\Module;
 use App\Models\Project;
+use App\Traits\SectionLoader;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Section extends Component
 {
+    use SectionLoader;
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        //
+        $this->loadSection('portfolio');
     }
 
     /**
@@ -25,14 +28,8 @@ class Section extends Component
     public function render(): View|Closure|string
     {
         return view('components.project.section', [
-            'module'   => Module::first(['portfolio']),
-            'data'     => Layout::first(['portfolio']),
-            'projects' => Project::query()
-                ->where('is_active', true)
-                ->with('category')
-                ->orderBy('created_at', 'desc')
-                ->take(12)
-                ->get(),
+            'module' => Module::first(['portfolio']),
+
         ]);
     }
 }
