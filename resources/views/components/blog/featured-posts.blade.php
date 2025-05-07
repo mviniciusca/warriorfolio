7a1eb9990571fb9ea0f6dd9ba9ddcc3ef28d0c7f
Marcos Coelho <mviniciusca@gmail.com>
2025-05-07 10:57:12 -0300

    fix: refactor featured-posts component to improve props organization and readability

--- resources/views/components/blog/featured-posts.blade.php
+++ resources/views/components/blog/featured-posts.blade.php
@@ -1,33 +1,20 @@
-@props(['posts', 'module_blog', 'info'])
+@props([
+'posts',
+'module_blog',
+'info',
+'title' => $info['header_title'] ?? null,
+'subtitle' => $info['header_subtitle'] ?? null,
+'is_heading_visible' => $info['is_heading_visible'] ?? false,
+'button_header' => $info['button'] ?? null,
+'button_url' => $info['button_url'] ?? null,
+
+])
 
 @if ($posts && $module_blog)
 @if ($info['module_is_active'] ?? false)
-
 <x-core.layout :with_padding="false">
+    <x-core.layout.section-header :$title :$subtitle :$is_heading_visible :$button_header :$button_url />
     <div class="my-12">
-        <x-slot name="module_title">
-
-            @if($info['header_title'] && ($info['is_heading_visible'] ?? true))
-            {{ $info['header_title'] ?? null}}
-            @endif
-
-            @if ($info['button'] ?? null)
-            <x-slot name="button_header">
-                <a href="{{ $info['button_url'] ?? null }}">
-                    {{ $info['button'] ?? null }}
-                </a>
-            </x-slot>
-            @endif
-
-        </x-slot>
-
-        @if($info['header_subtitle'] && ($info['is_heading_visible'] ?? true) ?? false)
-        <x-slot name="module_subtitle">
-            {{ $info['header_subtitle'] ?? null }}
-        </x-slot>
-        @endif
-
-
         <section class="mt-8">
             <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                 @foreach ($posts as $item)
