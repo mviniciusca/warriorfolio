c5359a9a10881712ef94631729e26b7d138c5dac
Marcos Coelho <mviniciusca@gmail.com>
2025-05-15 12:38:21 -0300

    fix: remove empty script tag from scroll-up component for cleaner code

--- resources/views/components/ui/scroll-up.blade.php
+++ resources/views/components/ui/scroll-up.blade.php
@@ -4,11 +4,5 @@
         class="fixed h-10 w-10 items-center justify-center border bottom-24 right-7 z-50 hidden bg-secondary-50 border-secondary-300 text-secondary-500 dark:bg-secondary-800 dark:text-white dark:border-secondary-700 rounded-md transition duration-300 hover:opacity-30 active:opacity-85">
         <x-ui.ionicon icon="chevron-up-outline" class="w-6 h-6" />
     </button>
-
-    <script>
-
-    </script>
-
-
 </div>
 @endif
