8a9ca202a920dd199971b457c763047f2ced3a02
Marcos Coelho <mviniciusca@gmail.com>
2025-05-19 11:16:40 -0300

    fix: refactor About component to include is_active and is_coupled properties

--- app/View/Components/Themes/Juno/About.php
+++ app/View/Components/Themes/Juno/About.php
@@ -2,7 +2,9 @@
 
 namespace App\View\Components\Themes\Juno;
 
+use App\Filament\Fabricator\PageBlocks\Component\Module;
 use App\Models\Course;
+use App\Models\Section;
 use App\Models\User;
 use Closure;
 use Illuminate\Contracts\View\View;
@@ -10,8 +12,13 @@ use Illuminate\View\Component;
 
 class About extends Component
 {
-    public function __construct()
-    {
+    public function __construct(
+        public bool $is_active = false,
+        public bool $is_coupled = false
+        ) {
+        $module = Section::where('slug', 'about-me')->first();
+        $this->is_active = $module->is_active;
+        $this->is_coupled = $module->is_coupled;
     }
 
     public function render(): View|Closure|string
--- resources/views/components/themes/juno/about.blade.php
+++ resources/views/components/themes/juno/about.blade.php
@@ -1,5 +1,6 @@
 @props(['data', 'courses'])
 
+@if($is_active)
 <div>
     <x-themes.juno.partials.header subtitle="Full-stack developer exploring the digital frontier" title="About Me" />
 
@@ -17,13 +18,13 @@
                 </div>
 
                 @if ($data->profile->is_downloadable && $data->profile->document)
-                    <div class="mt-6 flex justify-start">
-                        <a href="{{ asset('storage/' . $data->profile->document) }}" target="_blank">
-                            <x-ui.button :icon="'document-text-outline'" size='sm' style='outlined'>
-                                {{ __('Download Resume') }}
-                            </x-ui.button>
-                        </a>
-                    </div>
+                <div class="mt-6 flex justify-start">
+                    <a href="{{ asset('storage/' . $data->profile->document) }}" target="_blank">
+                        <x-ui.button :icon="'document-text-outline'" size='sm' style='outlined'>
+                            {{ __('Download Resume') }}
+                        </x-ui.button>
+                    </a>
+                </div>
                 @endif
             </div>
         </div>
@@ -42,40 +43,41 @@
 
                 <div class="relative h-full space-y-6 py-2">
                     @forelse ($courses as $course)
-                        <div class="relative pl-6">
-                            <!-- Marcador circular -->
-                            <div
-                                class="absolute -left-[0.3125rem] top-[1.125rem] h-2.5 w-2.5 rounded-full border-2 border-secondary-200 bg-white dark:border-secondary-800 dark:bg-secondary-900">
-                            </div>
-                            <div class="flex-1">
-                                <span class="text-xs font-medium text-secondary-500 dark:text-secondary-400">
-                                    {{ $course->start_date->format('m/Y') }} - {{ $course->end_date->format('m/Y') }}
-                                </span>
-                                <h3 class="mt-1 text-sm font-medium text-secondary-900 dark:text-secondary-100">
-                                    {{ $course->name }}
-                                </h3>
-                                <p class="mb-1 text-xs text-secondary-600 dark:text-secondary-400">
-                                    {{ $course->institution }}
-                                </p>
-                                <span @class([
-                                    'mt-0 -mx-2 inline-flex items-center rounded-lg px-2 py-0.5 text-xs font-medium',
-                                    $course->getStatusColor()['border'],
-                                    $course->getStatusColor()['text'],
-                                    $course->getStatusColor()['dark_border'],
-                                    $course->getStatusColor()['dark_text'],
+                    <div class="relative pl-6">
+                        <!-- Marcador circular -->
+                        <div
+                            class="absolute -left-[0.3125rem] top-[1.125rem] h-2.5 w-2.5 rounded-full border-2 border-secondary-200 bg-white dark:border-secondary-800 dark:bg-secondary-900">
+                        </div>
+                        <div class="flex-1">
+                            <span class="text-xs font-medium text-secondary-500 dark:text-secondary-400">
+                                {{ $course->start_date->format('m/Y') }} - {{ $course->end_date->format('m/Y') }}
+                            </span>
+                            <h3 class="mt-1 text-sm font-medium text-secondary-900 dark:text-secondary-100">
+                                {{ $course->name }}
+                            </h3>
+                            <p class="mb-1 text-xs text-secondary-600 dark:text-secondary-400">
+                                {{ $course->institution }}
+                            </p>
+                            <span
+                                @class([ 'mt-0 -mx-2 inline-flex items-center rounded-lg px-2 py-0.5 text-xs font-medium'
+                                , $course->getStatusColor()['border'],
+                                $course->getStatusColor()['text'],
+                                $course->getStatusColor()['dark_border'],
+                                $course->getStatusColor()['dark_text'],
                                 ])>
-                                    <x-ui.ionicon :icon="$course->getStatusIcon()" class="mr-1" />
-                                    {{ $course->getStatusLabel() }}
-                                </span>
-                            </div>
+                                <x-ui.ionicon :icon="$course->getStatusIcon()" class="mr-1" />
+                                {{ $course->getStatusLabel() }}
+                            </span>
                         </div>
+                    </div>
                     @empty
-                        <div class="text-center text-sm text-secondary-600 dark:text-secondary-400">
-                            {{ __('No courses added yet.') }}
-                        </div>
+                    <div class="text-center text-sm text-secondary-600 dark:text-secondary-400">
+                        {{ __('No courses added yet.') }}
+                    </div>
                     @endforelse
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endif
