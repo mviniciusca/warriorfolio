98b3aa45aafa1f48d1489ed878f1197253172a3b
Marcos Coelho <mviniciusca@gmail.com>
2025-05-19 12:31:27 -0300

    fix: remove DeleteAction from header actions in EditSection

--- app/Filament/Resources/SectionResource/Pages/EditSection.php
+++ app/Filament/Resources/SectionResource/Pages/EditSection.php
@@ -13,7 +13,7 @@ class EditSection extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            //
         ];
     }
 }
