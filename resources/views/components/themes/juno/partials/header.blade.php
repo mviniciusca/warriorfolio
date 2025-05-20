3de0a329095cc6d95275d929264efdeb8140bb56
Marcos Coelho <mviniciusca@gmail.com>
2025-05-20 15:04:28 -0300

    refactor: replace anchor tag with button component for consistent styling in header

--- resources/views/components/themes/juno/partials/header.blade.php
+++ resources/views/components/themes/juno/partials/header.blade.php
@@ -8,12 +8,8 @@
         @endif
     </div>
     @if ($button && $buttonUrl)
-    <a class="inline-flex items-center gap-2 rounded border border-secondary-300 bg-white px-4 py-1 text-xs font-medium text-secondary-900 transition-colors hover:bg-secondary-50 focus:outline-none focus:ring-2 focus:ring-secondary-500 focus:ring-offset-2 dark:border-secondary-800 dark:bg-secondary-950 dark:text-secondary-100 dark:hover:bg-secondary-900"
-        href="{{ $buttonUrl }}">
-        @if ($buttonIcon)
-        <x-ui.ionicon class="h-4 w-4" icon="{{ $buttonIcon }}" />
-        @endif
+    <x-ui.button style="outlined" :href="$buttonUrl" :icon="$buttonIcon" class="ml-4">
         {{ $button }}
-    </a>
+    </x-ui.button>
     @endif
 </div>
