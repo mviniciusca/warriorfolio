18ae79012402a1bb461eb11749c4c6e9eb45c7cb
Marcos Coelho <mviniciusca@gmail.com>
2025-05-23 13:09:44 -0300

    refactor: Simplify button group markup for better readability

--- resources/views/components/themes/common/partials/hero/button-group.blade.php
+++ resources/views/components/themes/common/partials/hero/button-group.blade.php
@@ -1,38 +1,38 @@
 @props([
-    'class' => null,
-    'color' => false,
-    'icon' => null,
-    'size' => null,
-    'style' => 'filled',
-    'target' => '_self',
-    'title' => null,
-    'url' => null,
+'class' => null,
+'color' => false,
+'icon' => null,
+'size' => null,
+'style' => 'filled',
+'target' => '_self',
+'title' => null,
+'url' => null,
 ])
 
 <div>
     {{-- Filled Button --}}
     @if ($style === 'filled' && $title)
-        @if ($url)
-            <a href="{{ $url }}" target="{{ $target }}">
+    @if ($url)
+    <a href="{{ $url }}" target="{{ $target }}">
         @endif
         <x-ui.button :$color :$icon :style="'filled'" class="{{ $class }}">
             {{ $title }}
         </x-ui.button>
         @if ($url)
-            </a>
-        @endif
+    </a>
+    @endif
     @endif
 
     {{-- Outlined Button --}}
     @if ($style === 'outlined' && $title)
-        @if ($url)
-            <a href="{{ $url }}" target="{{ $target }}">
+    @if ($url)
+    <a href="{{ $url }}" target="{{ $target }}">
         @endif
         <x-ui.button :$color :$icon :style="'outlined'" class="{{ $class }}">
             {{ $title }}
         </x-ui.button>
         @if ($url)
-            </a>
-        @endif
+    </a>
+    @endif
     @endif
 </div>
