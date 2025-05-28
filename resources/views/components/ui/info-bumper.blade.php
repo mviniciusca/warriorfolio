965e443742b4662a37373cf62649ba0031d44bee
Marcos Coelho <mviniciusca@gmail.com>
2025-05-28 01:01:18 -0300

    fix: Refactor info bumper component for improved structure and styling consistency

--- resources/views/components/ui/info-bumper.blade.php
+++ resources/views/components/ui/info-bumper.blade.php
@@ -12,22 +12,30 @@
 
 {{-- Info Bumper Component --}}
 @if ($is_active)
-<div class="{{ $is_center ? 'text-center' : 'text-left' }} my-4 mx-auto">
-    @if ($bumper_link && $bumper_target)
-    <a target="{{ $bumper_target }}" href="{{ $bumper_link ?? '#' }}">
+<div class="{{ $is_center ? 'text-center' : 'text-left' }} my-2 mx-auto">
+    @if ($bumper_link)
+    <a target="{{ $bumper_target }}" href="{{ $bumper_link }}" class="inline-block">
         @endif
+
         <div id="info-bumper"
-            class="{{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : 'animate-none' }} mb-2 inline-flex items-center justify-between border saturn-border rounded-full saturn-bg px-1 py-1 pr-4 text-xs"
-            role="{{ $bumper_role }}">
+            class="mb-2 inline-flex items-center justify-between border saturn-border rounded-full saturn-bg p-1 pr-4 text-xs transition-all duration-300 hover:shadow-md {{ $is_animated ? 'animate__animated animate__fadeInUp animate__delay-1s' : '' }}"
+            role="banner">
+
             <span
-                class="{{ $is_animated ? 'animate-pulse' : 'animate-none' }}
-                {{ $bumper_role === 'danger' ? 'bg-danger-600' : ($bumper_role === 'info' ? 'bg-info-600' : 'saturn-bg-inverse saturn-text-inverse') }} mr-2 rounded-full px-2 py-1 text-white text-xs">
+                class="mr-2 rounded-full px-2 py-1 text-white text-xs font-medium {{ $is_animated ? 'animate-pulse' : '' }} {{ $bumper_role === 'danger' ? 'bg-red-600' : ($bumper_role === 'info' ? 'bg-blue-600' : ($bumper_role === 'success' ? 'bg-green-600' : ($bumper_role === 'warning' ? 'bg-yellow-600' : 'saturn-bg-inverse saturn-text-inverse'))) }}">
                 {!! $bumper_tag !!}
             </span>
-            <span class="text-xs">{!! $bumper_title !!}</span>
-            <x-ui.ionicon :icon="$bumper_icon" class="ml-3" />
+
+            <span class="text-xs font-medium flex-1 text-left">
+                {!! $bumper_title !!}
+            </span>
+
+            @if ($bumper_icon)
+            <x-ui.ionicon :icon="$bumper_icon" class="ml-3 w-4 h-4" />
+            @endif
         </div>
-        @if ($bumper_link && $bumper_target)
+
+        @if ($bumper_link)
     </a>
     @endif
 </div>
