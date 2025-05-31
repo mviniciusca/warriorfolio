86a2d8127c1fee18caf549e3d2c7f282d730b723
Marcos Coelho <mviniciusca@gmail.com>
2025-05-31 12:08:37 -0300

    fix: Refactor section-heading component layout; update button style and improve structure for better readability

--- resources/views/components/core/partials/section-heading.blade.php
+++ resources/views/components/core/partials/section-heading.blade.php
@@ -1,9 +1,40 @@
-@props(['title' => null, 'subtitle' => null, 'button_header' => null, 'button_url' => null, 'button_icon' => null,
-'button_style' => 'primary', 'is_section_filled_inverted' => false, 'is_centered' => false])
+@props([
+'title' => null,
+'subtitle' => null,
+'button_header' => null,
+'button_url' => null,
+'button_icon' => null,
+'button_style' => 'outlined',
+'is_section_filled_inverted' => false,
+'is_centered' => false
+])
 
+@if($is_centered && $button_header)
+<div class="w-full py-6">
+    <div class="flex flex-col items-center text-center space-y-6">
+        <div class="flex flex-col space-y-1">
+            @if($title)
+            <h2 class="text-2xl font-bold">{!! $title !!}</h2>
+            @endif
+            @if($subtitle)
+            <p class="text-sm">{!! $subtitle !!}</p>
+            @endif
+        </div>
+        <div class="flex justify-center">
+            @if($button_url)
+            <a href="{{ $button_url }}">@endif
+                <x-ui.button :$button_style :$is_section_filled_inverted :icon="$button_icon" :label="$button_header" />
+                @if($button_url)
+            </a>
+            @endif
+        </div>
+    </div>
+</div>
+@else
 <div class="w-full flex items-center
-{{ $button_header ? 'justify-between' : ($is_centered ? 'justify-center' : 'justify-start') }} py-6">
-    <div class="flex flex-col {{ $is_centered && !$button_header ? 'text-center' : 'text-left' }}">
+    {{ $button_header ? 'justify-between' : ($is_centered ? 'justify-center' : 'justify-start') }} py-6">
+    <div class="flex flex-col {{ $is_centered && !$button_header ? 'text-center' : 'text-left' }}
+        {{ $button_header ? 'pr-8' : '' }}">
         @if($title)
         <h2 class="text-2xl font-bold">{!! $title !!}</h2>
         @endif
@@ -12,14 +43,14 @@
         @endif
     </div>
     @if($button_header)
-    <div>
+    <div class="ml-8 flex-shrink-0">
         @if($button_url)
         <a href="{{ $button_url }}">@endif
-            <x-ui.button :$button_style :$is_section_filled_inverted :icon="$button_icon" :style="$button_style"
-                :label="$button_header" />
+            <x-ui.button :$button_style :$is_section_filled_inverted :icon="$button_icon" :label="$button_header" />
             @if($button_url)
         </a>
         @endif
     </div>
     @endif
 </div>
+@endif
