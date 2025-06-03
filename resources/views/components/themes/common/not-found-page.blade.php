f6783813ce6ff4fc68954fe1981be86bbb7ecf46
Marcos Coelho <mviniciusca@gmail.com>
2025-06-03 00:49:40 -0300

    refactor: Replace action buttons with Saturn UI button components for consistency

--- resources/views/components/themes/common/not-found-page.blade.php
+++ resources/views/components/themes/common/not-found-page.blade.php
@@ -15,20 +15,15 @@
 
         <!-- Action buttons with Saturn UI styling -->
         <div class="flex flex-col items-center justify-center gap-3 sm:flex-row">
-            <a href="{{ url('/') }}" class="saturn-btn saturn-btn-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" class="size-4 mr-1" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3 12l2-2m0 0l7-7 7 7m-14 0l2 2m0 0l7 7 7-7m-14 0l2-2" />
-                </svg>
-                {{ __('Home') }}
+            <a href="{{ url('/') }}">
+                <x-ui.button :icon_before="true" icon="home-sharp" style="primary">
+                    {{ __('Go to Home') }}
+                </x-ui.button>
             </a>
-            <a href="{{ url()->previous() }}" class="saturn-btn saturn-btn-outlined">
-                <svg xmlns="http://www.w3.org/2000/svg" class="size-4 mr-1" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                </svg>
-                {{ __('Go Back') }}
+            <a href="{{ url()->previous() }}">
+                <x-ui.button :icon_before="true" icon="arrow-back-sharp" style="secondary">
+                    {{ __('Go Back') }}
+                </x-ui.button>
             </a>
         </div>
     </div>
