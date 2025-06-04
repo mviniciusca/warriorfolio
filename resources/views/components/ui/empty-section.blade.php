9f9712eb5083533e3e9b3da19279fb2d23928e44
Marcos Coelho <mviniciusca@gmail.com>
2025-06-03 21:57:29 -0300

    refactor: Simplify icon usage and improve text styling in empty section component

--- resources/views/components/ui/empty-section.blade.php
+++ resources/views/components/ui/empty-section.blade.php
@@ -5,20 +5,16 @@
 @if ($message || ($auth && auth()->check()))
 <div class="mx-auto text-center text-xs {{ $class }}">
     @if ($icon)
-    <x-ui.ionicon :$icon class="mx-auto  dark:text-white/50 text-black/50" />
+    <x-ui.ionicon :$icon class="mx-auto" />
     @else
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="mx-auto size-6 text-secondary-500">
-        <path stroke-linecap="round" stroke-linejoin="round"
-            d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
-    </svg>
+    <x-ui.ionicon icon="information-circle-outline" class="mx-auto" />
     @endif
-    <p class="mt-2 text-secondary-500">
+    <p class="mt-2">
         @if ($message)
         {!! __($message) !!}
         @endif
         @if ($auth && auth()->check())
-        <span class="block mt-1 text-primary-500">{!! __($auth) !!}</span>
+        <span class="block mt-1 opacity-60">{!! __($auth) !!}</span>
         @endif
     </p>
 </div>
