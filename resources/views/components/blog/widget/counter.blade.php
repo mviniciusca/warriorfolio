99c9afefac5be9763e100bf5ef5cba3f48915279
Marcos Coelho <mviniciusca@gmail.com>
2025-06-09 13:22:58 -0300

    refactor: Update heading text and icon in trend widget for improved clarity

--- resources/views/components/blog/widget/counter.blade.php
+++ resources/views/components/blog/widget/counter.blade.php
@@ -1,8 +1,9 @@
 @if ($blog_data['is_trend_widget_active'] ?? false)
 @if ($data)
 <div>
-    <h1 class="saturn-h5 saturn-text flex items-center mb-8">
-        {{ __('I write about') }}
+    <h1 class="saturn-h5 saturn-text flex justify-between items-center mb-8">
+        {{ __('Topics') }}
+        <x-ui.ionicon icon="analytics-outline" />
     </h1>
     <div class="flex flex-wrap gap-2 justify-center">
         @foreach ($data as $item)
