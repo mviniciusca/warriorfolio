97e0f008432222bcfaca56942d40df74a4a0764d
Marcos Coelho <mviniciusca@gmail.com>
2025-06-09 13:25:21 -0300

    refactor: Update newsletter heading and icon for improved presentation; remove deprecated profile widget

--- resources/views/components/blog/notes.blade.php
+++ resources/views/components/blog/notes.blade.php
@@ -49,7 +49,10 @@
                     </div>
                     <!-- Newsletter -->
                     <div class="saturn-bg-accent rounded-lg saturn-text border saturn-border-accent p-12 space-y-4">
-                        <h1 class="saturn-h5 saturn-text">{{ __('Join our Newsletter') }}</h1>
+                        <h1 class="saturn-h5 saturn-text flex justify-between items-center mb-8">
+                            {{ __('Newsletter') }}
+                            <x-ui.ionicon icon="mail-outline" />
+                        </h1>
                         <p class="text-sm opacity-70">{!! __('Join our
                             newsletter and stay updated with the latest articles, tips, and resources for Laravel and
                             development.')
