da2837a742db1c8a69666d3484b9f94d12aa26c8
Marcos Coelho <mviniciusca@gmail.com>
2025-11-27 21:41:25 -0300

    Add feature tests for startup application routes and access control
    - Implement tests to ensure redirection to login for admin access
    - Verify visibility of the login page
    - Check landing, documentation, and blog pages return a 200 status
    - Test access to the dashboard for authenticated users
    - Ensure unauthenticated users are redirected from the dashboard

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,17 @@
 
 namespace Tests;
 
-use App\Models\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use RefreshDatabase;
 
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
 }
